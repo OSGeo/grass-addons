94a26c41961cdb8f07d27bcea43782882af17890
Yann Chemin <yann.chemin@gmail.com>
2007-07-01 10:07:52 +0000

    added function
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@25655 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gipe/i.eb.disp/main.c
+++ gipe/i.eb.disp/main.c
@@ -1,12 +1,10 @@
 /****************************************************************************
  *
- * MODULE:       i.eb.disp
+ * MODULE:       i.eb.rohair
  * AUTHOR(S):    Yann Chemin - ychemin@gmail.com
- * PURPOSE:      Calculates the displacement height above skin surface
- *                as seen in Pawan (2004), a flag makes savi2lai instead of
- *                direct LAI input.
+ * PURPOSE:      Calculates the standard air density as seen in Pawan (2004)
  *
- * COPYRIGHT:    (C) 2002-2006 by the GRASS Development Team
+ * COPYRIGHT:    (C) 2002-2007 by the GRASS Development Team
  *
  *               This program is free software under the GNU General Public
  *   	    	 License (>=v2). Read the file COPYING that comes with GRASS
@@ -21,8 +19,7 @@
 #include <grass/glocale.h>
 
 
-double dis_p(double lai);
-double savi_lai(double savi);
+double roh_air(double dem);
 
 int main(int argc, char *argv[])
 {
@@ -33,9 +30,9 @@ int main(int argc, char *argv[])
 
 	int verbose=1;
 	struct GModule *module;
-	struct Option *input1, *output1;
+	struct Option *input1, *input2, *output1;
 	
-	struct Flag *flag1, *flag2;	
+	struct Flag *flag1;	
 	struct History history; //metadata
 	
 	/************************************/
@@ -43,69 +40,86 @@ int main(int argc, char *argv[])
 	char *name;   // input raster name
 	char *result1; //output raster name
 	//File Descriptors
-	int infd_lai;
+	int infd_dem, infd_tempka;
 	int outfd1;
 	
-	char *disp;
-	char *lai;	
+	char *rohair;
+	char *dem, *tempka;	
 	int i=0,j=0;
 	
-	void *inrast_lai;
+	void *inrast_dem, *inrast_tempka;
 	unsigned char *outrast1;
 	RASTER_MAP_TYPE data_type_output=DCELL_TYPE;
-	RASTER_MAP_TYPE data_type_lai;
+	RASTER_MAP_TYPE data_type_dem;
+	RASTER_MAP_TYPE data_type_tempka;
 	/************************************/
 	G_gisinit(argv[0]);
 
 	module = G_define_module();
-	module->keywords = _("disp, energy balance, SEBAL");
-	module->description = _("Displacement height above skin surface, as seen in Pawan (2004). A flag (-s) permits direct SAVI input using the equation in the same document, be careful using it.");
+	module->keywords = _("rohair, energy balance, SEBAL");
+	module->description = _("Standard height-based Air Density as seen in Pawan (2004).");
 
 	/* Define the different options */
 	input1 = G_define_option() ;
-	input1->key	   = _("lai");
+	input1->key	   = _("dem");
 	input1->type       = TYPE_STRING;
 	input1->required   = YES;
 	input1->gisprompt  =_("old,cell,raster") ;
-	input1->description=_("Name of the LAI map [-]");
-	input1->answer     =_("lai");
+	input1->description=_("Name of the DEM map [-]");
+	input1->answer     =_("dem");
+
+	input2 = G_define_option() ;
+	input2->key	   = _("dem");
+	input2->type       = TYPE_STRING;
+	input2->required   = YES;
+	input2->gisprompt  =_("old,cell,raster") ;
+	input2->description=_("Name of the Air Temperature map [K]");
+	input2->answer     =_("tempka");
 
 	output1 = G_define_option() ;
-	output1->key        =_("disp");
+	output1->key        =_("rohair");
 	output1->type       = TYPE_STRING;
 	output1->required   = YES;
 	output1->gisprompt  =_("new,dcell,raster");
-	output1->description=_("Name of the output disp layer");
-	output1->answer     =_("disp");
+	output1->description=_("Name of the output rohair layer");
+	output1->answer     =_("rohair");
 
 	
 	flag1 = G_define_flag();
 	flag1->key = 'q';
 	flag1->description = _("Quiet");
 	
-	flag2 = G_define_flag();
-	flag2->key = 's';
-	flag2->description = _("use savi2lai conversion (Pawan, 2004)");
-
 	/********************/
 	if (G_parser(argc, argv))
 		exit (EXIT_FAILURE);
 
-	lai	 	= input1->answer;
+	dem	 	= input1->answer;
 		
 	result1  = output1->answer;
 	verbose = (!flag1->answer);
 	/***************************************************/
-	mapset = G_find_cell2(lai, "");
+	mapset = G_find_cell2(dem, "");
 	if (mapset == NULL) {
-		G_fatal_error(_("cell file [%s] not found"), lai);
+		G_fatal_error(_("cell file [%s] not found"), dem);
 	}
-	data_type_lai = G_raster_map_type(lai,mapset);
-	if ( (infd_lai = G_open_cell_old (lai,mapset)) < 0)
-		G_fatal_error (_("Cannot open cell file [%s]"), lai);
-	if (G_get_cellhd (lai, mapset, &cellhd) < 0)
-		G_fatal_error (_("Cannot read file header of [%s])"), lai);
-	inrast_lai = G_allocate_raster_buf(data_type_lai);
+	data_type_dem = G_raster_map_type(dem,mapset);
+	if ( (infd_dem = G_open_cell_old (dem,mapset)) < 0)
+		G_fatal_error (_("Cannot open cell file [%s]"), dem);
+	if (G_get_cellhd (dem, mapset, &cellhd) < 0)
+		G_fatal_error (_("Cannot read file header of [%s])"), dem);
+	inrast_dem = G_allocate_raster_buf(data_type_dem);
+	/***************************************************/
+	/***************************************************/
+	mapset = G_find_cell2(tempka, "");
+	if (mapset == NULL) {
+		G_fatal_error(_("cell file [%s] not found"), tempka);
+	}
+	data_type_tempka = G_raster_map_type(tempka,mapset);
+	if ( (infd_tempka = G_open_cell_old (tempka,mapset)) < 0)
+		G_fatal_error (_("Cannot open cell file [%s]"), tempka);
+	if (G_get_cellhd (tempka, mapset, &cellhd) < 0)
+		G_fatal_error (_("Cannot read file header of [%s])"), tempka);
+	inrast_tempka = G_allocate_raster_buf(data_type_tempka);
 	/***************************************************/
 	G_debug(3, "number of rows %d",cellhd.rows);
 	nrows = G_window_rows();
@@ -118,42 +132,55 @@ int main(int argc, char *argv[])
 	for (row = 0; row < nrows; row++)
 	{
 		DCELL d;
-		DCELL d_lai;
+		DCELL d_dem;
+		DCELL d_tempka;
 		if(verbose)
 			G_percent(row,nrows,2);
-//		printf("row = %i/%i\n",row,nrows);
 		/* read soil input maps */	
-		if(G_get_raster_row(infd_lai,inrast_lai,row,data_type_lai)<0)
-			G_fatal_error(_("Could not read from <%s>"),lai);
+		if(G_get_raster_row(infd_dem,inrast_dem,row,data_type_dem)<0)
+			G_fatal_error(_("Could not read from <%s>"),dem);
 		/*process the data */
 		for (col=0; col < ncols; col++)
 		{
-			d_lai = ((DCELL *) inrast_lai)[col];
-			if(G_is_d_null_value(&d_lai)){
+			switch(data_type_dem){
+				case CELL_TYPE:
+					d_dem = (double) ((CELL *) inrast_dem)[col];
+					break;
+				case FCELL_TYPE:
+					d_dem = (double) ((FCELL *) inrast_dem)[col];
+					break;
+				case DCELL_TYPE:
+					d_dem = ((DCELL *) inrast_dem)[col];
+					break;
+			}
+			switch(data_type_tempka){
+				case CELL_TYPE:
+					d_tempka = (double) ((CELL *) inrast_tempka)[col];
+					break;
+				case FCELL_TYPE:
+					d_tempka = (double) ((FCELL *) inrast_tempka)[col];
+					break;
+				case DCELL_TYPE:
+					d_tempka = ((DCELL *) inrast_tempka)[col];
+					break;
+			}
+			if(G_is_d_null_value(&d_dem)||G_is_d_null_value(&d_tempka)){
 				((DCELL *) outrast1)[col] = -999.99;
 			} else {
-				if(flag2->answer){
-					/********************/
-					/* savi2lai()       */
-					d = savi_lai(d_lai);
-					/********************/
-					/* calculate disp   */
-					d = dis_p(d);
-					((DCELL *) outrast1)[col] = d;
-				} else {
-					/********************/
-					/* calculate disp   */
-					d = dis_p(d_lai);
-					((DCELL *) outrast1)[col] = d;
-				}
+				/********************/
+				/* calculate rohair */
+				d = roh_air(d_dem, tempka);
+				((DCELL *) outrast1)[col] = d;
 			}
 		}
 		if (G_put_raster_row (outfd1, outrast1, data_type_output) < 0)
 			G_fatal_error(_("Cannot write to output raster file"));
 	}
 
-	G_free (inrast_lai);
-	G_close_cell (infd_lai);
+	G_free (inrast_dem);
+	G_free (inrast_tempka);
+	G_close_cell (infd_dem);
+	G_close_cell (infd_tempka);
 	
 	G_free (outrast1);
 	G_close_cell (outfd1);
---
+++ gipe/i.eb.disp/roh_air.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+/* Atmospheric Air Density 
+ * Requires Air Temperature and DEM
+ */
+
+double roh_air(double dem, double tempka)
+{
+	double b, result;
+
+	b = (( tempka - 0.00627 * dem ) / tempka );
+	
+	result = 349.467 * pow( b , 5.26 ) / tempka ;
+		
+	return result;
+}
