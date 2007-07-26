9c3cabd3f8e5b2981bccf4f0506628937b4b6306
Massimiliano Cannata <massimiliano.cannata@supsi.ch>
2007-07-26 10:47:31 +0000

    added new command r.snow
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@25729 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- HydroFOSS/r.evapo.PM/local_proto.h
+++ HydroFOSS/r.evapo.PM/local_proto.h
@@ -5,4 +5,4 @@ DCELL calc_Eo			(DCELL T);
 DCELL calc_Ea			(DCELL Eo,DCELL RH);
 DCELL calc_G			(DCELL Rn, int t);
 DCELL calc_ETp			(DCELL T,DCELL Z,DCELL u2,DCELL Rn,int day,DCELL Rh,DCELL hc);
-DCELL calc_openwaterETp	(DCELL T,DCELL Z,DCELL u2,DCELL Rn,int day,DCELL Rh,DCELL hc);
+DCELL calc_openwaterETp	        (DCELL T,DCELL Z,DCELL u2,DCELL Rn,int day,DCELL Rh,DCELL hc);
