3d23624c7134b1674b333bcf7da1c174395f1752
Yann Chemin <yann.chemin@gmail.com>
2008-04-02 02:51:40 +0000

    Updated Aster Albedo equation to Liang, 2001
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@30837 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gipe/i.albedo/bb_alb_aster.c
+++ gipe/i.albedo/bb_alb_aster.c
@@ -1,10 +1,12 @@
 // Broadband albedo Aster 
+// After Liang, S.L., 2001. Narrowband to broadband conversion of land surface albedo 1 Algorithms. Remote Sensing of Environment. 2001, 76, 213-238.
+// Input: Ref1, ref3, Ref5, Ref6, Ref8, Ref9
 
-double bb_alb_aster( double greenchan, double redchan, double nirchan, double swirchan1, double swirchan2, double swirchan3, double swirchan4, double swirchan5, double swirchan6 )
+double bb_alb_aster( double greenchan, double nirchan, double swirchan2, double swirchan3, double swirchan5, double swirchan6 )
 {
 	double	result;
 	
-	result = ( 0.09*greenchan + 0.06*redchan + 0.1*nirchan + 0.092*swirchan1 + 0.035*swirchan2 + 0.04*swirchan3 + 0.047*swirchan4 + 0.07*swirchan5 + 0.068*swirchan6 ) / ((0.09+0.06+0.1+0.092+0.035+0.04+0.047+0.07+0.068)*1000.0);
+	result = ( 0.484*greenchan + 0.335*nirchan - 0.324*swirchan2 + 0.551*swirchan3 + 0.305*swirchan5 - 0.367*swirchan6 -0.0015 ) ;
 
 	return result;
 }
