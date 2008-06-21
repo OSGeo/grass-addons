6d0a0d7a8356d2a701b840613e670fdec3bceaf8
Markus Neteler <neteler@gmail.com>
2008-06-21 20:56:25 +0000

    Makefile added. Related variables fixed
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@31789 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.rast4d/bin/init_db.sh
+++ raster/r.rast4d/bin/init_db.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/sh -x
 #this script creates the database tables needed to store raster and temporal map informations
 
 #create the raster and temporal tables
-cat $SQL_DIR/create_raster_table.sql | sed s/TABLE_NAME/$RASTER_TABLE_NAME/ | $DBM $DATABASE 
-cat $SQL_DIR/create_raster_time_table.sql | sed s/TABLE_NAME/$RASTER_TIME_TABLE_NAME/ | $DBM $DATABASE 
-cat $SQL_DIR/create_raster_metadata_table.sql | sed s/TABLE_NAME/$RASTER_METADATA_TABLE_NAME/ | $DBM $DATABASE 
-cat $SQL_DIR/create_temporal_raster_table.sql | sed s/TABLE_NAME/$TEMPORAL_RASTER_TABLE_NAME/ | $DBM $DATABASE 
-cat $SQL_DIR/create_temporal_raster_metadata_table.sql | sed s/TABLE_NAME/$TEMPORAL_RASTER_METADATA_TABLE_NAME/ | $DBM $DATABASE 
+cat $GRAST4D_SQL_DIR/create_raster_table.sql | sed s/TABLE_NAME/$GRASTER_TABLE_NAME/ | $GRAST4D_DBM $GRAST4D_DATABASE 
+cat $GRAST4D_SQL_DIR/create_raster_time_table.sql | sed s/TABLE_NAME/$GRASTER_TIME_TABLE_NAME/ | $GRAST4D_DBM $GRAST4D_DATABASE 
+cat $GRAST4D_SQL_DIR/create_raster_metadata_table.sql | sed s/TABLE_NAME/$GRASTER_METADATA_TABLE_NAME/ | $GRAST4D_DBM $GRAST4D_DATABASE 
+cat $GRAST4D_SQL_DIR/create_temporal_raster_table.sql | sed s/TABLE_NAME/$GTEMPORAL_RASTER_TABLE_NAME/ | $GRAST4D_DBM $GRAST4D_DATABASE 
+cat $GRAST4D_SQL_DIR/create_temporal_raster_metadata_table.sql | sed s/TABLE_NAME/$GTEMPORAL_RASTER_METADATA_TABLE_NAME/ | $GRAST4D_DBM $GRAST4D_DATABASE 
 
 # create the raster view
-echo  "create view $RASTER_VIEW_NAME as select * from $RASTER_TABLE_NAME, $RASTER_TIME_TABLE_NAME, $RASTER_METADATA_TABLE_NAME where $RASTER_TABLE_NAME.name = $RASTER_TIME_TABLE_NAME.name AND $RASTER_TABLE_NAME.name = $RASTER_TIME_TABLE_NAME.name AND $RASTER_METADATA_TABLE_NAME.name = $RASTER_TIME_TABLE_NAME.name;" | $DBM $DATABASE
+echo  "create view $GRASTER_VIEW_NAME as select * from $GRASTER_TABLE_NAME, $GRASTER_TIME_TABLE_NAME, $GRASTER_METADATA_TABLE_NAME where $GRASTER_TABLE_NAME.name = $GRASTER_TIME_TABLE_NAME.name AND $GRASTER_TABLE_NAME.name = $GRASTER_TIME_TABLE_NAME.name AND $GRASTER_METADATA_TABLE_NAME.name = $GRASTER_TIME_TABLE_NAME.name;" | $GRAST4D_DBM $GRAST4D_DATABASE
 
 #register trigger functions
 
--- raster/r.rast4d/globals/defines.sh
+++ raster/r.rast4d/globals/defines.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 
 #definitions
-export BASE="`pwd`"
-export DB_DIR="$BASE/db"
-export BIN_DIR="$BASE/bin"
-export LIB_DIR="$BASE/lib"
-export SQL_DIR="$BASE/sql"
-export DATABASE=$DB_DIR/database.sqlite
-export PATH=$BIN_DIR:$PATH
-export DBM="sqlite3"
+export GRAST4D_BASE="`pwd`"
+export GRAST4D_DB_DIR="$GRAST4D_BASE/db"
+
+### setup enviro vars ###
+eval `g.gisenv`
+: ${GISBASE?} ${GISDBASE?} ${LOCATION_NAME?} ${MAPSET?}
+
+export GRAST4D_BIN_DIR="${GISBASE}/etc/r.rast4d/bin"
+export GRAST4D_LIB_DIR="${GISBASE}/etc/r.rast4d/lib"
+export GRAST4D_SQL_DIR="${GISBASE}/etc/r.rast4d/sql"
+export GRAST4D_DATABASE=$GRAST4D_DB_DIR/database.sqlite
+export PATH=$GRAST4D_BIN_DIR:$PATH
+export GRAST4D_DBM="sqlite3"
 
 # Table name definitions
-export RASTER_VIEW_NAME="raster_view"
-export RASTER_TABLE_NAME="raster_table"
-export RASTER_TIME_TABLE_NAME="raster_time_table"
-export RASTER_METADATA_TABLE_NAME="raster_metadata_table"
-export TEMPORAL_VIEW_NAME="temporal_view"
-export TEMPORAL_RASTER_TABLE_NAME="temporal_raster_table"
-export TEMPORAL_RASTER_METADATA_TABLE_NAME="temporal_raster_metadata_table"
+export GRASTER_VIEW_NAME="raster_view"
+export GRASTER_TABLE_NAME="raster_table"
+export GRASTER_TIME_TABLE_NAME="raster_time_table"
+export GRASTER_METADATA_TABLE_NAME="raster_metadata_table"
+export GTEMPORAL_VIEW_NAME="temporal_view"
+export GTEMPORAL_RASTER_TABLE_NAME="temporal_raster_table"
+export GTEMPORAL_RASTER_METADATA_TABLE_NAME="temporal_raster_metadata_table"
 
-export RASTER_REFERENCE_TABLE_PREFIX="raster_reference_table"
-export RASTER_GROUP_TABLE_PREFIX="raster_group_table"
-export RASTER_TEMPORAL_TABLE_PREFIX="raster_temporal_table"
-export RASTER_METADATA_TABLE_PREFIX="raster_metadata_table"
-export RASTER_CATEGORY_TABLE_PREFIX="raster_category_table"
+export GRASTER_REFERENCE_TABLE_PREFIX="raster_reference_table"
+export GRASTER_GROUP_TABLE_PREFIX="raster_group_table"
+export GRASTER_TEMPORAL_TABLE_PREFIX="raster_temporal_table"
+export GRASTER_METADATA_TABLE_PREFIX="raster_metadata_table"
+export GRASTER_CATEGORY_TABLE_PREFIX="raster_category_table"
 
