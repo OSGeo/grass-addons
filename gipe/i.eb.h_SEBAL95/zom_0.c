dd6d096bc9b86b961caec011a21972aa1f124824
Yann Chemin <yann.chemin@gmail.com>
2008-07-30 11:44:56 +0000

    bug fix Z0m
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@32370 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gipe/i.eb.h_SEBAL95/zom_0.c
+++ gipe/i.eb.h_SEBAL95/zom_0.c
@@ -5,15 +5,15 @@
 double zom_0(double ndvi, double ndvi_max)
 {
 	double a, b, zom;
-	double hv_ndvimax=1.5; /* crop vegetation height (m) */
-	double hv_desert=0.002; /* desert base vegetation height (m) */
+//	double hv_ndvimax=1.5; /* crop vegetation height (m) */
+//	double hv_desert=0.002; /* desert base vegetation height (m) */
 
-	a = (log(hv_desert)-((log(hv_ndvimax/7)-log(hv_desert))/(ndvi_max-0.02)*0.02));
-	b = ((log(hv_ndvimax/7)-log(hv_desert))/(ndvi_max-0.02)) * ndvi;
-	zom = exp(a+b); 
+//	a = (log(hv_desert)-((log(hv_ndvimax/7)-log(hv_desert))/(ndvi_max-0.02)*0.02));
+//	b = ((log(hv_ndvimax/7)-log(hv_desert))/(ndvi_max-0.02)) * ndvi;
+//	zom = exp(a+b); 
 	
 //	printf("****************zom = %5.3f\n", zom);
-	
+	zom = exp(3.3219*ndvi-3.9939);
 	return (zom);
 }
 
