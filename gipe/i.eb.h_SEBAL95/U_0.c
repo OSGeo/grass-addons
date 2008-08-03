bda2d854a98c796c3a167a76bb1931b6b80ccc5c
Yann Chemin <yann.chemin@gmail.com>
2008-08-03 09:46:40 +0000

    Restored clean-up version of i.eb.h_SEBAL95
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@32480 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ gipe/i.eb.h_SEBAL95/U_0.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+#include"functions.h"
+
+double U_0(double zom_0, double u2m)
+{
+	double u_0;
+
+	u_0 = u2m*0.41*log10(200/(0.15/7))/(log10(2/(0.15/7))*log10(200/zom_0)); 
+
+//	printf("u_0 = %5.3f\n", u_0);
+	
+	return (u_0);
+}
+
---
+++ gipe/i.eb.h_SEBAL95/h1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+#include"functions.h"
+
+double h1(double roh_air, double rah, double dtair)
+{
+	double result;
+
+	result = roh_air*1004*dtair / rah;
+	
+	return result;
+}
+
