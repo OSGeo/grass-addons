2c980996ee201d8dac7a3c92c36cf29b96882516
Yann Chemin <yann.chemin@gmail.com>
2008-10-07 02:29:57 +0000

    Updated GUIs with i.landsat.toar and i.modis.stateqa
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@33702 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gipe/gmmenu.tcl
+++ gipe/gmmenu.tcl
@@ -561,7 +561,9 @@ set descmenu [subst  {
 	{cascad {[G_msg "GIPE"]} {} "" $tmenu {
 		{cascad {[G_msg "DN2Rad2Ref"]} {} "" $tmenu {
 				{command {[G_msg "MODIS QC extrator"]} {} "i.qc.modis" {} -command {execute i.qc.modis }}
+				{command {[G_msg "MODIS State QA extrator"]} {} "i.modis.stateqa" {} -command {execute i.modis.stateqa }}
 				{separator}
+				{command {[G_msg "All types of Landsat"]} {} "i.landsat.toar" {} -command {execute i.landsat.toar }}
 				{command {[G_msg "Landsat 7 ETM+"]} {} "i.dn2ref.l7" {} -command {execute i.dn2ref.l7 }}
 				{command {[G_msg "Landsat 7 ETM+ (from .met)"]} {} "i.dn2full.l7" {} -command {execute i.dn2full.l7 }}
 				{command {[G_msg "Landsat 5 TM (from NLAPS report .txt)"]} {} "i.dn2full.l5" {} -command {execute i.dn2full.l5 }}
