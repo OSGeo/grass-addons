acf20a72f2c546580f420c96a636eda5ffa4f4c6
E. Jorge Tizado <ejtizado@ono.com>
2009-01-31 16:37:37 +0000

    Modified to compile with GRASS 7
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@35690 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass7/imagery/i.topo.corr/correction.c
@@ -0,0 +1,146 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include <grass/gis.h>
+#include <grass/glocale.h>
+
+#include "local_proto.h"
+
+void eval_tcor(int method, Gfile *out, Gfile *cosi, Gfile *band, double zenith)
+{
+	int i, row, col, nrows, ncols;
+	void *pref, *pcos;
+
+	double cos_z, cos_i, ref_i;
+	double n, sx, sxx, sy, sxy, tx, ty;
+	double a, m, cka, ckb, kk;
+
+
+	nrows = G_window_rows();
+	ncols = G_window_cols();
+
+	cos_z = cos(D2R * zenith);
+
+	/* Calculating regression */
+	if (method > NON_LAMBERTIAN)
+	{
+		n = sx = sxx = sy = sxy = 0.;
+		for (row = 0; row < nrows; row++)
+		{
+			G_percent(row, nrows, 2);
+
+			G_get_raster_row(band->fd, band->rast, row, DCELL_TYPE);
+			G_get_raster_row(cosi->fd, cosi->rast, row, cosi->type);
+
+			for (col = 0; col < ncols; col++)
+			{
+				switch( cosi->type )
+				{
+					case FCELL_TYPE:
+						pcos  = (void *)((FCELL *)cosi->rast + col);
+						cos_i = (double)((FCELL *)cosi->rast)[col];
+						break;
+					case DCELL_TYPE:
+						pcos  = (void *)((DCELL *)cosi->rast + col);
+						cos_i = (double)((DCELL *)cosi->rast)[col];
+						break;
+					default:
+						pcos  = NULL;
+						cos_i = 0.;
+						break;
+				}
+				pref = (void *)((DCELL *)band->rast + col);
+
+				if( !G_is_null_value(pref, DCELL_TYPE) &&
+					!G_is_null_value(pcos, cosi->type) )
+				{
+					ref_i = (double)*((DCELL *)pref);
+					switch( method )
+					{
+						case MINNAERT:
+							if (cos_i > 0. && cos_z > 0. && ref_i > 0.)
+							{
+								n++;
+								tx = log(cos_i/cos_z);
+								ty = log(ref_i);
+								sx  += tx; sxx += tx*tx;
+								sy  += ty; sxy += tx*ty;
+							}
+							break;
+						case C_CORRECT:
+							{
+								n++;
+								sx  += cos_i; sxx += cos_i*cos_i;
+								sy  += ref_i; sxy += cos_i*ref_i;
+							}
+							break;
+					}
+				}
+			}
+		}
+		m = (n == 0.) ? 1. : (n * sxy - sx * sy) / (n * sxx - sx * sx);
+		a = (n == 0.) ? 0. : (sy - m * sx) / n;
+	}
+	/* Calculating Constants */
+	switch( method )
+	{
+		case MINNAERT:
+			cka = ckb = 0.;
+			kk = m;
+			G_message("Minnaert constant = %lf", kk);
+			break;
+		case C_CORRECT:
+			cka = ckb = a/m;
+			kk = 1.;
+			G_message("C-factor constant = %lf (a = %.5f, m = %.5f)", cka, a, m);
+			break;
+		case PERCENT:
+			cka = 2. - cos_z; ckb = 1.;
+			kk = 1.;
+			break;
+		default: /* COSINE */
+			cka = ckb = 0.;
+			kk = 1.;
+	}
+	/* Topographic correction */
+	for (row = 0; row < nrows; row++)
+	{
+		G_percent(row, nrows, 2);
+
+		G_get_raster_row(band->fd, band->rast, row, band->type);
+		G_get_raster_row(cosi->fd, cosi->rast, row, cosi->type);
+
+		for (col = 0; col < ncols; col++)
+		{
+			switch( cosi->type )
+			{
+				case FCELL_TYPE:
+					pcos  = (void *)((FCELL *)cosi->rast + col);
+					cos_i = (double)*((FCELL *)pcos);
+					break;
+				case DCELL_TYPE:
+					pcos  = (void *)((DCELL *)cosi->rast + col);
+					cos_i = (double)*((DCELL *)pcos);
+					break;
+				default:
+					pcos  = NULL;
+					cos_i = 0.;
+					break;
+			}
+			pref = (void *)((DCELL *)band->rast + col);
+
+			if( pcos == NULL ||
+				G_is_null_value(pref, DCELL_TYPE) ||
+				G_is_null_value(pcos, cosi->type) )
+			{
+				G_set_null_value((DCELL *) out->rast + col, 1, DCELL_TYPE);
+			}
+			else
+			{
+				ref_i = (double)*((DCELL *)pref);
+				((DCELL *)out->rast)[col] = (DCELL)(ref_i * pow( (cos_z + cka)/(cos_i + ckb), kk ) );
+			}
+		}
+		G_put_raster_row(out->fd, out->rast, out->type);
+	}
+}
---
+++ grass7/imagery/i.topo.corr/main.c
@@ -0,0 +1,235 @@
+/****************************************************************************
+ *
+ * MODULE:       i.topo.corr
+ *
+ * AUTHOR(S):    E. Jorge Tizado - ej.tizado@unileon.es
+ *
+ * PURPOSE:      Topographic corrections
+ *
+ * COPYRIGHT:    (C) 2002, 2005 2008 by the GRASS Development Team
+ *
+ *               This program is free software under the GNU General Public
+ *               License (>=v2). Read the file COPYING that comes with GRASS
+ *               for details.
+ *
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <grass/gis.h>
+#include <grass/glocale.h>
+
+#include "local_proto.h"
+
+int full_open_old(Gfile * gf, char * fname)
+{
+	gf->fd = -1;
+
+	snprintf(gf->name, 127, "%s", fname);
+
+	gf->mapset = G_find_cell2(gf->name, "");
+	if( gf->mapset == NULL )
+		G_fatal_error("Cell file <%s> not found", gf->name);
+
+	gf->fd = G_open_cell_old(gf->name, gf->mapset);
+	if( gf->fd < 0 )
+		G_fatal_error(_("Cannot open file [%s] in mapset [%s]"), gf->name, gf->mapset);
+
+	gf->type = G_raster_map_type(gf->name, gf->mapset);
+
+	return gf->fd;
+}
+
+int full_open_new(Gfile * gf, char * fname, RASTER_MAP_TYPE ftype)
+{
+	gf->fd = -1;
+
+	snprintf(gf->name, 127, "%s", fname);
+	if( G_legal_filename(gf->name) < 0 )
+		G_fatal_error("[%s] is an illegal name", gf->name);
+
+	gf->type = ftype;
+	gf->mapset = G_mapset();
+	gf->fd  = G_open_raster_new(gf->name, gf->type);
+	if( gf->fd < 0 )
+		G_fatal_error(_("Cannot create file [%s] in mapset [%s]"), gf->name, gf->mapset);
+
+	return gf->fd;
+}
+
+int main(int argc, char *argv[])
+{
+	struct History history;
+	struct GModule *module;
+	struct Cell_head hd_band, hd_dem, window;
+
+	char bufname[128];
+	int nrows, ncols, row, col;
+
+	int i;
+	struct Option *base, *output, *input, *zeni, *azim, *metho;
+	struct Flag *ilum;
+
+	Gfile dem, out, band;
+	double zenith, azimuth;
+	int method = COSINE;
+
+	/* initialize GIS environment */
+	G_gisinit(argv[0]);
+
+	/* initialize module */
+	module = G_define_module();
+	module->description = _("Topografic correction");
+	module->keywords = _("Topographic correction, Cosine, Minnaert, C-Factor");
+
+	/* It defines the different parameters */
+
+	input = G_define_option();
+	input->key          = _("input");
+	input->type         = TYPE_STRING;
+	input->required     = NO;
+	input->multiple     = YES;
+	input->gisprompt    = _("input,cell,raster");
+	input->description  = _("List of reflectance band files");
+
+	output = G_define_option();
+	output->key         = _("output");
+	output->type        = TYPE_STRING;
+	output->required    = YES;
+	output->gisprompt   = _("output,cell,raster");
+	output->description = _("File name of output");
+
+	base = G_define_option();
+	base->key           = _("basemap");
+	base->type          = TYPE_STRING;
+	base->required      = YES;
+	base->gisprompt     = _("base,cell,raster");
+	base->description   = _("Base map for analysis (elevation or ilumination)");
+
+	zeni = G_define_option();
+	zeni->key          = _("zenith");
+	zeni->type         = TYPE_DOUBLE;
+	zeni->required     = YES;
+	zeni->gisprompt    = _("zenith");
+	zeni->description  = _("Solar zenith in degrees = 90 - [solar elevation])");
+
+	azim = G_define_option();
+	azim->key          = _("azimuth");
+	azim->type         = TYPE_DOUBLE;
+	azim->required     = NO;
+	azim->gisprompt    = _("azimuth");
+	azim->description  = _("Solar azimuth in degrees (only if flag -i)");
+
+	metho = G_define_option();
+	metho->key         = _("method");
+	metho->type        = TYPE_STRING;
+	metho->required    = NO;
+	metho->options     = "cosine,minnaert,c-factor,percent";
+	metho->gisprompt   = _("topographic correction method");
+	metho->description = _("Topographic correction method");
+	metho->answer      = "c-factor";
+
+	ilum = G_define_flag();
+	ilum->key          = 'i';
+	ilum->description  = _("Output sun ilumination terrain model");
+	ilum->answer       = 0;
+
+    if (G_parser(argc, argv))
+		exit(EXIT_FAILURE);
+
+	if (ilum->answer && azim->answer == NULL)
+		G_fatal_error("Solar azimuth is needed to calculate ilumination terrain model.");
+
+	if (!ilum->answer && input->answer == NULL)
+		G_fatal_error("Reflectance files are needed to topographic correction.");
+
+	zenith  = atof(zeni->answer);
+
+	/* Preparation files */
+	// Abrir el in aqui (común)
+
+	/* Evaluate only IL raster file */
+	// i.topo.corr -i out=cosi.on07 base=SRTM_v2 zenith=33.3631 azimuth=59.8897
+	if (ilum->answer)
+	{
+		azimuth = atof(azim->answer);
+		/* Warning: make buffers and output after set window */
+		full_open_old( &dem, base->answer );
+		/* Set window to DEM file */
+		G_get_window (&window);
+		G_get_cellhd (dem.name, dem.mapset, &hd_dem);
+		G_align_window (&window, &hd_dem);
+		G_set_window (&window);
+		/* Open and buffer of the elevation file */
+		if (G_raster_map_type(dem.name, dem.mapset) != FCELL_TYPE)
+			G_fatal_error(_("Elevation model must be FCELL"));
+		dem.rast  = G_allocate_raster_buf(FCELL_TYPE);
+		/* Open and buffer of the output file */
+		full_open_new( &out, output->answer, DCELL_TYPE );
+		out.rast = G_allocate_raster_buf(out.type);
+		/* -- -- -- */
+		eval_cosi( &out, &dem, zenith, azimuth );
+		/* Close files, buffers, and write history */
+		G_free(dem.rast);
+		G_close_cell(dem.fd);
+		G_free(out.rast);
+		G_close_cell(out.fd);
+		G_short_history(out.name, "raster", &history);
+		G_command_history(&history);
+		G_write_history(out.name, &history);
+	}
+	/* Evaluate topographic correction for all bands */
+	// i.topo.corr input=on07.toar.1 out=tcor base=cosi.on07 zenith=33.3631 method=c-correct
+	else
+	{
+		if (G_strcasecmp(metho->answer, "cosine") == 0)        method = COSINE;
+		else if (G_strcasecmp(metho->answer, "percent") == 0)  method = PERCENT;
+		else if (G_strcasecmp(metho->answer, "minnaert") == 0) method = MINNAERT;
+		else if (G_strcasecmp(metho->answer, "c-factor") == 0) method = C_CORRECT;
+		else G_fatal_error(_("Invalid method: %s"), metho->answer);
+
+		full_open_old( &dem, base->answer );
+		if (dem.type == CELL_TYPE)
+			G_fatal_error(_("Illumination model is CELL"));
+
+		for (i = 0; input->answers[i] != NULL; i++)
+		{
+			G_message("\nBand %s: ", input->answers[i]);
+			/* Abre fichero de bandas y el de salida */
+			full_open_old( &band, input->answers[i] );
+			if (band.type != DCELL_TYPE) {
+				G_warning(_("Reflectance of <%s> is not DCELL"), input->answers[i]);
+				G_close_cell(band.fd);
+				continue;
+			}
+			G_get_cellhd (band.name, band.mapset, &hd_band);
+			G_set_window (&hd_band); /* Antes de out_open y allocate para mismo tamaño */
+			/* ----- */
+			snprintf(bufname, 127, "%s.%s", output->answer, input->answers[i]);
+			////////////////////// QUE COJA SOLO EL NUMERO
+			full_open_new( &out, bufname, DCELL_TYPE );
+			out.rast  = G_allocate_raster_buf(out.type);
+			band.rast = G_allocate_raster_buf(band.type);
+			dem.rast  = G_allocate_raster_buf(dem.type);
+			/* ----- */
+			eval_tcor( method, &out, &dem, &band, zenith );
+			/* ----- */
+			G_free(dem.rast);
+
+			G_free(band.rast);
+			G_close_cell(band.fd);
+
+			G_free(out.rast);
+			G_close_cell(out.fd);
+
+			G_short_history(out.name, "raster", &history);
+			G_command_history(&history);
+			G_write_history(out.name, &history);
+		}
+
+		G_close_cell(dem.fd);
+	}
+
+    exit(EXIT_SUCCESS);
+}
+
