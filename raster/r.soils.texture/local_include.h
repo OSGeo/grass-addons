df519a94a23e8081ce97d7320b37eb27e89f66d1
Hamish Bowman <hamish_b@yahoo.com>
2009-04-20 02:52:03 +0000

    run grass_indent.sh on code
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@36801 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.soils.texture/local_include.h
+++ raster/r.soils.texture/local_include.h
@@ -1,3 +1,4 @@
+
 /****************************************************************************
  *
  * MODULE:       r.soils.texture
@@ -16,23 +17,26 @@
 #define MAX_POINT 15
 #define MAX_LABEL 50
 
-/*///////List element prototipe///////////////////////////////*/     
-struct TextureTriangleCoord {
-         int numVertex; // polygons vertex of texture triangle 
-         float xSand[MAX_POINT], yClay[MAX_POINT]; // coordinate vector polygons in triangle 
-         int codeTexture; // code for identify textural type
-	     struct TextureTriangleCoord *PointToNext; //pointer to next element
-         }; 
- 
-struct label_struct {
-		int id;
-		char etichetta[1024];
-		};
+/*///////List element prototipe/////////////////////////////// */
+struct TextureTriangleCoord
+{
+    int numVertex;		// polygons vertex of texture triangle 
+    float xSand[MAX_POINT], yClay[MAX_POINT];	// coordinate vector polygons in triangle 
+    int codeTexture;		// code for identify textural type
+    struct TextureTriangleCoord *PointToNext;	//pointer to next element
+};
+
+struct label_struct
+{
+    int id;
+    char etichetta[1024];
+};
+
 /*
  * function prototype 
  */
 
-struct TextureTriangleCoord *ReadTriangle(int *, char *); //read texture file criteria (es. USDA.DAT)
-int ReadList(struct TextureTriangleCoord *, int *, float, float); //read all list of texture criteria
-int DefineTexture (int numVert, float *xSand, float *yClay, float SandVal, float ClayVal, int codeTxt); /*define texture for each point*/
-void reclassTexture(char *SchemeName, char *result); //reclass texture file with label
+struct TextureTriangleCoord *ReadTriangle(int *, char *);	//read texture file criteria (es. USDA.DAT)
+int ReadList(struct TextureTriangleCoord *, int *, float, float);	//read all list of texture criteria
+int DefineTexture(int numVert, float *xSand, float *yClay, float SandVal, float ClayVal, int codeTxt);	/*define texture for each point */
+void reclassTexture(char *SchemeName, char *result);	//reclass texture file with label
