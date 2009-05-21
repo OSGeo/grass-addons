cacf55bc9e303a6e112f75d4e7cfde56448d9bc7
Anne Ghisla <a.ghisla@gmail.com>
2009-05-21 12:26:04 +0000

    provided an empty window
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@37326 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.autokrige2/v.autokrige2.py
+++ vector/v.autokrige2/v.autokrige2.py
@@ -18,10 +18,47 @@ COPYRIGHT: (C) 2009 by the GRASS Development Team
 
 #list of parameters
 
-#import directives
+#import directives. to be completed.
+import wx
+import sys
 
-#function(s)
-#@TODO
+#classes in alphabetical order
+class KrigingModule(wx.Frame):
+    """
+    Kriging module for GRASS GIS.
+    """
+    def __init__(self, parent, id=wx.ID_ANY, size=None, 
+                 style = wx.DEFAULT_FRAME_STYLE, title=None, log=None):
+        self.parent = parent
+        self.size = size
+        wx.Frame.__init__(self, parent, id, style=style, size=self.size)
+        
+        # setting properties
+        self.SetTitle("Kriging Module")
 
 #main
-#@TODO
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    """if len(argv) != 2:
+        print >> sys.stderr, __doc__
+        sys.exit()"""
+
+    # Command line arguments of the script to be run are preserved by the
+    # hotswap.py wrapper but hotswap.py and its options are removed that
+    # sys.argv looks as if no wrapper was present.
+    #print "argv:", `argv`
+
+    # commmented, as I don't know if the module will need it.
+    ##some applications might require image handlers
+    #wx.InitAllImageHandlers()
+
+    app = wx.PySimpleApp()
+    k = KrigingModule(parent=None, id=wx.ID_ANY, size=(300,300))
+    k.Show()
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
+
