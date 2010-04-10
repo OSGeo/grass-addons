417f8277c496fdc6786bf9fb890a274998576e58
Massimo Di Stefano <epiesasha@me.com>
2010-04-10 10:49:41 +0000

    adjusted indentation, added check for overview files (if no present make it, this avoid a possible crash when ossimplanet tries to build overview itself), fixed units parametres in the zoom-to action.
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@41770 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- ossim_grass/v.planet/v.planet.py
+++ ossim_grass/v.planet/v.planet.py
@@ -107,173 +107,185 @@ import grass.script as grass
 import string
 
 try:
-	from osgeo import osr, ogr, gdal
+    from osgeo import osr, ogr, gdal
 except ImportError:
-	import osr, ogr, gdal
+    import osr, ogr, gdal
 
 from ogrTovrt import ogrvrt, makestile
 
 
 def main():
-	add = flags['a']
-	remove = flags['r']
-	host = options['host']
-	dport = options['dport']
-	pport = options['pport']
-	grassenv = grass.gisenv()
-	mappa = options['map'].replace("@"," ")
-	mappa = mappa.split()
-	nflags = len(filter(None, [add, remove]))
-	if nflags > 1:
-		grass.run_command('g.message' , message = 'Cannot add & remove a map at same time.')
-	if nflags < 1:
-		grass.run_command('g.message' , message = 'No action requested , please choose one from "-a : add" or "-r : remove" flags.')
-	try :
-		vectorpath = os.path.join(grassenv['GISDBASE'], grassenv['LOCATION_NAME'], mappa[1], 'vector' , mappa[0] ) 
-		mapfile = os.path.join(vectorpath, 'head')
-		vrtdir = os.path.join(grassenv['GISDBASE'], grassenv['LOCATION_NAME'], mappa[1], 'vrt', 'vector/')
-	except :
-		vectorpath = os.path.join(grassenv['GISDBASE'], grassenv['LOCATION_NAME'], grassenv['MAPSET'], mappa[0] )
-		mapfile = os.path.join(vectorpath, 'head' )   
-		vrtdir = os.path.join(grassenv['GISDBASE'], grassenv['LOCATION_NAME'], grassenv['MAPSET'], 'vrt', 'vector/')
-	d = os.path.dirname(vrtdir)
-	if not os.path.exists(d):
-		os.makedirs(d)
-	vrtfilename =  mappa[0] + '.vrt'
-	output = os.path.join(vrtdir, vrtfilename)
-	if not os.path.exists(output):
-		ogrvrt(mapfile,output)
-		makestile(output, options['brush'], options['pen'], options['size'], options['fill'], options['thickness'])
-	proj_info = projinfo()
-	unit = proj_info['units']
-	if unit.lower() == 'meters':
-		zoom_position = setCPRJ(options['map'])
-	if unit == 'Degrees':
-		zoom_position = setCLL(options['map'])
-	lat = zoom_position[0]
-	lon = zoom_position[1]
-	distance = zoom_position[2]
-	if add :
-		addzoom(output,lon,lat,distance,host,dport,pport)
-		print 'Added vector file :', mappa[0]
-		print 'Camera positioned to : '
-		print 'Longitude = ',lon
-		print 'Latitude = ', lat
-		print 'Altitude = ' , distance
-	if remove :
-		removefile(output,host,dport)
-		print 'Removed vector file :', mappa[0]
+    add = flags['a']
+    remove = flags['r']
+    host = options['host']
+    dport = options['dport']
+    pport = options['pport']
+    grassenv = grass.gisenv()
+    mappa = options['map'].replace("@"," ")
+    mappa = mappa.split()
+    nflags = len(filter(None, [add, remove]))
+    if nflags > 1:
+        grass.run_command('g.message' , message = 'Cannot add & remove a map at same time.')
+    if nflags < 1:
+        grass.run_command('g.message' , message = 'No action requested , please choose one from "-a : add" or "-r : remove" flags.')
+    try :
+        vectorpath = os.path.join(grassenv['GISDBASE'], grassenv['LOCATION_NAME'], mappa[1], 'vector' , mappa[0] ) 
+        mapfile = os.path.join(vectorpath, 'head')
+        vrtdir = os.path.join(grassenv['GISDBASE'], grassenv['LOCATION_NAME'], mappa[1], 'vrt', 'vector/')
+    except :
+        vectorpath = os.path.join(grassenv['GISDBASE'], grassenv['LOCATION_NAME'], grassenv['MAPSET'], mappa[0] )
+        mapfile = os.path.join(vectorpath, 'head' )   
+        vrtdir = os.path.join(grassenv['GISDBASE'], grassenv['LOCATION_NAME'], grassenv['MAPSET'], 'vrt', 'vector/')
+    d = os.path.dirname(vrtdir)
+    if not os.path.exists(d):
+        os.makedirs(d)
+    vrtfilename =  mappa[0] + '.vrt'
+    output = os.path.join(vrtdir, vrtfilename)
+    if not os.path.exists(output):
+        ogrvrt(mapfile,output)
+        makestile(output, options['brush'], options['pen'], options['size'], options['fill'], options['thickness'])
+    proj_info = projinfo()
+    unit = proj_info['units']
+    if unit.lower() == 'meters':
+        zoom_position = setCPRJ(options['map'])
+    if unit.lower() == 'metres':
+        zoom_position = setCPRJ(options['map'])
+    if unit == 'Degrees':
+        zoom_position = setCLL(options['map'])
+    lat = zoom_position[0]
+    lon = zoom_position[1]
+    distance = zoom_position[2]
+    if add :
+        try:
+            addzoom(output,lon,lat,distance,host,dport,pport)
+            print 'Added vector file :', mappa[0]
+            print 'Camera positioned to : '
+            print 'Longitude = ',lon
+            print 'Latitude = ', lat
+            print 'Altitude = ' , distance
+        except :
+            print "conecction error"
+    if remove :
+        removefile(output,host,dport)
+        print 'Removed vector file :', mappa[0]
 
 
 def addfile(output,host,dport):
-	ossim_data_xml = "<Add target=':idolbridge'><Image groupType='groundTexture'><filename>%s</filename> <id>%s</id><name>%s</name></Image></Add>" % (output,output,output)
-	ossimdata = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	ossimdata.connect((host, int(dport)))
-	ossimdata.send(ossim_data_xml)
-	ossimdata.close()
+    overview = output.replace('.vrt','.ovr')
+    if not os.path.isfile(overview):
+        makeoverview(output)
+    ossim_data_xml = "<Add target=':idolbridge'><Image groupType='groundTexture'><filename>%s</filename> <id>%s</id><name>%s</name></Image></Add>" % (output,output,output)
+    ossimdata = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ossimdata.connect((host, int(dport)))
+    ossimdata.send(ossim_data_xml)
+    ossimdata.close()
 
 
 def zoomto(lon,lat,distance,host,pport):
-	ossim_zoom_xml = '<Set target=":navigator" vref="wgs84"><Camera><longitude>%s</longitude><latitude>%s</latitude><altitude>%s</altitude><heading>0</heading><pitch>0</pitch><roll>0</roll><altitudeMode>absolute</altitudeMode><range>%s</range></Camera></Set>' % (lon, lat, distance, distance)
-	ossimposition = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	ossimposition.connect((host, int(pport)))  
-	ossimposition.send(ossim_zoom_xml)
-	ossimposition.close()
+    ossim_zoom_xml = '<Set target=":navigator" vref="wgs84"><Camera><longitude>%s</longitude><latitude>%s</latitude><altitude>%s</altitude><heading>0</heading><pitch>0</pitch><roll>0</roll><altitudeMode>absolute</altitudeMode><range>%s</range></Camera></Set>' % (lon, lat, distance, distance)
+    ossimposition = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ossimposition.connect((host, int(pport)))  
+    ossimposition.send(ossim_zoom_xml)
+    ossimposition.close()
 
 
 def removefile(output,host,dport):
-	ossimdata = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	ossimdata.connect((host, int(dport)))
-	ossim_data_xml = "<Remove target=':idolbridge' id='%s' />" % (output)
-	ossimdata.send(ossim_data_xml)
-	ossimdata.close()
+    ossimdata = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ossimdata.connect((host, int(dport)))
+    ossim_data_xml = "<Remove target=':idolbridge' id='%s' />" % (output)
+    ossimdata.send(ossim_data_xml)
+    ossimdata.close()
 
 
 def addzoom(output,lon,lat,distance,host,dport,pport):
-	addfile(output,host,dport)
-	zoomto(lon,lat,distance,host,pport)
+    addfile(output,host,dport)
+    zoomto(lon,lat,distance,host,pport)
 
 
 def projinfo():
-	units = grass.read_command("g.proj", flags='p')
-	units = units.replace('-','')
-	units = grass.parse_key_val(units, ':')
-	units_key = units.keys()
-	for i in units_key :
-		key_value = str(units[i]).strip()
-		units[i] = key_value
-	return units
+    units = grass.read_command("g.proj", flags='p')
+    units = units.replace('-','')
+    units = grass.parse_key_val(units, ':')
+    units_key = units.keys()
+    for i in units_key :
+        key_value = str(units[i]).strip()
+        units[i] = key_value
+    return units
 
 
 
 def setCLL(map):
-	center = []
-	info_region = grass.read_command('g.region', flags = 'cael', vect = '%s' % (map)) 
-	dict_region = grass.parse_key_val(info_region, ':')
-	lon = dict_region['east-west center']	
-	lat = dict_region['north-south center']
-	lon = str(lon)
-	lat = str(lat)
-	lon = lon.replace(':', " ")
-	lat = lat.replace(':', " ")
-	if lat[-1] == 'N':
-		signlat = 1
-	if lat[-1] == 'S':
-		signlat = -1
-	if lon[-1] == 'E':
-		signlon = 1
-	if lon[-1] == 'W':
-		signlon = -1
-	lat = lat[:-1] 
-	lon = lon[:-1]
-	lat = [float(i) for i in lat.split()]
-	lon = [float(i) for i in lon.split()]
-	lat = (lat[0] + (lat[1] / 60) + lat[2] / 3600) * float(signlat)
-	lon = (lon[0] + (lon[1] / 60) + lon[2] / 3600) * float(signlon)
-	ns = float(dict_region['north-south extent'])
-	we = float(dict_region['east-west extent'])
-	distance = (ns + we) / 2
-	center.append(lat)
-	center.append(lon)
-	center.append(distance)
-	return center
+    center = []
+    info_region = grass.read_command('g.region', flags = 'cael', vect = '%s' % (map)) 
+    dict_region = grass.parse_key_val(info_region, ':')
+    lon = dict_region['east-west center']	
+    lat = dict_region['north-south center']
+    lon = str(lon)
+    lat = str(lat)
+    lon = lon.replace(':', " ")
+    lat = lat.replace(':', " ")
+    if lat[-1] == 'N':
+        signlat = 1
+    if lat[-1] == 'S':
+        signlat = -1
+    if lon[-1] == 'E':
+        signlon = 1
+    if lon[-1] == 'W':
+        signlon = -1
+    lat = lat[:-1] 
+    lon = lon[:-1]
+    lat = [float(i) for i in lat.split()]
+    lon = [float(i) for i in lon.split()]
+    lat = (lat[0] + (lat[1] / 60) + lat[2] / 3600) * float(signlat)
+    lon = (lon[0] + (lon[1] / 60) + lon[2] / 3600) * float(signlon)
+    ns = float(dict_region['north-south extent'])
+    we = float(dict_region['east-west extent'])
+    distance = (ns + we) / 2
+    center.append(lat)
+    center.append(lon)
+    center.append(distance)
+    return center
 
 
 def setCPRJ(map):
-	center = []
-	info_region = grass.read_command('g.region', flags = 'ael', vect = '%s' % (map)) 
-	dict_region = grass.parse_key_val(info_region, ':')
-	lon = dict_region['center longitude']	
-	lat = dict_region['center latitude']
-	lon = str(lon)
-	lat = str(lat)
-	lon = lon.replace(':', " ")
-	lat = lat.replace(':', " ")
-	if lat[-1] == 'N':
-		signlat = 1
-	if lat[-1] == 'S':
-		signlat = -1
-	if lon[-1] == 'E':
-		signlon = 1
-	if lon[-1] == 'W':
-		signlon = -1
-	lat = lat[:-1] 
-	lon = lon[:-1]
-	lat = [float(i) for i in lat.split()]
-	lon = [float(i) for i in lon.split()]
-	lat = (lat[0] + (lat[1] / 60) + lat[2] / 3600) * float(signlat)
-	lon = (lon[0] + (lon[1] / 60) + lon[2] / 3600) * float(signlon)
-	ns = float(dict_region['north-south extent'])
-	we = float(dict_region['east-west extent'])
-	distance = (ns + we) / 2
-	center.append(lat)
-	center.append(lon)
-	center.append(distance)
-	return center
+    center = []
+    info_region = grass.read_command('g.region', flags = 'ael', vect = '%s' % (map)) 
+    dict_region = grass.parse_key_val(info_region, ':')
+    lon = dict_region['center longitude']	
+    lat = dict_region['center latitude']
+    lon = str(lon)
+    lat = str(lat)
+    lon = lon.replace(':', " ")
+    lat = lat.replace(':', " ")
+    if lat[-1] == 'N':
+        signlat = 1
+    if lat[-1] == 'S':
+        signlat = -1
+    if lon[-1] == 'E':
+        signlon = 1
+    if lon[-1] == 'W':
+        signlon = -1
+    lat = lat[:-1] 
+    lon = lon[:-1]
+    lat = [float(i) for i in lat.split()]
+    lon = [float(i) for i in lon.split()]
+    lat = (lat[0] + (lat[1] / 60) + lat[2] / 3600) * float(signlat)
+    lon = (lon[0] + (lon[1] / 60) + lon[2] / 3600) * float(signlon)
+    ns = float(dict_region['north-south extent'])
+    we = float(dict_region['east-west extent'])
+    distance = (ns + we) / 2
+    center.append(lat)
+    center.append(lon)
+    center.append(distance)
+    return center
+
+def makeoverview(input):
+    os.system("ossim-img2rr %s" % input)
+
 
 
 if __name__ == "__main__":
-	options, flags = grass.parser()
-	sys.exit(main())
+    options, flags = grass.parser()
+    sys.exit(main())
 
 
