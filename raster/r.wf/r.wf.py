24182a815c61b23783fc9255b061ea311c29bb9b
Massimo Di Stefano <epiesasha@me.com>
2010-06-02 23:57:30 +0000

    changed a flag, update description
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@42450 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.wf/r.wf.py
+++ raster/r.wf/r.wf.py
@@ -3,10 +3,12 @@
 #
 # MODULE:       r.wf.py
 #
-# AUTHOR(S):    Massimo Di Stefano, Francesco Di Stefano , Margherita Di Leo 2010-02-7
+# AUTHOR(S):    Massimo Di Stefano, Francesco Di Stefano, Margherita Di Leo 
 #               
 #
-# PURPOSE:      Output a Width function graph 
+# PURPOSE:      The module produces the Width Function of a basin. The Width 
+#               Function W(x) gives the number of the cells in a basin at a 
+#               flow distance x from the outlet (distance-area function)
 #
 # COPYRIGHT:    (c) 2010 by the GRASS Development Team
 #
@@ -28,7 +30,7 @@
 #% type: string
 #% gisprompt: old,raster,raster
 #% key_desc: name
-#% description: Name of raster map 
+#% description: Distance to outlet map (from r.stream.distance) 
 #% required: yes
 #%end
 #%option
@@ -36,9 +38,9 @@
 #% type: string
 #% gisprompt: new_file,file,input
 #% key_desc: image
-#% description: output graph
+#% description: output plot
 #% required: yes
-#%end
+#%END
 
 
 import sys
@@ -48,27 +50,38 @@ import grass.script as grass
 import numpy as np
 
 def main():
-    stats = grass.read_command('r.stats', input = options['map'], fs = 'space', nv = '*', nsteps = '255', flags = 'inc').split('\n')[:-1]
-    res = float(grass.read_command('g.region', rast = options['map'], flags = 'm').strip().split('\n')[4].split('=')[1])
+    stats = grass.read_command('r.stats', input = options['map'], fs = 'space', nv = '*', nsteps = '255', flags = 'Anc').split('\n')[:-1]
+
+    # res = cellsize
+    res = float(grass.read_command('g.region', rast = options['map'], flags = 'm').strip().split('\n')[4].split('=')[1])    
     zn = np.zeros((len(stats),4),float)
     kl = np.zeros((len(stats),2),float)
     prc = np.zeros((9,2),float)
-    
+
     for i in range(len(stats)):
         if i == 0:
-            zn[i,0], zn[i, 1] = map(float, stats[i].split(' '))
+            zn[i,0],  zn[i,1] = map(float, stats[i].split(' '))
             zn[i,1] = zn[i,1]
             zn[i,2] = zn[i,1] * res
         if i != 0:
-            zn[i,0], zn[i, 1] = map(float, stats[i].split(' '))
-            zn[i,2] = zn[i,1] + zn[i-1,2]
+            zn[i,0],  zn[i,1] = map(float, stats[i].split(' '))
+            zn[i,2] = zn[i,1] + zn[i-1,2] 
             zn[i,3] = zn[i,1] * (res**2)
+            
     totcell = sum(zn[:,1])
+    print "Tot. cells", totcell
+    totarea = totcell * (res**2)
+    print "Tot. area", totarea
+    maxdist = max(zn[:,0])
+    print "Max distance", maxdist
+ 
     for i in range(len(stats)):
-        kl[i,0] = zn[i,0]
-        kl[i,1] = 1 - (zn[i,2] / totcell)
-    prc[0,0] , prc[0,1] = findint(kl,0.05) , 0.05
-    prc[1,0] , prc[1,1] = findint(kl,0.15) , 0.15
+        kl[i,0] = zn[i,0] 
+        kl[i,1] = zn[i,2] / totcell 
+
+    # quantiles
+    prc[0,0] , prc[0,1] = findint(kl,0.05) , 0.05   
+    prc[1,0] , prc[1,1] = findint(kl,0.15) , 0.15    
     prc[2,0] , prc[2,1] = findint(kl,0.3) , 0.3
     prc[3,0] , prc[3,1] = findint(kl,0.4) , 0.4
     prc[4,0] , prc[4,1] = findint(kl,0.5) , 0.5
@@ -76,9 +89,24 @@ def main():
     prc[6,0] , prc[6,1] = findint(kl,0.7) , 0.7
     prc[7,0] , prc[7,1] = findint(kl,0.85) , 0.85
     prc[8,0] , prc[8,1] = findint(kl,0.95) , 0.95
-    plotImage(zn[:,0], zn[:,3],options['image']+'_width_function.png','-','L [m]','A [m^2]','Width Function')
-    print prc
+ 
+    # plot
+    plotImage(zn[:,0], zn[:,3], options['image']+'_width_function.png','-','x','W(x)','Width Function')
 
+    print "==========================="
+    print "Whidth Function | quantiles"
+    print "==========================="
+    print '%.0f' %findint(kl,0.05) , "|", 0.05
+    print '%.0f' %findint(kl,0.15) , "|", 0.15
+    print '%.0f' %findint(kl,0.3) , "|", 0.3
+    print '%.0f' %findint(kl,0.4) , "|", 0.4
+    print '%.0f' %findint(kl,0.5) , "|", 0.5
+    print '%.0f' %findint(kl,0.6) , "|", 0.6
+    print '%.0f' %findint(kl,0.7) , "|", 0.7
+    print '%.0f' %findint(kl,0.85) , "|", 0.85
+    print '%.0f' %findint(kl,0.95) , "|", 0.95
+    print '\n'
+    print 'Done!'
 
 def plotImage(x,y,image,type,xlabel,ylabel,title):
     plt.plot(x, y, type)
