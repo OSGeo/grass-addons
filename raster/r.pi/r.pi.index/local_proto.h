5b11eb06d112832120fba083911ffbadeffbd8ae
Martin Wegmann <martin.wegmann@uni-wuerzburg.de>
2010-07-29 16:21:45 +0000

    initial commit: r.pi.index provides basic patch based spatial indices like area, shape, euclidean distance to nearest neighbour etc.
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@42949 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ raster/r.pi/r.pi.index/local_proto.h
@@ -0,0 +1,30 @@
+#ifndef LOCAL_PROTO_H
+#define LOCAL_PROTO_H
+
+#define MAX_DOUBLE 1000000.0
+
+typedef struct {
+		int x, y;
+		int neighbors;
+	} Coords;
+
+extern void writeFrag(int row, int col, int nbr_cnt);
+
+extern int f_area(DCELL *vals, Coords **frags, int);
+extern int f_perim(DCELL *vals, Coords **frags, int);
+extern int f_shapeindex(DCELL*, Coords**, int);
+extern int f_borderindex(DCELL*, Coords**, int);
+extern int f_compactness(DCELL*, Coords**, int);
+extern int f_asymmetry(DCELL*, Coords**, int);
+extern int f_area_perim_ratio(DCELL*, Coords**, int);
+extern int f_frac_dim(DCELL*, Coords**, int);
+extern int f_nearest_dist(DCELL*, Coords**, int);
+
+/* global variables */
+int nrows, ncols;
+Coords ** fragments;
+int *flagbuf;
+Coords * actpos;
+int verbose;
+
+#endif /* LOCAL_PROTO_H */
