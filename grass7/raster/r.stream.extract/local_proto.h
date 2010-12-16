bc5615a9f356da79d8f3ac9b6c228ff71e833249
Markus Metz <markus.metz.giswork@gmail.com>
2010-12-16 13:55:32 +0000

    r.stream.extract for 7 is coming
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@44614 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass7/raster/r.stream.extract/local_proto.h
@@ -0,0 +1,80 @@
+
+#ifndef __LOCAL_PROTO_H__
+#define __LOCAL_PROTO_H__
+
+#include "flag.h"
+#include "rbtree.h"
+
+#define INDEX(r, c) ((r) * ncols + (c))
+#define MAXDEPTH 1000     /* maximum supported tree depth of stream network */
+
+struct ddir
+{
+    int pos;
+    int dir;
+};
+
+struct point
+{
+    int r, c;
+};
+
+struct snode
+{
+    int r, c;
+    int id;
+    int n_trib;           /* number of tributaries */
+    int n_trib_total;     /* number of all upstream stream segments */
+    int n_alloc;          /* n allocated tributaries */
+    int *trib;
+    double *acc;
+} *stream_node;
+
+/* global variables */
+extern int nrows, ncols;
+extern unsigned int *astar_pts;
+extern unsigned int n_search_points, n_points, nxt_avail_pt;
+extern unsigned int heap_size, *astar_added;
+extern unsigned int n_stream_nodes, n_alloc_nodes;
+extern struct point *outlets;
+extern unsigned int n_outlets, n_alloc_outlets;
+extern DCELL *acc;
+extern CELL *ele;
+extern char *asp;
+extern CELL *stream;
+extern FLAG *worked, *in_list;
+extern char drain[3][3];
+extern unsigned int first_cum;
+extern char sides;
+extern int c_fac;
+extern int ele_scale;
+extern int have_depressions;
+extern struct RB_TREE *draintree;
+
+/* load.c */
+int load_maps(int, int, int);
+
+/* do_astar.c */
+int do_astar(void);
+unsigned int heap_add(int, int, CELL, char);
+
+/* streams.c */
+int do_accum(double);
+int extract_streams(double, double, int);
+
+/* thin.c */
+int thin_streams(void);
+
+/* basins.c */
+int basin_borders(void);
+
+/* del_streams.c */
+int del_streams(int);
+int seg_length(int, unsigned int *);
+int del_stream_seg(int);
+int update_stream_id(int, int);
+
+/* close.c */
+int close_maps(char *, char *, char *);
+
+#endif /* __LOCAL_PROTO_H__ */
