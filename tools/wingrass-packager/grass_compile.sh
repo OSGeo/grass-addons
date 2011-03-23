2b55a9f2ddf9978c1add86233728db47d5665438
Martin Landa <landa.martin@gmail.com>
2011-03-23 16:27:21 +0000

    winGRASS: include also ps.map's GUI from add-ons
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@45738 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/wingrass-packager/grass_compile.sh
+++ tools/wingrass-packager/grass_compile.sh
@@ -9,6 +9,15 @@ function update {
     svn up || (svn cleanup && svn up)
 }
 
+function update_wxgui_psmap {
+    psmap_dir="$SRC/grass_addons/gui/wxpython/psmap"
+    cd $SRC/$1/gui
+    cp $psmap_dir/docs/*.html      wxpython/docs/
+    cp $psmap_dir/gui_modules/*.py wxpython/gui_modules/
+    cp $psmap_dir/images/*.png     images/
+    cp $psmap_dir/xml/*.xml        wxpython/xml/
+}
+
 function compile {
     echo "Compiling $1..."
     cd $SRC/$1
@@ -20,6 +29,10 @@ function compile {
 export PATH=$PATH:/c/OSGeo4W/apps/msys/bin
 
 update grass_addons
+
+update_wxgui_psmap grass64_release
+update_wxgui_psmap grass6_devel
+update_wxgui_psmap grass_trunk
 
 compile grass64_release
 compile grass6_devel
