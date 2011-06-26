69564a9e42d9a4824dfd060e696aec4e067f8a38
Martin Landa <landa.martin@gmail.com>
2011-06-26 16:29:22 +0000

    wx.stream: cosmetics in template
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@46790 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/gui/wxpython/wx.stream/gui_modules/rstream.py
+++ grass7/gui/wxpython/wx.stream/gui_modules/rstream.py
@@ -1,3 +1,20 @@
+"""!
+@package rstream.py
+
+@brief GUI for r.stream.* modules
+
+See http://grass.osgeo.org/wiki/Wx.stream_GSoC_2011
+
+Classes:
+ - RStreamFrame
+
+(C) 2011 by Margherita Di Leo, and the GRASS Development Team
+This program is free software under the GNU General Public License
+(>=v2). Read the file COPYING that comes with GRASS for details.
+
+@author Margherita Di Leo (GSoC student 2011)
+"""
+
 import os
 import sys
 
@@ -9,8 +26,8 @@ import wx
 
 class RStreamFrame(wx.Frame):
     def __init__(self, parent = None, id = wx.ID_ANY,
-                 title = _("GRASS GIS r.stream Utility"), **kwargs):
-        """!Main window of r.stream GUI
+                 title = _("GRASS GIS Hydrological Modelling Utility"), **kwargs):
+        """!Main window of r.stream's GUI
         
         @param parent parent window
         @param id window id
