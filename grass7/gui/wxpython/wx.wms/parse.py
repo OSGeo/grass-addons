55a3aa6c112c510263a9e138d29373f06b087cb8
Sudeep Singh Walia <sudeep495@gmail.com>
2011-06-28 08:47:04 +0000

    Code for parsing the xml response of the GetCapabilites, current version supports only command line interface, run as  python parse.py
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@46818 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/gui/wxpython/wx.wms/parse.py
+++ grass7/gui/wxpython/wx.wms/parse.py
@@ -13,33 +13,7 @@ def parsexml():
     	namelist = node.getElementsByTagName('Name')
     	for name in namelist:
     		print name.toxml()
-       	'''temp = node.getElementsByTagName('Name')
-    	if hasattr(temp,'toxml'):
-    		temp.toxml()
-    		print 'yoyo'
-    	else:
-    		print 'dkndskjfnskdjnksdjfn'
- '''
-
-
-    #print layerlist
-    '''forecasts = []
-    for node in dom.getElementsByTagNameNS(WEATHER_NS, 'forecast'):
-        forecasts.append({
-            'date': node.getAttribute('date'),
-            'low': node.getAttribute('low'),
-            'high': node.getAttribute('high'),
-            'condition': node.getAttribute('text')
-        })
-    ycondition = dom.getElementsByTagNameNS(WEATHER_NS, 'condition')[0]
-    return {
-        'current_condition': ycondition.getAttribute('text'),
-        'current_temp': ycondition.getAttribute('temp'),
-        'forecasts': forecasts,
-        'title': dom.getElementsByTagName('title')[0].firstChild.data
-    }'''
-    
-
+       
 
 parsexml()
 
