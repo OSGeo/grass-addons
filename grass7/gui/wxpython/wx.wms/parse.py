c4248eb395808a8dee6c4bc8ff24713361b0356b
Sudeep Singh Walia <sudeep495@gmail.com>
2011-07-11 00:22:46 +0000

    Major Change in the Backend Code of GUI, now Auto Generated by wxGlade, removed redundant prints from parse.py
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@47092 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/gui/wxpython/wx.wms/parse.py
+++ grass7/gui/wxpython/wx.wms/parse.py
@@ -8,6 +8,9 @@ def parsexml(xml):
  soup = BeautifulSoup(xmltext)
  #layers = soup.findAll('layer', queryable="1")
  layers = soup.findAll('layer')
+ '''
+ for attr, value in soup.find('layer').attrs:
+ 	print attr, "       =          ", value '''
  namelist = []
  for layer in layers:
 	soupname = BeautifulSoup(str(layer))
@@ -15,5 +18,8 @@ def parsexml(xml):
 	if len(names) > 0:
 		namelist += names[0]
  return namelist
-	
-
+'''
+f = open('wmsmaris.xml','r')
+a=f.read()
+print a
+#parsexml(a) '''
