7adef4f883c3f4b46de0e4e8c67ce4a96fc23e23
Martin Landa <landa.martin@gmail.com>
2012-01-25 20:55:03 +0000

    winGRASS support scripts: update also VERSION_NUMBER and GRASS_BASE
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@50454 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/wingrass-packager/grass_svn_info.sh
+++ tools/wingrass-packager/grass_svn_info.sh
@@ -14,16 +14,19 @@ function update {
     read MAJOR <&3 
     read MINOR <&3 
     read PATCH <&3 
+    VERSION=$MAJOR.$MINOR.$PATCH
+    
     if [[ "$PATCH" == *svn* ]] ; then
 	TYPE="Devel"
     else
 	TYPE="Release"
     fi
-    sed "s/SVN_REVISION \"36599\"/SVN_REVISION \"$REV\"/g" $HOME/$2/GRASS-Installer.nsi > tmp1
-    sed "s/BINARY_REVISION \"1\"/BINARY_REVISION \"$NUM\"/g" tmp1 > tmp2
-    sed "s/INSTALLER_TYPE \"Devel\"/INSTALLER_TYPE \"$TYPE\"/g" tmp2 > tmp3
-    rm tmp1 tmp2
-    mv tmp3 $HOME/$2/GRASS-Installer.nsi
+    sed -e "s/SVN_REVISION \"36599\"/SVN_REVISION \"$REV\"/g" \
+	-e "s/BINARY_REVISION \"1\"/BINARY_REVISION \"$NUM\"/g" \
+	-e "s/INSTALLER_TYPE \"Devel\"/INSTALLER_TYPE \"$TYPE\"/g" \
+	-e "s/VERSION_NUMBER \".*\"/VERSION_NUMBER \"$VERSION\"/g" \
+	-e "s/GRASS_BASE \"GRASS .*\"/GRASS_BASE \"GRASS $VERSION\"/g" $HOME/$2/GRASS-Installer.nsi > tmp
+    mv tmp $HOME/$2/GRASS-Installer.nsi
 }
 
 export PATH=$PATH:/c/OSGeo4W/apps/msys/bin
