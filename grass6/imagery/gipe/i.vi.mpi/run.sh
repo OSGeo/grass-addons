be6985a3c787916ab7a83f9d2c4c382093938bb6
Markus Neteler <neteler@gmail.com>
2012-03-12 16:16:48 +0000

    cosmetics
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@51050 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass6/imagery/gipe/i.vi.mpi/run.sh
+++ grass6/imagery/gipe/i.vi.mpi/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-#foo will be the number of processors available in teh Cluster
+# foo will be the number of processors available in the Cluster
 foo=3
-		filename=ndvi1-new-$foo
-		 time mpirun -np $foo /home/shamim/grass-6.0.2/dist.i686-pc-linux-gnu/bin/r.vi.mpi viname=ndvi red=p126r050_7t20001231_z48_nn30.tif nir=p126r050_7t20001231_z48_nn40.tif vi=$filename tmp=1
+filename=ndvi1-new-$foo
+
+time mpirun -np $foo r.vi.mpi viname=ndvi red=p126r050_7t20001231_z48_nn30.tif nir=p126r050_7t20001231_z48_nn40.tif vi=$filename tmp=1
+
 exit 0
