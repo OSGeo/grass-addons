f43620840848950390a8d1efa420953996478676
Markus Neteler <neteler@gmail.com>
2012-03-12 16:18:12 +0000

    cosmetics
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@51051 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass6/imagery/gipe/i.vi.mpi/run.sh
+++ grass6/imagery/gipe/i.vi.mpi/run.sh
@@ -4,6 +4,6 @@
 foo=3
 filename=ndvi1-new-$foo
 
-time mpirun -np $foo r.vi.mpi viname=ndvi red=p126r050_7t20001231_z48_nn30.tif nir=p126r050_7t20001231_z48_nn40.tif vi=$filename tmp=1
+time mpirun -np $foo i.vi.mpi viname=ndvi red=p126r050_7t20001231_z48_nn30.tif nir=p126r050_7t20001231_z48_nn40.tif vi=$filename tmp=1
 
 exit 0
