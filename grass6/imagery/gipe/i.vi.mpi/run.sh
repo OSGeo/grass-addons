f6903d23029e3a4364cb9d8a7941d4e399a1103f
Yann Chemin <yann.chemin@gmail.com>
2012-03-15 08:47:14 +0000

    moved i.evapo.senay to add-ons/grass7 and removed i.vi.mpi from add-ons/grass6 as it is now in add-ons/grass7
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@51064 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass6/imagery/gipe/i.vi.mpi/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# foo will be the number of processors available in the Cluster
-foo=3
-filename=ndvi1-new-$foo
-
-time mpirun -np $foo i.vi.mpi viname=ndvi red=p126r050_7t20001231_z48_nn30.tif nir=p126r050_7t20001231_z48_nn40.tif vi=$filename tmp=1
-
-exit 0
