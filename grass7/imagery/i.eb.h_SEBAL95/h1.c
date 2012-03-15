ba8250ccbd35ab17ae56ae3e62df1d2d5f7977be
Yann Chemin <yann.chemin@gmail.com>
2012-03-15 09:48:13 +0000

    more maintenance on gipe, upgrade some modules to G7 addons
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@51066 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass7/imagery/i.eb.h_SEBAL95/h1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+#include"functions.h"
+
+double h1(double roh_air, double rah, double dtair)
+{
+    double result;
+
+    result = roh_air * 1004 * dtair / rah;
+
+    return result;
+}
