4d8ecba6165cb41bbd4bcf7aa245dcb1cffd4cd2
Hamish Bowman <hamish_b@yahoo.com>
2012-05-20 20:59:57 +0000

    typo
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@51658 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass6/raster/r.to.vect.lines/r.to.vect.lines.py
+++ grass6/raster/r.to.vect.lines/r.to.vect.lines.py
@@ -95,7 +95,7 @@ def main():
     elif data_type == FCELL_TYPE:
         ptype = POINTER(c_float)
         type_name = 'FCELL'
-    elif data_type == DCELL_TYPR:
+    elif data_type == DCELL_TYPE:
         ptype = POINTER(c_double)
         type_name = 'DCELL'
 
