2ffd62519e8aecab1fcecdf26fb177f92f3b939b
Markus Neteler <neteler@gmail.com>
2012-05-24 11:11:42 +0000

    fix install path (from r.modis)
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@51701 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/general/g.cloud/g.cloud.py
+++ grass7/general/g.cloud/g.cloud.py
@@ -111,8 +111,14 @@ import os, sys, pexpect, tarfile, ast, tempfile, getpass, itertools, collections
 from types import *
 import grass.script as grass
 
+# add the folder containing libraries to python path
+if os.path.isdir(os.path.join(os.getenv('GISBASE'), 'etc', 'g.cloud', os.sep)):
+    cloudpath = os.path.join(os.getenv('GISBASE'), 'etc', 'g.cloud')
+elif os.path.isdir(os.path.join(os.getenv('GRASS_ADDON_PATH'), 'etc', 'g.cloud', os.sep)):
+    cloudpath = os.path.join(os.getenv('GRASS_ADDON_PATH'), 'etc', 'g.cloud')
+
+# search python environment
 python = os.getenv('GRASS_PYTHON', 'python')
-cloudpath = os.path.join(os.getenv('GISBASE'), 'etc', 'g.cloud')
 sys.path.append(cloudpath)
 
 try:
