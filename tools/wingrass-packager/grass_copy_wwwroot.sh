0be5ca2d8b68f12b9872deddc113d40bcca8d4d3
Martin Landa <landa.martin@gmail.com>
2012-07-02 16:01:16 +0000

    wingrass: copy also addons
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@52283 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/wingrass-packager/grass_copy_wwwroot.sh
+++ tools/wingrass-packager/grass_copy_wwwroot.sh
@@ -7,16 +7,22 @@ HOME=/c/Users/landa/grass_packager
 function copy {
     cd $HOME/grass$1
 
+    echo "Copying grass$1..."
+    
     rm -rf $WWWDIR/grass$1
     mkdir $WWWDIR/grass$1
     
-    cp WinGRASS*.exe* $WWWDIR/grass$1
+    cp    WinGRASS*.exe* $WWWDIR/grass$1
     
     mkdir $WWWDIR/grass$1/osgeo4w
-    cp grass*.tar.bz2 $WWWDIR/grass$1/osgeo4w
+    cp    grass*.tar.bz2 $WWWDIR/grass$1/osgeo4w
     
     mkdir $WWWDIR/grass$1/logs
-    cp -r log-r*      $WWWDIR/grass$1/logs
+    cp -r log-r*         $WWWDIR/grass$1/logs
+
+    mkdir $WWWDIR/grass$1/addons
+    cp    addons/*zip addons/*.md5sum $WWWDIR/grass$1/addons
+    cp -r addons/logs                 $WWWDIR/grass$1/addons
 }
 
 export PATH=$PATH:/c/OSGeo4W/apps/msys/bin
