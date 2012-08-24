c10fc912d2fc6caafed9d20826d3e73ca10ec27c
Markus Metz <markus.metz.giswork@gmail.com>
2012-08-24 17:55:38 +0000

    new module r.sun.angle
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@52876 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass6/raster/r.sun.angle/global.h
@@ -0,0 +1,12 @@
+#ifdef MAIN
+#define GLOBAL
+#else
+#define GLOBAL extern
+#endif
+
+GLOBAL float asol, phi0, sun_zenith, sun_azimuth;	/* from nadir, from north */
+GLOBAL int sunset;
+
+/* proto */
+long calc_solar_position(double, double, double, int, int, int, int, int,
+			 int);
