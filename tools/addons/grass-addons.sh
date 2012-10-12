2a1915f9223c91d8d7b9a0a2c111ea89ba839fb1
Martin Landa <landa.martin@gmail.com>
2012-10-12 10:05:46 +0000

    addons maintance scripts: add cronjob script, now the collection should complete
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@53372 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ tools/addons/grass-addons.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+DIR=$HOME/src
+
+build_addons() {
+cd $DIR/grass-addons/ 
+
+revl=`svn info | grep 'Revision' | cut -d':' -f2 | tr -d ' '`
+revr=`svn info -rHEAD | grep 'Revision' | cut -d':' -f2 | tr -d ' '`
+
+echo "$revl X $revr"
+
+if [ "$revl" != "$revr" ] || [ "$1" = "f" ] ; then
+    svn up || (svn cleanup && svn up)
+
+    cd tools/addons/ 
+    ./compile-xml.sh
+fi
+}
+
+recompile_grass() {
+    cd $DIR
+
+    for dir in "grass_trunk" "grass6_devel" "grass64_release" ; do
+	cd $dir
+	svn up
+	make distclean
+	if [ $dir == "grass_trunk" ] ; then 
+	    num=7
+	else
+	    num=6
+	fi
+	../configures.sh grass$num
+	make
+	cd ..
+    done
+}
+
+#recompile_grass
+build_addons $1
+
+exit 0
