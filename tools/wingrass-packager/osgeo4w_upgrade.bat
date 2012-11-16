d73a02249f5862e19a730125d6a813887e2fe70c
Martin Landa <landa.martin@gmail.com>
2012-11-16 10:52:10 +0000

    wingrass: update also other osgeo4w installations (G6, G7, and dev)
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@53844 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/wingrass-packager/osgeo4w_upgrade.bat
+++ tools/wingrass-packager/osgeo4w_upgrade.bat
@@ -1,8 +1,32 @@
 @echo off
 
-rem Upgrade OSGeo4W Dev installation
+rem Upgrade OSGeo4W installations
 
-set PATH=C:\OSGeo4W_dev\bin;%PATH%
+set PATH_ORIG=%PATH%
+
+rem
+rem OSGeo4W (GRASS 6)
+rem
+set PATH=C:\OSGeo4W\bin;%PATH_ORIG%
+call o4w_env.bat
+
+apt update
+apt upgrade
+
+rem
+rem OSGeo4W (GRASS 7)
+rem
+set PATH=C:\OSGeo4W_g7\bin;%PATH_ORIG%
+call o4w_env.bat
+
+apt update
+apt upgrade
+
+rem
+rem OSGeo4W (Dev)
+rem
+
+set PATH=C:\OSGeo4W_dev\bin;%PATH_ORIG%
 call o4w_env.bat
 
 apt update
