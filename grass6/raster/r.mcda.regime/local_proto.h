fff760c29b937f9d8b88c853003f5d0d23a78d2a
Gianluca Massei <g_massa@libero.it>
2012-11-19 13:36:49 +0000

    git-svn-id: https://svn.osgeo.org/grass/grass-addons@53909 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass6/raster/r.mcda.regime/local_proto.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <grass/gis.h>
+#include <grass/glocale.h>
+#include <grass/config.h>
+
+
+struct input
+{
+    char *name, *mapset;	/* input raster name  and mapset name */
+    int infd;
+    void *inrast;		/* input buffer */
+};
