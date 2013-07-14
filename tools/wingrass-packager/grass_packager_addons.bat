39182371e9e637c2b8675823f2d0e4a34eb79071
Martin Landa <landa.martin@gmail.com>
2013-07-14 09:58:54 +0000

    wingrass: clean up scripts + add script for addons
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@57107 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ tools/wingrass-packager/grass_packager_addons.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd C:\Users\landa\grass_packager
+
+C:\OSGeo4W\apps\msys\bin\sh.exe .\grass_addons.sh
+
+C:\OSGeo4W\apps\msys\bin\sh.exe .\grass_copy_wwwroot.sh
--- tools/wingrass-packager/grass_packager_release.bat
+++ tools/wingrass-packager/grass_packager_release.bat
@@ -36,7 +36,3 @@ C:\OSGeo4W\apps\msys\bin\sh.exe .\grass_md5sum.sh %MAJOR%%MINOR%%PATCH%
 C:\OSGeo4W\apps\msys\bin\sh.exe .\grass_addons.sh %MAJOR%%MINOR%%PATCH%
 
 C:\OSGeo4W\apps\msys\bin\sh.exe .\grass_copy_wwwroot.sh %MAJOR%%MINOR%%PATCH% %MAJOR%.%MINOR%.%PATCH% 
-
-rem pscp.exe -r -i .\ssh\id_dsa.ppk .\grass%MAJOR%%MINOR%%PATCH%\addons\*.zip    landa@geo102:/work/wingrass/grass%MAJOR%%MINOR%/addons/grass-%MAJOR%.%MINOR%.%PATCH%
-rem pscp.exe -r -i .\ssh\id_dsa.ppk .\grass%MAJOR%%MINOR%%PATCH%\addons\*.md5sum landa@geo102:/work/wingrass/grass%MAJOR%%MINOR%/addons/grass-%MAJOR%.%MINOR%.%PATCH%
-rem pscp.exe -r -i .\ssh\id_dsa.ppk .\grass%MAJOR%%MINOR%%PATCH%\addons\logs     landa@geo102:/work/wingrass/grass%MAJOR%%MINOR%/addons/grass-%MAJOR%.%MINOR%.%PATCH%
