88bd5345e51f6ff3364a53cdf1820510203b47b9
Markus Metz <markus.metz.giswork@gmail.com>
2013-09-22 14:40:29 +0000

    add v.centerpoint
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@57812 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass7/vector/v.centerpoint/areas.c
@@ -0,0 +1,574 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <grass/gis.h>
+#include <grass/vector.h>
+#include <grass/glocale.h>
+#include "local_proto.h"
+
+static double d_ulp(double d)
+{
+    int exp;
+
+    if (d == 0)
+	return GRASS_EPSILON;
+
+    d = frexp(d, &exp);
+    exp -= 51;
+    d = ldexp(d, exp);
+
+    return d;
+}
+
+int areas_center(struct Map_info *In, struct Map_info *Out, int layer, 
+                 struct cat_list *cat_list, int mode)
+{
+    int area, nareas, isle, nisles, nisles_alloc;
+    struct line_pnts *Points, **IPoints, *OPoints;
+    struct line_cats *Cats;
+    struct bound_box box;
+    double x, y, z, meanx, meany, meanz;
+    double *xp, *yp;
+    double w, tot_w;
+    int i;
+    
+    Points = Vect_new_line_struct();
+    OPoints = Vect_new_line_struct();
+    Cats = Vect_new_cats_struct();
+    
+    nisles_alloc = 10;
+    IPoints = G_malloc(nisles_alloc * sizeof (struct line_pnts *));
+    
+    nisles = nisles_alloc;
+    for (isle = 0; isle < nisles_alloc; isle++) {
+	IPoints[isle] = Vect_new_line_struct();
+    } 
+
+    nareas = Vect_get_num_areas(In);
+    
+    /* geometric mean = center of gravity */
+    if (mode & A_MEAN) {
+
+	G_message(_("Calculating centers of gravity for areas..."));
+
+	G_percent(0, nareas, 4);
+
+	for (area = 1; area <= nareas; area++) {
+
+	    Vect_reset_cats(Cats);
+	    if (layer > 1) {
+		Vect_get_area_cats(In, area, Cats);
+	        if (!Vect_cats_in_constraint(Cats, layer, cat_list))
+		    continue;
+	    }
+	    
+	    Vect_get_area_points(In, area, Points);
+	    Vect_line_prune(Points);
+	    
+	    nisles = Vect_get_area_num_isles(In, area);
+	    if (nisles > nisles_alloc) {
+		IPoints = G_realloc(IPoints, nisles * sizeof (struct line_pnts *));
+		for (isle = nisles_alloc; isle < nisles; isle++) {
+		    IPoints[isle] = Vect_new_line_struct();
+		}
+		nisles_alloc = nisles;
+	    }
+	    for (isle = 1; isle < nisles; isle++) {
+		Vect_get_isle_points(In, isle, IPoints[isle - 1]);
+		Vect_line_prune(IPoints[isle - 1]);
+	    }
+
+	    /* surveyor's / shoelace formula */
+	    /* the surveyor should not be too far away (fp precision limit) */
+	    /* surveyor's position: */
+	    Vect_line_box(Points, &box);
+	    x = (box.W + box.E) / 2.;
+	    y = (box.S + box.N) / 2.;
+	    z = 0;
+	    meanx = meany = meanz = 0.;
+	    tot_w = 0.;
+
+	    xp = Points->x;
+	    yp = Points->y;
+	    for (i = 1; i < Points->n_points; i++) {
+		w = (x - xp[i - 1]) * (yp[i] - y) - 
+		    (x - xp[i]) * (yp[i - 1] - y);
+
+		meanx += (xp[i - 1] + xp[i] + x) * w;
+		meany += (yp[i - 1] + yp[i] + y) * w;
+		tot_w += w;
+	    }
+
+	    for (isle = 0; isle < nisles; isle++) {
+		xp = IPoints[isle]->x;
+		yp = IPoints[isle]->y;
+		for (i = 1; i < IPoints[isle]->n_points; i++) {
+		    w = (x - xp[i - 1]) * (yp[i] - y) - 
+			(x - xp[i]) * (yp[i - 1] - y);
+
+		    meanx += (xp[i - 1] + xp[i] + x) * w;
+		    meany += (yp[i - 1] + yp[i] + y) * w;
+		    tot_w += w;
+		}
+	    }
+	    if (tot_w != 0) {
+		x = meanx / (tot_w * 3);
+		y = meany / (tot_w * 3);
+	    }
+	    if (Out) {
+		Vect_reset_line(OPoints);
+		Vect_append_point(OPoints, x, y, z);
+		Vect_write_line(Out, GV_POINT, OPoints, Cats);
+	    }
+	    else
+		fprintf(stdout, "7|%.15g|%.15g|%.15g\n", x, y, z);
+
+	    G_percent(area, nareas, 4);
+	}
+    }
+
+    /* approximate geometric median with Weiszfeld's algorithm:
+     * iterative least squares reduction */
+    if (mode & A_MEDIAN) {
+	struct line_pnts *CPoints;
+	double sx, sy, cx, cy, dx, dy;
+	double medx, medy;
+	double *wc;
+	int nwc, nwc_alloc, wci;
+	double dist, distsum, dist2all, lastdist2all;
+	int j, iter, maxiter = 1000;
+
+	G_message(_("Calculating geometric medians for areas..."));
+	
+	CPoints = Vect_new_line_struct();
+	
+	nwc_alloc = 100;
+	wc = G_malloc(nwc_alloc * sizeof(double));
+	
+	G_percent(0, nareas, 4);
+
+	for (area = 1; area <= nareas; area++) {
+
+	    Vect_reset_cats(Cats);
+	    if (layer > 1) {
+		Vect_get_area_cats(In, area, Cats);
+	        if (!Vect_cats_in_constraint(Cats, layer, cat_list))
+		    continue;
+	    }
+
+	    Vect_get_area_points(In, area, Points);
+	    Vect_line_prune(Points);
+	    nwc = Points->n_points - 1;
+	    
+	    nisles = Vect_get_area_num_isles(In, area);
+	    if (nisles > nisles_alloc) {
+		IPoints = G_realloc(IPoints, nisles * sizeof (struct line_pnts *));
+		for (isle = nisles_alloc; isle < nisles; isle++) {
+		    IPoints[isle] = Vect_new_line_struct();
+		}
+		nisles_alloc = nisles;
+	    }
+	    for (isle = 1; isle < nisles; isle++) {
+		Vect_get_isle_points(In, isle, IPoints[isle - 1]);
+		Vect_line_prune(IPoints[isle - 1]);
+		nwc += IPoints[isle - 1]->n_points - 1;
+	    }
+
+	    if (nwc_alloc < nwc) {
+		nwc_alloc = nwc;
+		wc = G_realloc(wc, nwc_alloc * sizeof(double));
+	    }
+		
+	    /* surveyor's / shoelace formula */
+	    /* the surveyor should not be too far away (fp precision limit) */
+	    /* surveyor's position: */
+	    Vect_line_box(Points, &box);
+	    sx = (box.W + box.E) / 2.;
+	    sy = (box.S + box.N) / 2.;
+	    x = y = z = 0;
+	    meanx = meany = meanz = 0.;
+	    tot_w = 0.;
+
+	    wci = 0;
+	    Vect_reset_line(CPoints);
+	    xp = Points->x;
+	    yp = Points->y;
+	    for (i = 1; i < Points->n_points; i++) {
+		w = (sx - xp[i - 1]) * (yp[i] - sy) - 
+		    (sx - xp[i]) * (yp[i - 1] - sy);
+		
+		tot_w += w;
+
+		cx = xp[i - 1] + xp[i] + sx;
+		cy = yp[i - 1] + yp[i] + sy;
+		meanx += cx * w;
+		meany += cy * w;
+
+		wc[wci++] = w;
+		Vect_append_point(CPoints, cx / 3., cy / 3., 0);
+	    }
+
+	    for (isle = 0; isle < nisles; isle++) {
+		xp = IPoints[isle]->x;
+		yp = IPoints[isle]->y;
+		for (i = 1; i < IPoints[isle]->n_points; i++) {
+		    w = (sx - xp[i - 1]) * (yp[i] - sy) - 
+			(sx - xp[i]) * (yp[i - 1] - sy);
+
+		    tot_w += w;
+
+		    cx = xp[i - 1] + xp[i] + sx;
+		    cy = yp[i - 1] + yp[i] + sy;
+		    meanx += cx * w;
+		    meany += cy * w;
+
+		    wc[wci++] = w;
+		    Vect_append_point(CPoints, cx / 3., cy / 3., 0);
+		}
+	    }
+	    if (tot_w != 0) {
+		x = meanx / (tot_w * 3);
+		y = meany / (tot_w * 3);
+	    }
+
+	    medx = x;
+	    medy = y;
+
+	    /* update surveyor's point */
+	    sx = x;
+	    sy = y;
+
+	    x = y = z = 0;
+	    meanx = meany = meanz = 0.;
+	    tot_w = 0.;
+
+	    wci = 0;
+	    Vect_reset_line(CPoints);
+	    xp = Points->x;
+	    yp = Points->y;
+	    for (i = 1; i < Points->n_points; i++) {
+		w = (sx - xp[i - 1]) * (yp[i] - sy) - 
+		    (sx - xp[i]) * (yp[i - 1] - sy);
+		
+		tot_w += w;
+
+		cx = xp[i - 1] + xp[i] + sx;
+		cy = yp[i - 1] + yp[i] + sy;
+		meanx += cx * w;
+		meany += cy * w;
+
+		wc[wci++] = w;
+		Vect_append_point(CPoints, cx / 3., cy / 3., 0);
+	    }
+
+	    for (isle = 0; isle < nisles; isle++) {
+		xp = IPoints[isle]->x;
+		yp = IPoints[isle]->y;
+		for (i = 1; i < IPoints[isle]->n_points; i++) {
+		    w = (sx - xp[i - 1]) * (yp[i] - sy) - 
+			(sx - xp[i]) * (yp[i - 1] - sy);
+
+		    tot_w += w;
+
+		    cx = xp[i - 1] + xp[i] + sx;
+		    cy = yp[i - 1] + yp[i] + sy;
+		    meanx += cx * w;
+		    meany += cy * w;
+
+		    wc[wci++] = w;
+		    Vect_append_point(CPoints, cx / 3., cy / 3., 0);
+		}
+	    }
+	    if (tot_w != 0) {
+		x = meanx / (tot_w * 3);
+		y = meany / (tot_w * 3);
+	    }
+
+	    lastdist2all = -1;
+
+	    G_debug(3, "Approximating geometric median...");
+	    /* depends on the location of the surveyor
+	     * why? */
+
+	    for (iter = 0; iter < maxiter; iter++) {
+
+		distsum = dist2all = 0.;
+		x = y = z = 0.;
+
+		for (j = 0; j < CPoints->n_points; j++) {
+
+		    dx = CPoints->x[j] - medx;
+		    dy = CPoints->y[j] - medy;
+		    dist = sqrt(dx * dx + dy * dy);
+
+		    if (dist) {
+			w = wc[j] / dist;
+			x += CPoints->x[j] * w;
+			y += CPoints->y[j] * w;
+
+			distsum += w;
+		    }
+		    if (wc[j])
+			dist2all += dist / fabs(wc[j]);
+		}
+		
+		if (distsum) {
+		    x /= distsum;
+		    y /= distsum;
+		}
+
+		G_debug(3, "dist2all: %.15g", dist2all);
+		G_debug(3, "lastdist2all: %.15g", lastdist2all);
+		if (lastdist2all > 0) {
+		    double d = d_ulp(lastdist2all);
+		    
+		    if (lastdist2all - dist2all < d)
+			break;
+		}
+		if (dist2all == 0.)
+		    break;
+
+		lastdist2all = dist2all;
+
+		medx = x;
+		medy = y;
+
+		/* update surveyor's point */
+		sx = x;
+		sy = y;
+
+		x = y = z = 0;
+		meanx = meany = meanz = 0.;
+		tot_w = 0.;
+
+		wci = 0;
+		Vect_reset_line(CPoints);
+		xp = Points->x;
+		yp = Points->y;
+		for (i = 1; i < Points->n_points; i++) {
+		    w = (sx - xp[i - 1]) * (yp[i] - sy) - 
+			(sx - xp[i]) * (yp[i - 1] - sy);
+		    
+		    tot_w += w;
+
+		    cx = xp[i - 1] + xp[i] + sx;
+		    cy = yp[i - 1] + yp[i] + sy;
+		    meanx += cx * w;
+		    meany += cy * w;
+
+		    wc[wci++] = w;
+		    Vect_append_point(CPoints, cx / 3., cy / 3., 0);
+		}
+
+		for (isle = 0; isle < nisles; isle++) {
+		    xp = IPoints[isle]->x;
+		    yp = IPoints[isle]->y;
+		    for (i = 1; i < IPoints[isle]->n_points; i++) {
+			w = (sx - xp[i - 1]) * (yp[i] - sy) - 
+			    (sx - xp[i]) * (yp[i - 1] - sy);
+
+			tot_w += w;
+
+			cx = xp[i - 1] + xp[i] + sx;
+			cy = yp[i - 1] + yp[i] + sy;
+			meanx += cx * w;
+			meany += cy * w;
+
+			wc[wci++] = w;
+			Vect_append_point(CPoints, cx / 3., cy / 3., 0);
+		    }
+		}
+		if (tot_w != 0) {
+		    x = meanx / (tot_w * 3);
+		    y = meany / (tot_w * 3);
+		}
+	    }
+	    G_debug(3, "Iteration converged after %d passes, dist = %.15g",
+		    iter, lastdist2all);
+
+	    x = medx;
+	    y = medy;
+
+	    if (Out) {
+		Vect_reset_line(OPoints);
+		Vect_append_point(OPoints, x, y, 0);
+		Vect_write_line(Out, GV_POINT, OPoints, Cats);
+	    }
+	    else
+		fprintf(stdout, "8|%.15g|%.15g|%.15g\n", x, y, 0.);
+
+	    G_percent(area, nareas, 4);
+	}
+    }
+
+    /* approximate geometric median with Weiszfeld's algorithm:
+     * iterative least squares reduction */
+    if (mode & A_MEDIAN_B) {
+	struct line_pnts *CPoints;
+	double cx, cy, dx, dy;
+	double medx, medy;
+	double *wb, tot_w;
+	int nw, nw_alloc, wi;
+	double dist, distsum, dist2all, lastdist2all;
+	int j, iter, maxiter = 1000;
+
+	G_message(_("Calculating geometric medians for areas..."));
+	
+	CPoints = Vect_new_line_struct();
+	
+	nw_alloc = 100;
+	wb = G_malloc(nw_alloc * sizeof(double));
+	
+	G_percent(0, nareas, 4);
+
+	for (area = 1; area <= nareas; area++) {
+
+	    Vect_reset_cats(Cats);
+	    if (layer > 1) {
+		Vect_get_area_cats(In, area, Cats);
+	        if (!Vect_cats_in_constraint(Cats, layer, cat_list))
+		    continue;
+	    }
+
+	    Vect_get_area_points(In, area, Points);
+	    Vect_line_prune(Points);
+	    nw = Points->n_points - 1;
+	    
+	    nisles = Vect_get_area_num_isles(In, area);
+	    if (nisles > nisles_alloc) {
+		IPoints = G_realloc(IPoints, nisles * sizeof (struct line_pnts *));
+		for (isle = nisles_alloc; isle < nisles; isle++) {
+		    IPoints[isle] = Vect_new_line_struct();
+		}
+		nisles_alloc = nisles;
+	    }
+	    for (isle = 1; isle < nisles; isle++) {
+		Vect_get_isle_points(In, isle, IPoints[isle - 1]);
+		Vect_line_prune(IPoints[isle - 1]);
+		nw += IPoints[isle - 1]->n_points - 1;
+	    }
+
+	    if (nw_alloc < nw) {
+		nw_alloc = nw;
+		wb = G_realloc(wb, nw_alloc * sizeof(double));
+	    }
+
+	    /* segment lengths */
+	    meanx = meany = 0.;
+	    tot_w = 0.;
+	    wi = 0;
+	    Vect_reset_line(CPoints);
+	    xp = Points->x;
+	    yp = Points->y;
+	    for (i = 1; i < Points->n_points; i++) {
+		dx = xp[i] - xp[i - 1];
+		dy = yp[i] - yp[i - 1];
+		w = sqrt(dx * dx + dy * dy);
+
+		cx = xp[i - 1] + xp[i];
+		meanx += cx * w;
+		cy = yp[i - 1] + yp[i];
+		meany += cy * w;
+
+		wb[wi++] = w;
+		tot_w += w;
+
+		Vect_append_point(CPoints, cx / 2., cy / 2., 0);
+	    }
+
+	    for (isle = 0; isle < nisles; isle++) {
+		xp = IPoints[isle]->x;
+		yp = IPoints[isle]->y;
+		for (i = 1; i < IPoints[isle]->n_points; i++) {
+		    dx = xp[i] - xp[i - 1];
+		    dy = yp[i] - yp[i - 1];
+		    w = sqrt(dx * dx + dy * dy);
+
+		    cx = xp[i - 1] + xp[i];
+		    meanx += cx * w;
+		    cy = yp[i - 1] + yp[i];
+		    meany += cy * w;
+
+		    wb[wi++] = w;
+		    tot_w += w;
+
+		    Vect_append_point(CPoints, cx / 2., cy / 2., 0);
+		}
+	    }
+	    if (tot_w != 0) {
+		meanx /= (tot_w * 2);
+		meany /= (tot_w * 2);
+	    }
+
+	    medx = meanx;
+	    medy = meany;
+
+	    G_debug(3, "Approximating geometric median...");
+
+	    lastdist2all = -1;
+	    for (iter = 0; iter < maxiter; iter++) {
+
+		distsum = dist2all = 0.;
+		x = y = z = 0.;
+
+		for (j = 0; j < CPoints->n_points; j++) {
+
+		    dx = CPoints->x[j] - medx;
+		    dy = CPoints->y[j] - medy;
+		    dist = sqrt(dx * dx + dy * dy);
+
+		    if (dist) {
+			w = wb[j] / dist;
+			x += CPoints->x[j] * w;
+			y += CPoints->y[j] * w;
+
+			distsum += w;
+		    }
+		    if (wb[j])
+			dist2all += dist / wb[j];
+		}
+		
+		if (distsum) {
+		    x /= distsum;
+		    y /= distsum;
+		}
+
+		G_debug(3, "dist2all: %.15g", dist2all);
+		G_debug(3, "lastdist2all: %.15g", lastdist2all);
+		if (lastdist2all > 0) {
+		    double d = d_ulp(lastdist2all);
+		    
+		    if (lastdist2all - dist2all < d)
+			break;
+		}
+		if (dist2all == 0.)
+		    break;
+
+		lastdist2all = dist2all;
+
+		medx = x;
+		medy = y;
+	    }
+	    G_debug(3, "Iteration converged after %d passes, dist = %.15g",
+		    iter, lastdist2all);
+
+	    x = meanx;
+	    y = meany;
+
+	    x = medx;
+	    y = medy;
+
+	    if (Out) {
+		Vect_reset_line(OPoints);
+		Vect_append_point(OPoints, x, y, 0);
+		Vect_write_line(Out, GV_POINT, OPoints, Cats);
+	    }
+	    else
+		fprintf(stdout, "9|%.15g|%.15g|%.15g\n", x, y, 0.);
+
+	    G_percent(area, nareas, 4);
+	}
+    }
+
+    return 1;
+}
---
+++ grass7/vector/v.centerpoint/lines.c
@@ -0,0 +1,258 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <grass/gis.h>
+#include <grass/vector.h>
+#include <grass/glocale.h>
+#include "local_proto.h"
+
+static double d_ulp(double d)
+{
+    int exp;
+
+    if (d == 0)
+	return GRASS_EPSILON;
+
+    d = frexp(d, &exp);
+    exp -= 51;
+    d = ldexp(d, exp);
+
+    return d;
+}
+
+int lines_center(struct Map_info *In, struct Map_info *Out, int layer, 
+                 struct cat_list *cat_list, int n_primitives, int mode)
+{
+    struct line_pnts *Points, *OPoints;
+    struct line_cats *Cats;
+    double x, y, z;
+    int type;
+    int counter;
+
+    Points = Vect_new_line_struct();
+    OPoints = Vect_new_line_struct();
+    Cats = Vect_new_cats_struct();
+
+    /* mid point */
+    if (mode & L_MID) {
+	double len;
+
+	counter = 0;
+
+	Vect_rewind(In);
+	while ((type = Vect_read_next_line(In, Points, Cats)) > 0) {
+	    G_percent(counter, n_primitives, 4);
+	    counter++;
+
+	    if (!(type & GV_LINE))
+		continue;
+	    if (layer > 0 &&
+	        !Vect_cats_in_constraint(Cats, layer, cat_list))
+		continue;
+
+	    Vect_line_prune(Points);
+
+	    len = Vect_line_length(Points) / 2.;
+	    
+	    Vect_point_on_line(Points, len, &x, &y, &z, NULL, NULL);
+	    
+	    if (Out) {
+		Vect_reset_line(OPoints);
+		Vect_append_point(OPoints, x, y, z);
+		Vect_write_line(Out, GV_POINT, OPoints, Cats);
+	    }
+	    else
+		fprintf(stdout, "4|%.15g|%.15g|%.15g\n", x, y, z);
+	}
+	G_percent(1, 1, 1);
+    }
+    
+    /* center of gravity */
+    if (mode & L_MEAN) {
+	double len, slen;
+	double dx, dy, dz;
+	int i;
+
+	counter = 0;
+
+	Vect_rewind(In);
+	while ((type = Vect_read_next_line(In, Points, Cats)) > 0) {
+	    G_percent(counter, n_primitives, 4);
+	    counter++;
+	    if (!(type & GV_LINE))
+		continue;
+	    if (layer > 0 &&
+	        !Vect_cats_in_constraint(Cats, layer, cat_list))
+		continue;
+
+	    Vect_line_prune(Points);
+
+	    len = 0.;
+	    
+	    x = y = z = 0.;
+
+	    for (i = 1; i < Points->n_points; i++) {
+		dx = Points->x[i - 1] - Points->x[i];
+		dy = Points->y[i - 1] - Points->y[i];
+		dz = Points->z[i - 1] - Points->z[i];
+		
+		slen = sqrt(dx * dx + dy * dy + dz * dz);
+		
+		x += (Points->x[i - 1] + Points->x[i]) * slen;
+		y += (Points->y[i - 1] + Points->y[i]) * slen;
+		z += (Points->z[i - 1] + Points->z[i]) * slen;
+		
+		len += slen;
+	    }
+	    
+	    x /= (len * 2);
+	    y /= (len * 2);
+	    z /= (len * 2);
+
+	    if (Out) {
+		Vect_reset_line(OPoints);
+		Vect_append_point(OPoints, x, y, z);
+		Vect_write_line(Out, GV_POINT, OPoints, Cats);
+	    }
+	    else
+		fprintf(stdout, "5|%.15g|%.15g|%.15g\n", x, y, z);
+	}
+	G_percent(1, 1, 1);
+    }
+
+    /* approximate geometric median with Weiszfeld's algorithm:
+     * iterative least squares reduction */
+    if (mode & L_MEDIAN) {
+	struct line_pnts *SPoints;
+	double *len;
+	double dx, dy, dz;
+	double xmean, ymean, zmean;
+	double medx, medy, medz;
+	double dist, distsum, dist2all, lastdist2all;
+	int i, j, iter, maxiter = 100;
+	
+	SPoints = Vect_new_line_struct();
+
+	counter = 0;
+
+	Vect_rewind(In);
+	while ((type = Vect_read_next_line(In, Points, Cats)) > 0) {
+	    G_percent(counter, n_primitives, 4);
+	    counter++;
+	    if (!(type & GV_LINE))
+		continue;
+	    if (layer > 0 &&
+	        !Vect_cats_in_constraint(Cats, layer, cat_list))
+		continue;
+
+	    Vect_line_prune(Points);
+	    
+	    len = G_malloc((Points->n_points - 1) * sizeof(double));
+	    Vect_reset_line(SPoints);
+	    
+	    xmean = ymean = zmean = 0.;
+	    
+	    for (i = 0; i < Points->n_points - 1; i++) {
+		dx = Points->x[i + 1] - Points->x[i];
+		dy = Points->y[i + 1] - Points->y[i];
+		dz = Points->z[i + 1] - Points->z[i];
+		
+		len[i] = sqrt(dx * dx + dy * dy + dz * dz);
+
+		x = (Points->x[i + 1] + Points->x[i]) / 2.;
+		y = (Points->y[i + 1] + Points->y[i]) / 2.;
+		z = (Points->z[i + 1] + Points->z[i]) / 2.;
+		
+		Vect_append_point(SPoints, x, y, z);
+		
+		xmean += Points->x[i];
+		ymean += Points->y[i];
+		zmean += Points->z[i];
+	    }
+	    
+	    i = Points->n_points - 1;
+	    xmean += Points->x[i];
+	    ymean += Points->y[i];
+	    zmean += Points->z[i];
+	    
+	    xmean /= Points->n_points;
+	    ymean /= Points->n_points;
+	    zmean /= Points->n_points;
+
+	    medx = xmean;
+	    medy = ymean;
+	    medz = zmean;
+
+	    lastdist2all = -1;
+
+	    G_debug(3, "Approximating geometric median...");
+
+	    for (iter = 0; iter < maxiter; iter++) {
+
+		distsum = dist2all = 0.;
+		x = y = z = 0.;
+
+		for (j = 0; j < SPoints->n_points; j++) {
+
+		    dx = SPoints->x[j] - medx;
+		    dy = SPoints->y[j] - medy;
+		    dz = SPoints->z[j] - medz;
+		    dist = sqrt(dx * dx + dy * dy + dz * dz);
+		    
+		    if (dist) {
+			x += SPoints->x[j] * len[j] / dist;
+			y += SPoints->y[j] * len[j] / dist;
+			z += SPoints->z[j] * len[j] / dist;
+
+			distsum += len[j] / dist;
+			dist2all += dist;
+		    }
+		    if (len[j])
+			dist2all += dist / len[j];
+		}
+		
+		if (distsum) {
+		    x /= distsum;
+		    y /= distsum;
+		    z /= distsum;
+		}
+
+		G_debug(3, "dist2all: %.15g", dist2all);
+		G_debug(3, "lastdist2all: %.15g", lastdist2all);
+		if (lastdist2all > 0) {
+		    double d = d_ulp(lastdist2all);
+		    
+		    if (lastdist2all - dist2all < d)
+			break;
+		}
+		if (dist2all == 0.)
+		    break;
+
+		lastdist2all = dist2all;
+
+		medx = x;
+		medy = y;
+		medz = z;
+	    }
+	    G_debug(3, "Iteration converged after %d passes, dist = %.15g",
+		    iter, lastdist2all);
+
+	    G_free(len);
+
+	    x = medx;
+	    y = medy;
+	    z = medz;
+
+	    if (Out) {
+		Vect_reset_line(OPoints);
+		Vect_append_point(OPoints, x, y, z);
+		Vect_write_line(Out, GV_POINT, OPoints, Cats);
+	    }
+	    else
+		fprintf(stdout, "6|%.15g|%.15g|%.15g\n", x, y, z);
+	}
+	G_percent(1, 1, 1);
+    }
+
+    return 1;
+}
