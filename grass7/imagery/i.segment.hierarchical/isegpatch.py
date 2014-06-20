d244799c34717fc8ecad4a07251d159029d64ce8
Pietro Zambelli <peter.zamb@gmail.com>
2014-06-20 12:51:31 +0000

    Comment an unsed function: node_patch
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@60888 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/imagery/i.segment.hierarchical/isegpatch.py
+++ grass7/imagery/i.segment.hierarchical/isegpatch.py
@@ -4,15 +4,9 @@ Created on Mon Oct 21 12:54:42 2013
 
 @author: pietro
 """
-import os
-import re
-
-import grass.lib.gis as libgis
-
 from grass.pygrass.modules.grid.patch import get_start_end_index
-from grass.pygrass.modules.grid.split import split_region_tiles
-from grass.pygrass.modules.grid.node import row_order
 from grass.pygrass.raster import RasterRow
+from grass.pygrass.messages import get_msgr
 
 
 def rpatch_row(rast, rasts, bboxes, max_rasts):
@@ -37,6 +31,7 @@ def rpatch_map(raster, mapset, mset_str, bbox_list, overwrite=False,
     rast = RasterRow(prefix + raster, mapset)
     with RasterRow(name=raster, mapset=mset_str % (0, 0), mode='r') as rtype:
         rast.open('w', mtype=rtype.mtype, overwrite=overwrite)
+    msgr = get_msgr()
     rasts = []
     mrast = 0
     nrows = len(bbox_list)
@@ -44,7 +39,7 @@ def rpatch_map(raster, mapset, mset_str, bbox_list, overwrite=False,
         rrasts = []
         max_rasts = []
         for col in range(len(rbbox)):
-            libgis.G_percent(row, nrows, 1)
+            msgr.percent(row, nrows, 1)
             rrasts.append(RasterRow(name=raster,
                                     mapset=mset_str % (start_row + row,
                                                        start_col + col)))
@@ -59,23 +54,29 @@ def rpatch_map(raster, mapset, mset_str, bbox_list, overwrite=False,
 
     rast.close()
 
-
-def node_patch(cmd, nwidth, nheight, out_regexp, overwrite=True):
-    from grass.lib.gis import G_tempfile
-    tmp, dummy = os.path.split(os.path.split(G_tempfile())[0])
-    tmpdir = os.path.join(cmd)
-    bboxes = split_region_tiles(width=nwidth, height=nheight)
-    for out in os.listdir(tmpdir):
-        outdir = os.path.join(tmpdir, out)
-        rasts = os.listdir(outdir)
-        rsts = row_order(rasts, bboxes)
-        rst = RasterRow(re.findall(out_regexp, rasts[0])[0])
-        rst.open('w', mtype=rsts[0][0].mtype, overwrite=overwrite)
-        for rrst, rbbox in zip(rsts, bboxes):
-            rpatch_row(rst, rrst, rbbox)
-
-        for rrst in rsts:
-            for r in rrst:
-                r.close()
-
-        rst.close()
+# run on a cluster
+#import os
+#import re
+#from grass.pygrass.modules.grid.split import split_region_tiles
+#from grass.pygrass.modules.grid.node import row_order
+#
+#
+#def node_patch(cmd, nwidth, nheight, out_regexp, overwrite=True):
+#    from grass.lib.gis import G_tempfile
+#    tmp, dummy = os.path.split(os.path.split(G_tempfile())[0])
+#    tmpdir = os.path.join(cmd)
+#    bboxes = split_region_tiles(width=nwidth, height=nheight)
+#    for out in os.listdir(tmpdir):
+#        outdir = os.path.join(tmpdir, out)
+#        rasts = os.listdir(outdir)
+#        rsts = row_order(rasts, bboxes)
+#        rst = RasterRow(re.findall(out_regexp, rasts[0])[0])
+#        rst.open('w', mtype=rsts[0][0].mtype, overwrite=overwrite)
+#        for rrst, rbbox in zip(rsts, bboxes):
+#            rpatch_row(rst, rrst, rbbox)
+#
+#        for rrst in rsts:
+#            for r in rrst:
+#                r.close()
+#
+#        rst.close()
