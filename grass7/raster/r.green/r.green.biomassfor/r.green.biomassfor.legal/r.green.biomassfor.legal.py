fb59f0ad24797c485b007e2709814837a68b2cf5
Francesco Geri <fgeri@icloud.com>
2015-02-23 08:52:07 +0000

    fix bug to r.green.biomassfor.legal
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@64726 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/raster/r.green/r.green.biomassfor/r.green.biomassfor.legal/r.green.biomassfor.legal.py
+++ grass7/raster/r.green/r.green.biomassfor/r.green.biomassfor.legal/r.green.biomassfor.legal.py
@@ -107,7 +107,7 @@ def main(opts, flgs):
 
     ECOT=l_bioenergy+' = ('+l_bioenergyHF+' + '+l_bioenergyC+')'
 
-    run_command("r.mapcalc", overwrite=ow,expression='yield_pix1 = ('+yield1+'/'+yield_surface+')*((ewres()*nsres())/10000)')
+    run_command("r.mapcalc", overwrite=ow,expression='yield_pix1 = ('+yield_+'/'+yield_surface+')*((ewres()*nsres())/10000)')
        
     run_command("r.mapcalc", overwrite=ow,
                 expression=ECOHF % tuple(map(float, (opts['energy_tops_hf'],
