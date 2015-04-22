bbaf4e80681a4410088f7c2c68800fc73cdd3f4a
Martin Landa <landa.martin@gmail.com>
2015-04-22 13:30:42 +0000

    wingrass: fix bat generation
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@65120 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/wingrass-packager/grass_addons.sh
+++ tools/wingrass-packager/grass_addons.sh
@@ -7,8 +7,10 @@ ADDON_PATH=/c/Users/landa/grass_packager
 
 PATH_ORIG=`echo $PATH`
 
-cd $SVN_PATH
-svn up || (svn cleanup && svn up)
+(cd $SVN_PATH && \
+ export PATH=$PATH_ORIG:/c/osgeo4w$3/apps/msys/bin:/c/Subversion/bin && \
+ svn up || (svn cleanup && svn up) \
+)
 
 # see http://lists.osgeo.org/pipermail/grass-dev/2011-December/056938.html
 function tidy_citizen {
@@ -24,7 +26,7 @@ function tidy_citizen {
 }
 
 function compile {
-    export PATH=$PATH:/c/OSGeo4W$4/apps/msys/bin:/c/OSGeo4W$4/bin:$2/dist.i686-pc-mingw32/bin:$2/dist.i686-pc-mingw32/scripts
+    export PATH=$PATH:/c/OSGeo4W$4/apps/msys/bin:/c/OSGeo4W$4/bin:$2/dist.i686-pc-mingw32/bin:$2/dist.i686-pc-mingw32/scripts:/c/subversion/bin/svn
     export PYTHONHOME=/c/OSGeo4W$4/apps/Python27
     rm -rf $3
     $SVN_PATH/tools/addons/compile.sh $1 $2 $3 1
@@ -35,15 +37,17 @@ function compile {
 	    continue
 	fi
 	cd $mod
-	if [ -f bin/${mod}.bat ] ; then
+	echo $mod
+	for f in `ls bin/*.bat 2> /dev/null` ; do
+	    echo $f
 	    if [ `echo $1 | sed -e 's/\(^.*\)\(.$\)/\2/'` = "6" ] ; then
 		replace_gisbase="GRASS_ADDON_PATH"
 	    else
 		replace_gisbase="GRASS_ADDON_BASE"
 	    fi
-	    sed "s/GISBASE/$replace_gisbase/" bin/${mod}.bat > tmp
-	    mv tmp bin/${mod}.bat
-	fi
+	    sed "s/GISBASE/$replace_gisbase/" $f > tmp
+	    mv tmp $f
+	done
 	# if [ `echo $1 | sed -e 's/\(^.*\)\(.$\)/\2/'` = "6" ] ; then
 	#     tidy_citizen
 	# fi
@@ -59,9 +63,9 @@ export LANGUAGE=C
 
 if test -z $1 ; then
     compile $SVN_PATH/grass6 $GISBASE_PATH/grass644        $ADDON_PATH/grass644/addons
-    compile $SVN_PATH/grass6 $GISBASE_PATH/grass64_release $ADDON_PATH/grass64/addons
+    ### compile $SVN_PATH/grass6 $GISBASE_PATH/grass64_release $ADDON_PATH/grass64/addons
     compile $SVN_PATH/grass7 $GISBASE_PATH/grass700        $ADDON_PATH/grass700/addons
-    compile $SVN_PATH/grass7 $GISBASE_PATH/grass70_release $ADDON_PATH/grass70/addons
+    ### compile $SVN_PATH/grass7 $GISBASE_PATH/grass70_release $ADDON_PATH/grass70/addons
     compile $SVN_PATH/grass7 $GISBASE_PATH/grass_trunk     $ADDON_PATH/grass71/addons
 else
     compile $SVN_PATH/grass6 $GISBASE_PATH/grass$1         $ADDON_PATH/grass$1/addons
