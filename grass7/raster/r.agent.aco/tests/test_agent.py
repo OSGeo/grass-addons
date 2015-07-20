56bfb8102a6b44b0ee97f64aebaa12444c1bde86
Annalisa Minelli <annalisa.minelli@gmail.com>
2015-07-20 11:19:53 +0000

    rename of the module and fix of makefiles
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@65699 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass7/raster/r.agent.aco/tests/test_agent.py
@@ -0,0 +1,46 @@
+
+import unittest2 as unittest
+#import unittest
+
+from libagent import playground, world, agent
+
+class TestAgent(unittest.TestCase):
+    def setUp(self):
+        self.pg = playground.Playground()
+        self.pg.setregion(3,3)
+        self.world = world.World(self.pg, None)
+        self.world.bear(1, [1,1], agent.Agent)
+        self.agent = self.world.agents[0]
+
+    def test_setposition(self):
+        self.assertTrue(self.agent.getposition() == [1,1])
+        self.agent.setposition([3,3])
+        self.assertTrue(self.agent.getposition() == [3,3])
+
+    def test_getposition(self):
+        self.assertTrue(self.agent.getposition() == [1,1])
+
+    def test_randomposition(self):
+        position = [1,2]
+        self.assertEqual(position,
+                    self.agent.randomposition([position, position]))
+
+#    def test_move(self):
+# not implemented yet
+#        pass
+
+    def test_age(self):
+        self.agent.ttl = 1
+        age = self.agent.ttl
+        self.agent.age()
+        self.assertTrue(self.agent.ttl < age)
+        self.agent.age()
+        self.assertTrue(len(self.world.agents) == 0)
+
+    def test_snuffit(self):
+        self.assertTrue(len(self.world.agents) == 1)
+        self.agent.snuffit()
+        self.assertTrue(len(self.world.agents) == 0)
+
+#    def tearDown(self):
+
