fe7942b2285d23117f6648512fcac33859626631
Martin Landa <landa.martin@gmail.com>
2015-07-20 19:59:33 +0000

    add grass-ci scripts (thanks to Ivan Mincik)
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@65727 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ tools/grass-ci/authors-prog.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Assign unknown email value to all unknown SVN commiters, which doesn't exist in grass-authors.txt file.
+
+# Author: Ivan Mincik, ivan.mincik@gmail.com
+
+echo "$1 <unknown-email>"
+
+# vim: set ts=4 sts=4 sw=4 noet:
