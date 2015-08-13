cfc195afd6b278d43717d005f12a9e28c49e969d
Matej Krejci <matejkrejci@gmail.com>
2015-08-13 09:24:49 +0000

    addons wx.metadata: chck dependency from Makefile
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@65924 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass7/gui/wxpython/wx.metadata/dependency.py
@@ -0,0 +1,46 @@
+owslib=False
+try:
+    import owslib
+    owslib=True
+except:
+    print'owslib library is missing. Check requirements on the manual page < https://grasswiki.osgeo.org/wiki/ISO/INSPIRE_Metadata_Support >'
+
+if owslib:
+    import owslib
+    owsvs= owslib.__version__
+    try:
+        from owslib.iso import *
+
+        MD_Metadata()
+    except:
+        print ('Installed version of owslib library is < %s >.'%owsvs)
+        print('owslib >=0.9 is required. Check requirements on the manual page < https://grasswiki.osgeo.org/wiki/ISO/INSPIRE_Metadata_Support >')
+
+
+try:
+    import jinja2
+except:
+    print'jinja2 library is missing. Check requirements on the manual page < https://grasswiki.osgeo.org/wiki/ISO/INSPIRE_Metadata_Support >'
+pycsw=False
+
+try:
+    import pycsw
+    pycsw=True
+except:
+    print('pycsw library is missing. Check requirements on the manual page < https://grasswiki.osgeo.org/wiki/ISO/INSPIRE_Metadata_Support >')
+
+
+if pycsw:
+    import pycsw
+    cswvs= pycsw.__version__
+    try:
+        from pycsw.core import admin
+    except:
+        print ('Installed version of pycsw library is < %s >.'%cswvs)
+        print('pycsw >=2.0 is required. Check requirements on the manual page < https://grasswiki.osgeo.org/wiki/ISO/INSPIRE_Metadata_Support >')
+
+
+from owslib.iso import *
+import jinja2
+from pycsw.core import admin
+
