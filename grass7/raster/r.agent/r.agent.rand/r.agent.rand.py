8929e906fb6a6a331361ff040e8ed6901bfd08e3
Michael Lustenberger <mic@inofix.ch>
2015-11-11 18:36:34 +0000

    remove debug sym
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@66814 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/raster/r.agent/r.agent.rand/r.agent.rand.py
+++ grass7/raster/r.agent/r.agent.rand/r.agent.rand.py
@@ -133,7 +133,6 @@ def run(rounds, maxagents, agentlife, mark, overwrite):
             agent.step()
             position = agent.getposition()
             newvalue = world.playground.getcellvalue('RESULT', position) + mark
-            grass.message("setting " + position + " to new value " + newvalue)
             world.playground.setcellvalue('RESULT', position, newvalue)
     world.playground.writelayer('RESULT', False, overwrite)
 
