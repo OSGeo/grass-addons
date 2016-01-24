e80275276bec6e82a3f38812f208aef5fd651eef
Yann Chemin <yann.chemin@gmail.com>
2016-01-24 22:06:07 +0000

    Added tcw, wi and awei libraries
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@67655 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass7/imagery/i.wi/awei.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+    /* Automated Water Extraction Index
+     * Feyisa, G.L., Meilby, H., Fensholt, R., & Proud, S.R. (2014). Automated Water Extraction
+Index: A new technique for surface water mapping using Landsat imagery. Remote
+Sensing of Environment, 140, 23–35. http://dx.doi.org/10.1016/j.rse.2013.08.029.
+     * Landsat TM/ETM+ : 4 × (b2 - b5 ) - (0.25 × b4 + 2.75 × b5 )
+     */ 
+double awei_noshadow(double greenchan, double nirchan, double mirchan) 
+{
+    double result= 4 * (greenchan - mirchan ) - (0.25 * nirchan + 2.75 * mirchan )
+    return result;
+}
+
+    /* Automated Water Extraction Index
+     * Feyisa, G.L., Meilby, H., Fensholt, R., & Proud, S.R. (2014). Automated Water Extraction
+Index: A new technique for surface water mapping using Landsat imagery. Remote
+Sensing of Environment, 140, 23–35. http://dx.doi.org/10.1016/j.rse.2013.08.029.
+     * Landsat TM/ETM+ : b1 + 2.5 × b2 - 1.5 × (b4 + b5 ) - 0.25 × b7
+     */ 
+double awei_shadow(double bluechan, double greenchan, double nirchan, double mirchan, double band7chan) 
+{
+    double result= bluechan + 2.5 * greenchan - 1.5 * (nirchan + mirchan) - 0.25 * band7chan
+    return result;
+}
