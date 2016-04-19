3d4f77f8f58e306fbba64090d3da601cb46217db
Paulo Van Breugel <p.vanbreugel@gmail.com>
2016-04-19 14:20:49 +0000

    r.vif addon: improvements in how output is handled; adding option to only print finally selected variables; updated html file accordingly
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@68284 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/raster/r.vif/r.vif.py
+++ grass7/raster/r.vif/r.vif.py
@@ -66,15 +66,23 @@
 #% guisection: Input
 #%end
 
-#=======================================================================
+#%flag
+#% key: s
+#% description: Only print selected variables
+#%end
+
+#%rules
+#%requires_all: -s,maxvif
+#%end
+
+#==============================================================================
 ## General
-#=======================================================================
+#==============================================================================
 
 # import libraries
 import os
 import sys
 import math
-import tempfile
 import numpy as np
 import grass.script as grass
 
@@ -112,22 +120,32 @@ def main():
     if MXVIF != '':
         MXVIF = float(MXVIF)
     OPF = options['file']
-    if OPF == '':
-        idf, OPF = tempfile.mkstemp()
+    flag_s = flags['s']
 
-    #=======================================================================
+    #==========================================================================
     ## Calculate VIF and write to standard output (& optionally to file)
-    #=======================================================================
+    #==========================================================================
 
-    # Calculate VIF and write results to text file
+    # Determine maximum width of the columns to be printed to std output
     name_lengths = []
     for i in IPF:
         name_lengths.append(len(i))
     nlength = max(name_lengths)
+
+    # Create arrays to hold results (which will be written to file at end)
+    out_vif = []
+    out_sqrt = []
+    out_variable = []
+    out_round = []
+
+    # VIF is computed for each variable
+    #--------------------------------------------------------------------------
     if MXVIF =='':
-        text_file = open(OPF, "w")
-        text_file.write("variable,vif,sqrtvif\n")
-        grass.info('{0[0]:{1}s} {0[1]:8s} {0[2]:8s}'.format(['variable', 'vif', 'sqrtvif'], nlength))
+        # Print header of table to std output
+        print('{0[0]:{1}s} {0[1]:8s} {0[2]:8s}'.format(
+            ['variable', 'vif', 'sqrtvif'], nlength))
+
+        # Compute the VIF
         for k in xrange(len(IPF)):
             MAPy = IPF[k]
             MAPx = IPF[:]
@@ -144,22 +162,46 @@ def main():
                 rsqr = float(vifstat[1][1])
                 vif = 1 / (1 - rsqr)
                 sqrtvif = math.sqrt(vif)
-            RES = [MAPy, vif, sqrtvif]
-            text_file.write('{0[0]}, {0[1]}, {0[2]}\n'.format(RES))
-            grass.info('{0[0]:{1}s} {0[1]:8.2f} {0[2]:8.2f}'.format(RES, nlength))
-        text_file.close()
+
+            # Write results to array (this will be printed to file at end
+            # of script)
+            out_vif.append(vif)
+            out_sqrt.append(sqrtvif)
+            out_variable.append(MAPy)
+            print('{0[0]:{1}s} {0[1]:8.2f} {0[2]:8.2f}'.format([MAPy, vif,
+                  sqrtvif], nlength))
+        print
+        if len(OPF) > 0:
+            print("Statistics are written to " + OPF + "\n")
+
+    # The stepwise variable selection procedure is run
+    #--------------------------------------------------------------------------
     else:
-        text_file = open(OPF, "w")
         rvifmx = MXVIF + 1
         m = 0
+        remove_variable = 'none'
+        out_removed = []
+        txt_message = "Working"
+
+        # The VIF will be computed across all variables. Next, the variable
+        # with highest vif is removed and the procedure is repeated. This
+        # continuous till the maximum vif across the variables > maxvif
         while MXVIF < rvifmx:
             m += 1
-            grass.info("\n")
-            grass.info("VIF round " + str(m))
-            grass.info("----------------------------------------")
             rvif = np.zeros(len(IPF))
-            text_file.write("variable\tvif\tsqrtvif\n")
-            grass.info('{0[0]:{1}s} {0[1]:8s} {0[2]:8s}'.format(['variable', 'vif', 'sqrtvif'], nlength))
+
+            # print the header of the output table to the console
+            if not flag_s:
+                print
+                print("VIF round " + str(m))
+                print("--------------------------------------")
+                print('{0[0]:{1}s} {0[1]:>8s} {0[2]:>8s}'.format(
+                    ['variable', 'vif', 'sqrtvif'], nlength))
+            else:
+                txt_message = txt_message + "."
+                grass.message(txt_message)
+
+            # Compute the VIF and sqrt(vif) for all variables in this round
             for k in xrange(len(IPF)):
                 MAPy = IPF[k]
                 MAPx = IPF[:]
@@ -176,45 +218,70 @@ def main():
                     rsqr = float(vifstat[1][1])
                     vif = 1 / (1 - rsqr)
                     sqrtvif = math.sqrt(vif)
-                RES = [MAPy, vif, sqrtvif]
-                text_file.write('{0[0]}, {0[1]}, {0[2]}\n'.format(RES))
-                grass.info('{0[0]:{1}s} {0[1]:8.2f} {0[2]:8.2f}'.format(RES, nlength))
+
+                # Write results to arrays (which will be written to csv file
+                # at end of script)
+                out_vif.append(vif)
+                out_sqrt.append(sqrtvif)
+                out_variable.append(MAPy)
+                out_round.append(m)
+                out_removed.append(remove_variable)
+
+                # print result to console
+                if not flag_s:
+                    print('{0[0]:{1}s} {0[1]:8.2f} {0[2]:8.2f}'.format([MAPy,
+                          vif, sqrtvif], nlength))
+
+                # If variable is set to be retained by the user, the VIF
+                # is set to -9999 to ensure it will not have highest VIF
                 if IPFn[k] in IPRn:
                     rvif[k] = -9999
                 else:
                     rvif[k] = vif
 
+            # Compute the maximum vif across the variables for this round and
+            # remove the variable with the highest VIF
             rvifmx = max(rvif)
             if rvifmx >= MXVIF:
                 rvifindex = np.argmax(rvif, axis=None)
-                varremove = IPF[rvifindex]
-                text_file.write("\n")
-                text_file.write("Removing " + varremove)
-                text_file.write("\n---------------------------\n")
-                text_file.write("\n")
+                remove_variable = IPFn[rvifindex]
                 del IPF[rvifindex]
                 del IPFn[rvifindex]
-            else:
-                text_file.write("\n\n")
-                text_file.write("Final selection (above) has maximum VIF = " + str(round(rvifmx, 3)))
-        text_file.close()
 
-        # Write to std output
-        grass.info("")
-        grass.info("selected variables are: ")
-        grass.info("----------------------------------------")
-        grass.info(', '.join(IPFn))
-        grass.info("with as maximum VIF: " + str(rvifmx))
-    grass.info("")
-    if options['file'] != '':
-        grass.info("Statistics are written to " + OPF + "\n")
-    grass.info("")
+
+        # Write final selected variables to std output
+        if not flag_s:
+            print
+            print("selected variables are: ")
+            print("--------------------------------------")
+            print(', '.join(IPFn))
+            print
+            if len(OPF) > 0:
+                print("Statistics are written to " + OPF + "\n")
+            print
+        else:
+            print(','.join(IPFn))
+
+    if len(OPF) > 0:
+        try:
+            text_file = open(OPF, "w")
+            if MXVIF =='':
+                text_file.write("variable,vif,sqrtvif\n")
+                for i in xrange(len(out_vif)):
+                    text_file.write('{0:s},{1:.6f},{2:.6f}\n'.format(
+                        out_variable[i], out_vif[i], out_sqrt[i]))
+            else:
+                text_file.write("round,removed,variable,vif,sqrtvif\n")
+                for i in xrange(len(out_vif)):
+                    text_file.write('{0:d},{1:s},{2:s},{3:.6f},{4:.6f}\n'.format(
+                        out_round[i], out_removed[i],out_variable[i],
+                         out_vif[i], out_sqrt[i]))
+        finally:
+            text_file.close()
 
 if __name__ == "__main__":
     options, flags = grass.parser()
     sys.exit(main())
-
-
 
 
 
