1b335ab5dda7e8841861354191441ca40e315392
Adam Laza <ad.laza32@gmail.com>
2016-08-04 20:28:44 +0000

    html, header file
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@69084 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/display/d.legend.vect/local_proto.h
+++ grass7/display/d.legend.vect/local_proto.h
@@ -1,3 +1,3 @@
 /* draw.c */
-void draw(char *, double, double, char *, int, int, int,
+void draw(char *, double, double, char *, int, int, int, int,
           int, char *, int, char *, int, char *, int, int, int);
