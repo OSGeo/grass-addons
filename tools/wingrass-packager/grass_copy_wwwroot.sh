3d2ae18e12dca8c7f4ab40f59788eb6cea80692d
Martin Landa <landa.martin@gmail.com>
2016-12-29 15:10:13 +0000

    WinGRASS: prepare for 7.2.0 release
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@70173 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/wingrass-packager/grass_copy_wwwroot.sh
+++ tools/wingrass-packager/grass_copy_wwwroot.sh
@@ -89,7 +89,7 @@ if test -z $2 ; then
     copy_addon 703       7.0.3
     copy_addon 704       7.0.4
     copy_addon 705       7.0.5
-    copy_addon 720RC2    7.2.0RC2
+    copy_addon 720       7.2.0
 else
     copy        $2       $3
 fi
