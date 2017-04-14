db89d0e119bf443606b22e44f2ec1ccb0350356e
Vaclav Petras <wenzeslaus@gmail.com>
2017-04-14 22:31:39 +0000

    r.fill.gaps: run indent script, remove tabs from first comment
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@70879 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/raster/r.fill.gaps/cell_funcs.h
+++ grass7/raster/r.fill.gaps/cell_funcs.h
@@ -1,22 +1,25 @@
+
 /***************************************************************************
  *
- * MODULE:	r.fill.gaps (commandline)
- * FILE:	cell_funcs.h
- * AUTHOR(S):	Benjamin Ducke
- * PURPOSE:	See cell_funcs.c
+ * MODULE:    r.fill.gaps (commandline)
+ * FILE:      cell_funcs.h
+ * AUTHOR(S): Benjamin Ducke
+ * PURPOSE:   See cell_funcs.c
  *
- * COPYRIGHT:	(C) 2011 by the GRASS Development Team
+ * COPYRIGHT: (C) 2011 by the GRASS Development Team
  *
- *		This program is free software under the GPL (>=v2)
- *		Read the file COPYING that comes with GRASS for details.
+ *            This program is free software under the GPL (>=v2)
+ *            Read the file COPYING that comes with GRASS for details.
+ *
  ****************************************************************************
  */
 
 #ifndef CELL_FUNCS_H
 #define CELL_FUNCS_H
 
-RASTER_MAP_TYPE IN_TYPE; /* stuff for cell input and output data */
+RASTER_MAP_TYPE IN_TYPE;        /* stuff for cell input and output data */
 RASTER_MAP_TYPE OUT_TYPE;
+
 /* below are the sizes (in bytes) for the different GRASS cell types */
 unsigned char CELL_IN_SIZE;
 unsigned char CELL_IN_PTR_SIZE;
@@ -24,9 +27,9 @@ unsigned char CELL_OUT_SIZE;
 unsigned char CELL_OUT_PTR_SIZE;
 unsigned char CELL_ERR_SIZE;
 
-void (*WRITE_CELL_VAL) (void*, void*); /* write a cell value of any type into an output cell */
-void (*WRITE_DOUBLE_VAL) (void*, double); /* write a double value into an output cell */
-int (*IS_NULL) (void*); /* check if a cell is "no data" */
-void (*SET_NULL) (void*, unsigned long); /* write null value(s) */
+void (*WRITE_CELL_VAL) (void *, void *);        /* write a cell value of any type into an output cell */
+void (*WRITE_DOUBLE_VAL) (void *, double);      /* write a double value into an output cell */
+int (*IS_NULL) (void *);        /* check if a cell is "no data" */
+void (*SET_NULL) (void *, unsigned long);       /* write null value(s) */
 
 #endif /* CELL_FUNCS_H */
