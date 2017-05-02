6be077463d640db6fab1e78828be72ae22bd16b9
Vaclav Petras <wenzeslaus@gmail.com>
2017-05-02 00:49:05 +0000

    r.binning: create raster from vector points using binning (incomplete wrapper of v.out.ascii piped to r.in.xyz)
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@70996 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass7/raster/r.binning/r.binning.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+############################################################################
+#
+# MODULE:    r.binning
+# AUTHOR(S): Vaclav Petras
+# PURPOSE:
+# COPYRIGHT: (C) 2017 by Vaclav Petras and the GRASS Development Team
+#
+#            This program is free software under the GNU General Public
+#            License (>=v2). Read the file COPYING that comes with GRASS
+#            for details.
+#
+#############################################################################
+
+#%module
+#% description: Bin vector points into a raster map
+#% keyword: raster
+#% keyword: vector
+#% keyword: points
+#% overwrite: yes
+#%end
+#%option G_OPT_V_INPUT
+#%end
+#%option G_OPT_R_OUTPUT
+#%end
+
+
+import grass.script as gs
+
+
+def main():
+    options, flags = grass.parser()
+
+    vector = options['input']
+    layer = 1
+    raster = options['output']
+    method = 'mean'
+    sep = 'pipe'
+
+    out_process = gs.pipe_command(
+        'v.out.ascii', input=vector, layer=layer, format='point',
+        separator=sep)
+    in_process = gs.start_command(
+        'r.in.xyz', input='-', output=raster, method=method,
+        separator=sep, stdin=out_process.stdout)
+    in_process.communicate()
+    out_process.wait()
+
+
+if __name__ == "__main__":
+    main()
