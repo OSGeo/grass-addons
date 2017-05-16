fixed r2 calculation with r.learn.ml
git-svn-id: https://svn.osgeo.org/grass/grass-addons@71092 15284696-431f-4ddb-bdfa-cd5b030d7da7

grass7/raster/r.learn.ml/rlearn_crossval.py
@@ -185,8 +185,7 @@ def cross_val_scores(estimator, X, y, groups=None, sample_weight=None, cv=3,
     """
 
     from sklearn import metrics
-    from sklearn.model_selection import (
-        RandomizedSearchCV, GridSearchCV, StratifiedKFold)
+    from sklearn.model_selection import StratifiedKFold
     from sklearn.externals.joblib import Parallel, delayed
 
     # -------------------------------------------------------------------------
@@ -317,7 +316,9 @@ def cross_val_scores(estimator, X, y, groups=None, sample_weight=None, cv=3,
             # metrics that have no averaging for multiclass
             elif m == 'kappa' or m == 'specificity' or m == 'accuracy' \
             or m == 'hamming_loss' or m == 'jaccard_similarity' \
-            or m == 'log_loss' or m == 'zero_one_loss' or m == 'matthews_corrcoef':
+            or m == 'log_loss' or m == 'zero_one_loss' \
+            or m == 'matthews_corrcoef' or m == 'r2' \
+            or m == 'neg_mean_squared_error':
                 scores[m] = np.append(
                     scores[m], scoring_methods[m](y_test, y_pred))
 