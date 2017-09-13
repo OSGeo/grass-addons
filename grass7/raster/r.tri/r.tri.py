983a640c21fc598d103b292b8d85837de9d3868b
Steven Pawley <dr.stevenpawley@gmail.com>
2017-09-13 15:46:35 +0000

    r.tri standardized arguments similar to other GRASS tools. Minor manual updates.
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@71488 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/raster/r.tri/r.tri.py
+++ grass7/raster/r.tri/r.tri.py
@@ -6,12 +6,12 @@
 #
 # AUTHOR(S):    Steven Pawley
 #
-# PURPOSE:      Calculates the Terrain Ruggedness Index (TRI) of
-#                           Riley et al. (1999)
+# PURPOSE:      Simple script to calculate the Terrain Ruggedness Index (TRI)
+#               of Riley et al. (1999)
 #
-# COPYRIGHT:    (C) 2015 Steven Pawley, and by the GRASS Development Team
+# COPYRIGHT:    (C) 2015 Steven Pawley and by the GRASS Development Team
 #
-##############################################################################
+###############################################################################
 
 #%module
 #% description: Terrain Ruggedness Index
@@ -19,42 +19,44 @@
 
 #%option G_OPT_R_INPUT
 #% description: Input elevation raster
-#% key: dem
+#% key: input
 #% required : yes
 #%end
 
 #%option G_OPT_R_OUTPUT
 #% description: Output Terrain Ruggedness Index (TRI)
-#% key: tri
+#% key: output
 #% required : yes
 #%end
 
 #%option
-#% key: wsize
+#% key: size
 #% type: integer
-#% description: Radius of neighbourhood in cells
-#% answer: 1
+#% description: Size of neighbourhood in cells
+#% answer: 3
 #% guisection: Required
 #%end
 
 import sys
 import os
-import grass.script as grass
+import grass.script as gs
 
 def main():
-    dem = options['dem']
-    tri = options['tri']
-    wsize = int(options['wsize'])
-    neighcells = ((wsize*2+1)**2)-1
+    dem = options['input']
+    tri = options['output']
+    size = int(options['size'])
+
+    radius = (size-1)/2
+    neighcells = (size**2)-1
 
     # calculate TRI based on map calc statements
-    grass.message("Calculating the Topographic Ruggedness Index:")
+    gs.message("Calculating the Topographic Ruggedness Index:")
 
     # generate a list of spatial neighbourhood indexs for the chosen radius
     # ignoring the center cell
     offsets = []
-    for j in range(-wsize, wsize+1):
-        for i in range(-wsize, wsize+1):
+    for j in range(-radius, radius+1):
+        for i in range(-radius, radius+1):
             if (j,i) != (0,0):
                 offsets.append((j,i))
 
@@ -65,10 +67,10 @@ def main():
     expr = "$tri = (%s" % " + ".join(terms) + ") / $neighcells"
 
     # perform the r.mapcalc calculation with the moving window
-    grass.mapcalc(expr, tri=tri, dem=dem, neighcells=neighcells)
+    gs.mapcalc(expr, tri=tri, dem=dem, neighcells=neighcells)
     
     return 0
 
 if __name__ == "__main__":
-    options, flags = grass.parser()
+    options, flags = gs.parser()
     sys.exit(main())
