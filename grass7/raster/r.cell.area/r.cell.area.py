c17e2b83d38594ef600b726d492448b0177243c3
Andy Wickert <andrewwickert@gmail.com>
2017-09-13 03:53:04 +0000

    Tool to compute area in each raster cell
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@71487 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ grass7/raster/r.cell.area/r.cell.area.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python
+
+#!/usr/bin/env python
+############################################################################
+#
+# MODULE:       r.cell.area
+#
+# AUTHOR(S):    Andrew Wickert
+#
+# PURPOSE:      Compute raster cell areas
+#
+# COPYRIGHT:    (c) 2017 Andrew Wickert
+#
+#               This program is free software under the GNU General Public
+#               License (>=v2). Read the file COPYING that comes with GRASS
+#               for details.
+#
+#############################################################################
+#
+
+#%module
+#% description: Calculate cell sizes within the computational region
+#% keyword: raster
+#%end
+
+#%option G_OPT_R_OUTPUT
+#%  key: output
+#%  type: string
+#%  description: Output grid of cell sizes
+#%  required: yes
+#%end
+
+#%option
+#%  key: units
+#%  type: string
+#%  description: Units for output areas
+#%  options: m2, km2
+#%  required: yes
+#%end
+
+##################
+# IMPORT MODULES #
+##################
+
+# PYTHON
+import os
+import glob
+import numpy as np
+# GRASS
+import grass.script as grass
+from grass.script import array as garray
+from grass.pygrass.vector import VectorTopo
+
+def main():
+    """
+    Compute cell areas
+    """
+    
+    projinfo = grass.parse_command('g.proj', flags='g')
+    
+    options, flags = grass.parser()
+    output = options['output']
+    units = options['units']
+    
+    # First check if output exists
+    if len(grass.parse_command('g.list', type='rast', 
+                               pattern=options['output'])):
+        if not grass.overwrite():
+            grass.fatal("Raster map '" + options['output'] + 
+                        "' already exists. Use '--o' to overwrite.")
+
+    # Then compute
+    if projinfo['units'] == 'meters':
+        if units == 'm2':
+            grass.mapcalc(output+' = nsres() * ewres()')
+        elif units == 'km2':
+            grass.mapcalc(output+' = nsres() * ewres() / 10.^6')
+    elif projinfo['units'] == 'degrees':
+        if units == 'm2':
+            grass.mapcalc(output+' = ( 111195. * nsres() ) * \
+                          ( ewres() * (3.14159/180.) * 6371000. * cos(y()) )')
+        elif units == 'km2':
+            grass.mapcalc(output+' = ( 111.195 * nsres() ) * \
+                          ( ewres() * (3.14159/180.) * 6371. * cos(y()) )')
+    else:
+        print 'Units: ', + projinfo['units'] + ' not currently supported'
+    
+if __name__ == "__main__":
+    main()
+
