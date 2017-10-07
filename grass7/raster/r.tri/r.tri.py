2194fa0bbbab6ff77032fbfb9496469dbb9f4df1
Steven Pawley <dr.stevenpawley@gmail.com>
2017-10-07 19:46:51 +0000

    r.tri enhancement - added option to perform cell padding to minimize edge effect
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@71538 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/raster/r.tri/r.tri.py
+++ grass7/raster/r.tri/r.tri.py
@@ -37,19 +37,73 @@
 #% guisection: Required
 #%end
 
+#%flag
+#% key: p
+#% label: Cell padding
+#% description: Use cell padding to reduce edge effect
+#% guisection: Optional
+#%end
+
+
 import sys
+import atexit
+import random
+import string
+from subprocess import PIPE
 import grass.script as gs
+from grass.script.utils import parse_key_val
+from grass.pygrass.modules.shortcuts import general as g
+from grass.pygrass.modules.shortcuts import raster as r
+from grass.pygrass.gis.region import Region
+from grass.pygrass.raster import RasterRow
+
+TMP_RAST = []
+
+def cleanup():
+    gs.message("Deleting intermediate files...")
+
+    for f in TMP_RAST:
+        gs.run_command(
+            "g.remove", type="raster", name=f, flags="f", quiet=True)
+
+
+def temp_map(name):
+    tmp = name + ''.join(
+        [random.choice(string.ascii_letters + string.digits) for n in range(4)])
+    TMP_RAST.append(tmp)
+
+    return (tmp)
+
 
 def main():
     dem = options['input']
     tri = options['output']
     size = int(options['size'])
+    p = flags['p']
 
     radius = (size-1)/2
     neighcells = (size**2)-1
 
+    # store current region settings
+    current_reg = Region()
+
+    if p:
+        # check for existing mask
+        if RasterRow('MASK').exist():
+            gs.fatal('Cell padding option cannot be used with existing mask...please remove first')
+
+        # grow the map to remove border effects
+        gs.message('Growing DEM to avoid edge effects...')
+        dem_grown = temp_map('tmp_elevation_grown')
+        g.region(n=current_reg.north + (current_reg.nsres * (radius+2)),
+                 s=current_reg.south - (current_reg.nsres * (radius+2)),
+                 w=current_reg.west - (current_reg.ewres * (radius+2)),
+                 e=current_reg.east + (current_reg.ewres * (radius+2)))
+        r.grow(input=dem, output=dem_grown, radius=radius+2, quiet=True)
+        dem = dem_grown
+
     # calculate TRI based on map calc statements
-    gs.message("Calculating the Topographic Ruggedness Index:")
+    gs.message("Calculating the Topographic Ruggedness Index...")
 
     # generate a list of spatial neighbourhood indexs for the chosen radius
     # ignoring the center cell
@@ -66,10 +120,19 @@ def main():
     expr = "$tri = float(%s" % " + ".join(terms) + ") / $neighcells"
 
     # perform the r.mapcalc calculation with the moving window
-    gs.mapcalc(expr, tri=tri, dem=dem, neighcells=neighcells)
+    if p:
+        output_tmp = temp_map('tmp')
+        gs.mapcalc(expr, tri=output_tmp, dem=dem, neighcells=neighcells)
+        r.mask(raster=options['input'], quiet=True)
+        r.mapcalc('{x}={y}'.format(x=tri, y=output_tmp))
+        r.mask(flags='r', quiet=True)
+        Region.write(current_reg)
+    else:
+        gs.mapcalc(expr, tri=tri, dem=dem, neighcells=neighcells)
 
     return 0
 
 if __name__ == "__main__":
     options, flags = gs.parser()
+    atexit.register(cleanup)
     sys.exit(main())
