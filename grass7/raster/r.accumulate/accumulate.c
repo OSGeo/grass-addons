6ca661b780e9e74765702092ac8ac00fb20db576
Huidae Cho <grass4u@gmail.com>
2018-06-22 05:59:14 +0000

    r.accumulate: Remove a redundant variable
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@72878 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/raster/r.accumulate/accumulate.c
+++ grass7/raster/r.accumulate/accumulate.c
@@ -6,7 +6,7 @@ accumulate(CELL ** dir_buf, RASTER_MAP weight_buf, RASTER_MAP acc_buf,
 	   char **done, int row, int col)
 {
     int rows = weight_buf.rows, cols = weight_buf.cols;
-    int i, j, neighbor_dir, loop_dir, has_inflow;
+    int i, j, neighbor_dir, loop_dir;
     double acc_flow;
 
     if (done[row][col])
@@ -23,72 +23,55 @@ accumulate(CELL ** dir_buf, RASTER_MAP weight_buf, RASTER_MAP acc_buf,
 	for (j = -1; j <= 1; j++) {
 	    if (col + j < 0 || col + j >= cols || (i == 0 && j == 0))
 		continue;
-	    has_inflow = 0;
 	    neighbor_dir = dir_buf[row + i][col + j];
 	    loop_dir = 0;
 	    switch (i) {
 	    case -1:
 		switch (j) {
 		case -1:
-		    if (neighbor_dir == 315) {
-			has_inflow = 1;
+		    if (neighbor_dir == 315)
 			loop_dir = 135;
-		    }
 		    break;
 		case 0:
-		    if (neighbor_dir == 270) {
-			has_inflow = 1;
+		    if (neighbor_dir == 270)
 			loop_dir = 90;
-		    }
 		    break;
 		case 1:
-		    if (neighbor_dir == 225) {
-			has_inflow = 1;
+		    if (neighbor_dir == 225)
 			loop_dir = 45;
-		    }
 		    break;
 		}
 		break;
 	    case 0:
 		switch (j) {
 		case -1:
-		    if (neighbor_dir == 360) {
-			has_inflow = 1;
+		    if (neighbor_dir == 360)
 			loop_dir = 180;
-		    }
 		    break;
 		case 1:
-		    if (neighbor_dir == 180) {
-			has_inflow = 1;
+		    if (neighbor_dir == 180)
 			loop_dir = 360;
-		    }
 		    break;
 		}
 		break;
 	    case 1:
 		switch (j) {
 		case -1:
-		    if (neighbor_dir == 45) {
-			has_inflow = 1;
+		    if (neighbor_dir == 45)
 			loop_dir = 225;
-		    }
 		    break;
 		case 0:
-		    if (neighbor_dir == 90) {
-			has_inflow = 1;
+		    if (neighbor_dir == 90)
 			loop_dir = 270;
-		    }
 		    break;
 		case 1:
-		    if (neighbor_dir == 135) {
-			has_inflow = 1;
+		    if (neighbor_dir == 135)
 			loop_dir = 315;
-		    }
 		    break;
 		}
 		break;
 	    }
-	    if (has_inflow && dir_buf[row][col] != loop_dir)
+	    if (loop_dir && dir_buf[row][col] != loop_dir)
 		acc_flow +=
 		    accumulate(dir_buf, weight_buf, acc_buf, done, row + i,
 			       col + j);
