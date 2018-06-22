d5c10f85be3c536d41f0ff77e3008896990c042e
Huidae Cho <grass4u@gmail.com>
2018-06-22 21:39:11 +0000

    r.accumulate: Rename acc_flow to acc
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@72885 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/raster/r.accumulate/accumulate.c
+++ grass7/raster/r.accumulate/accumulate.c
@@ -7,15 +7,15 @@ accumulate(CELL ** dir_buf, RASTER_MAP weight_buf, RASTER_MAP acc_buf,
 {
     int rows = weight_buf.rows, cols = weight_buf.cols;
     int i, j, neighbor_dir, loop_dir;
-    double acc_flow;
+    double acc;
 
     if (done[row][col])
 	return get(acc_buf, row, col);
 
     if (weight_buf.map.v)
-	acc_flow = get(weight_buf, row, col);
+	acc = get(weight_buf, row, col);
     else
-	acc_flow = 1.0;
+	acc = 1.0;
 
     for (i = -1; i <= 1; i++) {
 	if (row + i < 0 || row + i >= rows)
@@ -72,14 +72,14 @@ accumulate(CELL ** dir_buf, RASTER_MAP weight_buf, RASTER_MAP acc_buf,
 		break;
 	    }
 	    if (loop_dir && dir_buf[row][col] != loop_dir)
-		acc_flow +=
+		acc +=
 		    accumulate(dir_buf, weight_buf, acc_buf, done, row + i,
 			       col + j);
 	}
     }
 
-    set(acc_buf, row, col, acc_flow);
+    set(acc_buf, row, col, acc);
     done[row][col] = 1;
 
-    return acc_flow;
+    return acc;
 }
