6b43d6b51eb7f4f36e297cfb28c06dee4cc2026d
Martin Landa <landa.martin@gmail.com>
2018-09-26 21:38:49 +0000

    svn2git: work in progress, draft of migrate.sh script addded
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@73427 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ tools/svn2git/migrate.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Initialize git repo (preferably use AUTHORS.txt from SVN)
+
+mkdir grass-gis-git ; cd grass-gis-git
+git svn init --stdlayout --prefix=svn https://svn.osgeo.org/grass/grass
+git svn --authors-file=../AUTHORS.txt fetch
+
+# Create local branches
+git branch develbranch_6 origin/develbranch_6
+for branch in `git branch -r | grep releasebranch | sed 's#  origin/##g'`; do
+    git branch $branch origin/$branch
+done
+git checkout develbranch_6
+git branch -D master
+git branch master origin/trunk
+git checkout master
+
+# Rename tags
+for i in `git branch -r | grep tags`; do
+    b=`echo $i | sed 's#origin/##'`
+    j=`echo $i | sed 's#origin/tags/release_[0-9]\+_##g'`
+    git branch $b $i
+    git tag -a $j -m "Tagging release $j"
+done
+git checkout master
+for i in `git branch | grep tags`; do
+    git branch -D $i
+done
+
+# Remove origin branches
+# for i in `git branch -r | grep origin`; do echo "git push origin --delete $i"; done
+
+# Fix commit messages (#x -> https://trac.osgeo.org/...)
+git reset --hard HEAD && git checkout master
+git filter-branch --msg-filter 'python  ../rewrite.py' -- --all
+# check out /tmp/log.txt for changes overview ...
--- tools/svn2git/readme.txt
+++ tools/svn2git/readme.txt
@@ -12,8 +12,4 @@ $ echo "(no author) = unknown <unknown@unknown>" > authors.txt
 $ svn log --xml --quiet | grep author | sort -u | perl -pe 's/.*>(.*?)<.*/$1 = /' >> authors.txt
 $ python3 svn-contributors-to-git.py
 
-1. Initialize git repo (preferably use AUTHORS.txt from SVN)
-
-$ mkdir grass-gis-git ; cd grass-gis-git
-$ git svn init --stdlayout https://svn.osgeo.org/grass/grass
-$ git svn --authors-file=path/to/grass_addons/tools/svn2git/AUTHORS.txt
+Run migrate.sh script...
