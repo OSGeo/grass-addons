e7408e61f3ef762f565c529950013da55ecdd6bf
Ondřej Pešek <pesej.ondrek@gmail.com>
2018-10-29 13:17:16 +0000

    follow Matterport's updates
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@73621 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/imagery/i.ann.maskrcnn/maskrcnnlib/model.py
+++ grass7/imagery/i.ann.maskrcnn/maskrcnnlib/model.py
@@ -44,11 +44,12 @@ def log(text, array=None):
     """
     if array is not None:
         text = text.ljust(25)
-        text += ("shape: {:20}  min: {:10.5f}  max: {:10.5f}  {}".format(
-            str(array.shape),
-            array.min() if array.size else "",
-            array.max() if array.size else "",
-            array.dtype))
+        text += ("shape: {:20}  ".format(str(array.shape)))
+        if array.size:
+            text += ("min: {:10.5f}  max: {:10.5f}".format(array.min(), array.max()))
+        else:
+            text += ("min: {:10}  max: {:10}".format("", ""))
+        text += "  {}".format(array.dtype)
     print(text)
 
 
@@ -2832,7 +2833,7 @@ def unmold_image(normalized_images, config):
 #  Miscellenous Graph Functions
 ############################################################
 
-def trim_zeros_graph(boxes, name=None):
+def trim_zeros_graph(boxes, name='trim_zeros'):
     """Often boxes are represented with matrices of shape [N, 4] and
     are padded with zeros. This removes zero boxes.
 
