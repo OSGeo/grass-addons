42362b02bbb1b4bf533e6ebf94fb92d3355f9980
Ondřej Pešek <pesej.ondrek@gmail.com>
2019-02-05 12:10:00 +0000

    i.ann.maskrcnn: skip unnecessary computations, fix typos
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@74058 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- grass7/imagery/i.ann.maskrcnn/maskrcnnlib/config.py
+++ grass7/imagery/i.ann.maskrcnn/maskrcnnlib/config.py
@@ -67,7 +67,7 @@ class ModelConfig(object):
     # Minimum scaling ratio. Checked after MIN_IMAGE_DIM and can force further
     # up scaling. For example, if set to 2 then images are scaled up to double
     # the width and height, or more, even if MIN_IMAGE_DIM doesn't require it.
-    # Howver, in 'square' mode, it can be overruled by IMAGE_MAX_DIM.
+    # However, in 'square' mode, it can be overruled by IMAGE_MAX_DIM.
     IMAGE_MIN_SCALE = 0
 
     # Image mean (RGB)
