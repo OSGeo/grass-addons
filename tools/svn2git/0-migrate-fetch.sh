ef516a6bd8ede852400d5e485aa36078c9d56f45
Martin Landa <landa.martin@gmail.com>
2019-02-09 14:17:49 +0000

    svn2git: split migration script into pieces, split repo into grass and grass-legacy
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@74070 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ tools/svn2git/0-migrate-fetch.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# core repo
+DIR=grass-fetch
+mkdir $DIR ; cd $DIR
+git svn init --stdlayout https://svn.osgeo.org/grass/grass
+git svn --authors-file=../AUTHORS.txt fetch
+
+# addons repo
+DIR=grass-addons-fetch
+mkdir $DIR ; cd $DIR
+git svn init https://svn.osgeo.org/grass/grass-addons
+git svn --authors-file=../AUTHORS.txt fetch
+
+# promo
+DIR=grass-promo-fetch
+mkdir $DIR ; cd $DIR
+git svn init https://svn.osgeo.org/grass/grass-addons
+git svn --authors-file=../AUTHORS.txt fetch
--- tools/svn2git/1-migrate-branches-tags.sh
+++ tools/svn2git/1-migrate-branches-tags.sh
@@ -1,24 +1,34 @@
 #!/bin/sh
 
-# Initialize git repo (preferably use AUTHORS.txt from SVN)
+# TO BE SET
+DIR=grass
+### DIR=grass-legacy
 
-mkdir grass-gis-git
-cd grass-gis-git
-git svn init --stdlayout https://svn.osgeo.org/grass/grass # --no-metadata 
-git svn --authors-file=../AUTHORS.txt fetch
+rm -rf $DIR
+cp -r grass-fetch $DIR
+cd $DIR
 
 # Create local branches
-# git branch develbranch_6 origin/develbranch_6
-for branch in `git branch -r | grep releasebranch | sed 's#  origin/##g'`; do
+if [ $DIR = "grass" ] ; then
+    branch_filter="releasebranch_7"
+else
+    branch_filter="releasebranch_[0-6]"
+fi
+for branch in `git branch -r | grep $branch_filter | sed 's#  origin/##g'`; do
     git branch $branch origin/$branch
 done
-# git checkout develbranch_6
-git branch -D master
-git branch master origin/trunk
-git checkout master
+if [ $DIR = "grass" ] ; then
+    git branch master origin/trunk
+    git checkout master
+fi
 
 # Rename tags
-for i in `git branch -r | grep tags`; do
+if [ $DIR = "grass" ] ; then
+    tag_filter="grass_7"
+else
+    tag_filter="grass_[0-6]"
+fi
+for i in `git branch -r | grep tags | grep $tag_filter`; do
     b=`echo $i | sed 's#origin/##'`
     if [ `echo $i | grep -c release` -gt 0 ] ; then
         j=`echo $i | sed 's#origin/tags/release_[0-9]\+_##g'`
@@ -30,7 +40,10 @@ for i in `git branch -r | grep tags`; do
     h=`git log -1 --format=%h --date=iso $b`
     GIT_COMMITTER_DATE="$d" git tag -a $j -m "Tagging release $j" $h
 done
-git checkout master
+
+if [ $DIR = "grass" ] ; then
+    git checkout master
+fi
 for i in `git branch | grep tags`; do
     git branch -D $i
 done
@@ -40,7 +53,7 @@ for i in `git branch -r | grep origin`; do git branch -dr $i; done
 
 # Fix commit messages (#x -> https://trac.osgeo.org/...)
 git reset --hard HEAD && git checkout master
-SCRIPT=`realpath $0` # realpath is a separate package and doesn't need to be installed
-SCRIPTPATH=`dirname $SCRIPT`
-git filter-branch --msg-filter "python  $SCRIPTPATH/../rewrite.py" -- --all
+#SCRIPT=`realpath $0` # realpath is a separate package and doesn't need to be installed
+#SCRIPTPATH=`dirname $SCRIPT`
+#git filter-branch --msg-filter "python  $SCRIPTPATH/../rewrite.py" -- --all
 # check out /tmp/log_touched.txt and /tmp/log_untoched.txt for changes overview ...
