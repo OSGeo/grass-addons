79739ebf8bd010f2290d4a0cebd8ff1c17037dd6
Martin Landa <landa.martin@gmail.com>
2019-02-17 15:31:14 +0000

    svn2git: grass repo start at https://trac.osgeo.org/grass/changeset/31142
    git-svn-id: https://svn.osgeo.org/grass/grass-addons@74096 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/svn2git/0-migrate-fetch.sh
+++ tools/svn2git/0-migrate-fetch.sh
@@ -3,17 +3,20 @@
 SCRIPT=`realpath $0` # realpath is a separate package and doesn't need to be installed
 SCRIPTPATH=`dirname $SCRIPT`
 
-function fetch() {
+fetch() {
     DIR=$1
     OPT=$2
+    RANGE=$3
     
+    rm -rf $DIR
     mkdir $DIR
     cd $DIR
     git svn init $OPT https://svn.osgeo.org/grass/grass
-    git svn --authors-file=${SCRIPTPATH}/AUTHORS.txt fetch
+    git svn $RANGE --authors-file=${SCRIPTPATH}/AUTHORS.txt fetch
     cd ..
 }
 
-fetch "grass-fetch" "--stdlayout"
+fetch "grass-fetch" "--stdlayout" "-r31142:75000"
+fetch "grass-legacy-fetch" "--stdlayout"
 fetch "grass-addons-fetch"
 fetch "grass-promo-fetch"
