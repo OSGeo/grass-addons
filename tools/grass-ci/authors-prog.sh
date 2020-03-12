84b211f240de38cfb1f44256a9179aad0bdeb1b8
Martin Landa <landa.martin@gmail.com>
2020-03-12 16:53:30 +0100

    remove unused grass-ci scripts (CI is running on https://github.com/OSGeo/grass), the scripts archived at https://svn.osgeo.org/grass/grass-addons/tools/grass-ci/

--- tools/grass-ci/authors-prog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Assign unknown email value to all unknown SVN commiters, which doesn't exist in grass-authors.txt file.
-
-# Author: Ivan Mincik, ivan.mincik@gmail.com
-
-echo "$1 <unknown-email>"
-
-# vim: set ts=4 sts=4 sw=4 noet:
