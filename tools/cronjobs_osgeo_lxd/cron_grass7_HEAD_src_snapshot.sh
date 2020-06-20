0692cd4b385fc14a02cf3d4506d0176fd7be5dcd
Markus Neteler <neteler@gmail.com>
2020-06-20 11:38:38 +0200

    grasslxd server: cronjobs hugo, snapshots, progman, addon manuals (#216)
    scripts to compile on grasslxd server:
    - hugo based web pages
    - source code weekly snapshots
    - Linux binary weekly snapshots
    - addon manuals
    - programmer's manual
    
    rsync configuration for mirrors
    
    Fixes https://github.com/OSGeo/grass-website/issues/171

---
+++ tools/cronjobs_osgeo_lxd/cron_grass7_HEAD_src_snapshot.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+# script to build GRASS sources package from git master of 7
+# (c) GPL 2+ Markus Neteler <neteler@osgeo.org>
+# Markus Neteler 2002, 2003, 2005, 2006, 2007, 2008, 2012, 2015, 2018, 2019, 2020
+#
+# GRASS GIS github, https://github.com/OSGeo/grass
+#
+## prep
+# git clone https://github.com/OSGeo/grass.git master
+#
+###################################################################
+
+MAINDIR=/home/neteler
+GMAJOR=7
+GMINOR=9
+GVERSION=$GMAJOR.$GMINOR.git
+DOTVERSION=$GMAJOR.$GMINOR
+GSHORTGVERSION=$GMAJOR$GMINOR
+
+####################
+# where to find the GRASS sources (git clone):
+SOURCE=$MAINDIR/src/
+BRANCH=master  # releasebranch_7_8
+# where to put the resulting .tar.gz file:
+TARGETMAIN=~/var/www/grass/grass-cms
+TARGET=$TARGETMAIN/grass${GSHORTGVERSION}/source/snapshot
+PACKAGENAME=grass-${GVERSION}_
+
+############################## nothing to change below:
+
+MYMAKE="nice make"
+TAR=tar
+
+# catch CTRL-C and other breaks:
+trap "echo 'user break.' ; exit" 2 3 9 15
+
+halt_on_error()
+{
+  echo "ERROR: $1"
+  exit 1
+}
+
+# create a source code snapshot:
+CWD=`pwd`
+
+mkdir -p $TARGET
+cd $SOURCE/$BRANCH/
+date
+
+#clean up
+$MYMAKE distclean > /dev/null 2>&1
+
+# cleanup leftover garbage
+git status | grep '.rst' | xargs rm -f
+rm -rf lib/python/docs/_build/ lib/python/docs/_templates/layout.html
+rm -f config_${DOTVERSION}.git_log.txt ChangeLog
+
+## hard reset local git repo (just in case)
+#git checkout master && git reset --hard HEAD~1 && git reset --hard origin
+
+echo "git update..."
+git fetch --all --prune       || halt_on_error "git fetch error!"
+# we dont have upstream in this cronjob repo
+git merge origin/$BRANCH
+
+git status
+
+#generate changelog
+touch include/Make/Platform.make # workaround for https://trac.osgeo.org/grass/ticket/3853
+make changelog
+rm -f include/Make/Platform.make
+
+# go to parent for packaging
+cd ..
+
+date
+#package it (we rename the directory to have the date inside the package):
+DATE=`date '+_%Y_%m_%d'`
+mv $BRANCH $PACKAGENAME\src_snapshot$DATE
+# exclude version control system directories
+$TAR cfz $PACKAGENAME\src_snapshot$DATE.tar.gz $PACKAGENAME\src_snapshot$DATE --exclude-vcs
+mv $PACKAGENAME\src_snapshot$DATE $BRANCH
+
+#remove old snapshot:
+rm -f $TARGET/$PACKAGENAME\src_snapshot*
+rm -f $TARGET/ChangeLog.gz
+
+#publish the new one:
+cd $BRANCH/
+cp -p ChangeLog AUTHORS CHANGES CITING COPYING GPL.TXT INSTALL REQUIREMENTS.html $TARGET
+
+cd ..
+gzip $TARGET/ChangeLog
+cp $PACKAGENAME\src_snapshot$DATE.tar.gz $TARGET
+rm -f $PACKAGENAME\src_snapshot$DATE.tar.gz
+chmod a+r,g+w $TARGET/* 2> /dev/null
+chgrp grass $TARGET/*   2> /dev/null
+
+# link for convenience:
+(cd $TARGET ; rm -f $PACKAGENAME\src_snapshot_latest.tar.gz ; ln -s $PACKAGENAME\src_snapshot$DATE.tar.gz $PACKAGENAME\src_snapshot_latest.tar.gz)
+
+echo "Written: https://grass.osgeo.org/grass${GSHORTGVERSION}/source/snapshot/"
+
+exit 0
