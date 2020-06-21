a37640dbbc81bb743462683e697941c1b039ce1a
Martin Landa <landa.martin@gmail.com>
2020-06-21 19:30:32 +0200

    major script update (argparse, py3) WIP

--- tools/addons/build-xml.py
+++ tools/addons/build-xml.py
@@ -5,10 +5,9 @@
 import os
 import sys
 import glob
+import tempfile
+import argparse
 from   datetime import datetime
-
-ADDON_PATH = os.path.join(os.getenv('HOME'), 'src', 'grass-addons')
-DIST = 'dist.x86_64-pc-linux-gnu'
 
 def get_list(addons):
     mlist = [d for d in os.listdir(os.path.join(addons)) if os.path.isdir(os.path.join(addons, d))]
@@ -17,24 +16,17 @@ def get_list(addons):
     mlist.sort()
     return mlist
 
-def get_gui_list(g7 = True):
-    mlist = list()
-    for m in os.listdir(os.path.join(ADDON_PATH, 'grass%s' % ('7' if g7 else '6'),
-                                     'gui', 'wxpython')):
-        if len(m) > 3 and m[:3] == 'wx.':
-            mlist.append(m)
+# def get_gui_list(g7 = True):
+#     mlist = list()
+#     for m in os.listdir(os.path.join(ADDON_PATH, 'grass%s' % ('7' if g7 else '6'),
+#                                      'gui', 'wxpython')):
+#         if len(m) > 3 and m[:3] == 'wx.':
+#             mlist.append(m)
     
-    return mlist
+#     return mlist
                       
-def start_grass(g7 = True):
-    if g7:
-        ver = '78'
-    else:
-        ver = '64'
-    gisbase = os.environ['GISBASE'] = os.path.join(os.getenv('HOME'),
-                                                   "src/grass%s/%s" % (ver, DIST))
-    
-    gisdbase = os.path.join(gisbase)
+def start_grass():
+    gisdbase = tempfile.gettempdir()
     location = "demolocation"
     mapset   = "PERMANENT"
  
@@ -44,112 +36,126 @@ def start_grass(g7 = True):
     
     gsetup.init(gisbase,
                 gisdbase, location, mapset)
- 
-def parse_modules(fd, mlist):
-    indent = 4
-    blacklist = ['v.feature.algebra', 'm.eigensystem']
-    for m in mlist:
-        if m in blacklist:
-            continue # skip blacklisted modules
-        print "Parsing <%s>..." % m,
-        desc, keyw = get_module_metadata(m)
-        fd.write('%s<task name="%s">\n' % (' ' * indent, m))
-        indent += 4
-        fd.write('%s<description>%s</description>\n' % (' ' * indent, desc))
-        fd.write('%s<keywords>%s</keywords>\n' % (' ' * indent, ','.join(keyw)))
-        fd.write('%s<binary>\n' % (' ' * indent))
-        indent += 4
-        for f in get_module_files(m):
-            fd.write('%s<file>%s</file>\n' % (' ' * indent, f))
-        indent -= 4
-        fd.write('%s</binary>\n' % (' ' * indent))
-        indent -= 4
-        fd.write('%s</task>\n' % (' ' * indent))
-        if desc:
-            print " SUCCESS"
-        else:
-            print " FAILED"
-  
-def parse_gui_modules(fd, mlist):
-    indent = 4
-    for m in mlist:
-        print "Parsing <%s>..." % m
-        fd.write('%s<task name="%s">\n' % (' ' * indent, m))
-        fd.write('%s</task>\n' % (' ' * indent))
-    
-def scandirs(path):
-    flist = list()
-    for f in glob.glob(os.path.join(path, '*') ):
-        if os.path.isdir(f):
-            flist += scandirs(f)
-        else:
-            flist.append(f)
-    
-    return flist
+    grass.create_location(gisdbase,
+                          location, overwrite=True)
 
-def get_module_files(name):
-    os.chdir(os.path.join(sys.argv[1], name))
-    return scandirs('*')
-                    
-def get_module_metadata(name):
-    import grass.script.task as gtask
-    path = os.environ['PATH']
-    os.environ['PATH'] += os.pathsep + os.path.join(sys.argv[1], name, 'bin') + os.pathsep + \
-        os.path.join(sys.argv[1], name, 'scripts')
-    try:
-        task = gtask.parse_interface(name)
-    except:
-        task = None
+class BuildXML:
+    def __init__(self, build_path):
+        self.build_path
 
-    os.environ['PATH'] = path
-    if not task:
-        return '', ''
-    
-    return task.get_description(full = True), \
-        task.get_keywords()
+    def run(self):
+        with open(os.path.join(self.build_path, 'modules.xml'), 'w') as fd:
+            self._header(fd)
+            self._parse_modules(fd, get_list(build_path))
+            # self._parse_gui_modules(fd, get_gui_list(g7))
+            self.footer(fd)
 
-def header(fd):
-    import grass.script.core as grass
-    fd.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-    fd.write('<!DOCTYPE task SYSTEM "grass-addons.dtd">\n') # TODO
-    # doesn't work in GRASS 6
-    # vInfo = grass.version()
-    vInfo = grass.parse_command('g.version', flags='g')
-    fd.write('<addons version="%s" revision="%s" date="%s">\n' % \
-                 (vInfo['version'].split('.')[0],
-                  vInfo['revision'],
-                  datetime.now()))
+    def _parse_modules(fd, mlist):
+        indent = 4
+        blacklist = ['v.feature.algebra', 'm.eigensystem']
+        for m in mlist:
+            if m in blacklist:
+                continue # skip blacklisted modules
+            print("Parsing <{}>...".format(m), end='')
+            desc, keyw = self._get_module_metadata(m)
+            fd.write('%s<task name="%s">\n' % (' ' * indent, m))
+            indent += 4
+            fd.write('%s<description>%s</description>\n' % (' ' * indent, desc))
+            fd.write('%s<keywords>%s</keywords>\n' % (' ' * indent, ','.join(keyw)))
+            fd.write('%s<binary>\n' % (' ' * indent))
+            indent += 4
+            for f in self._get_module_files(m):
+                fd.write('%s<file>%s</file>\n' % (' ' * indent, f))
+            indent -= 4
+            fd.write('%s</binary>\n' % (' ' * indent))
+            indent -= 4
+            fd.write('%s</task>\n' % (' ' * indent))
+            if desc:
+                print(" SUCCESS")
+            else:
+                print(" FAILED")
 
+    @staticmethod
+    def parse_gui_modules(fd, mlist):
+        indent = 4
+        for m in mlist:
+            print("Parsing <{}>...".format(m))
+            fd.write('%s<task name="%s">\n' % (' ' * indent, m))
+            fd.write('%s</task>\n' % (' ' * indent))
 
-def footer(fd):
-    fd.write('</addons>\n')
+    def _scandirs(self, path):
+        flist = list()
+        for f in glob.glob(os.path.join(path, '*') ):
+            if os.path.isdir(f):
+                flist += self._scandirs(f)
+            else:
+                flist.append(f)
 
-def main():
-    if len(sys.argv) < 2:
-        sys.exit("Usage: %s path_to_addons" % sys.argv[0])
-        
-    addons = sys.argv[1]
-    try:
-        version = os.path.split(addons)[-2][-1]
-        g7 = version == '7'
-    except:
-        g7 = True
-    
-    path = os.path.join(addons, 'modules.xml')
-    print "-----------------------------------------------------"
-    print "Creating XML file '%s'..." % path
-    print "-----------------------------------------------------"
-    fd = open(path, 'w')
-    start_grass(g7)
+        return flist
 
-    header(fd)
-    parse_modules(fd, get_list(addons))
-    # parse_gui_modules(fd, get_gui_list(g7))
-    footer(fd)
+    def _get_module_files(self, name):
+        os.chdir(os.path.join(self.build_path, name))
+        return scandirs('*')
 
-    fd.close()
+    def _get_module_metadata(self, name):
+        import grass.script.task as gtask
+        path = os.environ['PATH']
+        os.environ['PATH'] += os.pathsep + os.path.join(self.build_path, name, 'bin') + os.pathsep + \
+            os.path.join(self.build_path, name, 'scripts')
+        try:
+            task = gtask.parse_interface(name)
+        except:
+            task = None
+
+        os.environ['PATH'] = path
+        if not task:
+            return '', ''
+
+        return task.get_description(full = True), \
+            task.get_keywords()
+
+    @staticmethod
+    def _header(fd):
+        import grass.script.core as grass
+        fd.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        fd.write('<!DOCTYPE task SYSTEM "grass-addons.dtd">\n') # TODO
+        # doesn't work in GRASS 6
+        # vInfo = grass.version()
+        vInfo = grass.parse_command('g.version', flags='g')
+        fd.write('<addons version="%s" revision="%s" date="%s">\n' % \
+                     (vInfo['version'].split('.')[0],
+                      vInfo['revision'],
+                      datetime.now()))
+
+    @staticmethod
+    def _footer(fd):
+        fd.write('</addons>\n')
+
+def main(build_path):
+    print("-----------------------------------------------------")
+    print("Creating XML file '{}'...".format(path))
+    print("-----------------------------------------------------")
+    start_grass()
+
+    with open(os.path.join(build_path, 'modules.xml'), 'w') as fd:
+        header(fd)
+        parse_modules(fd, get_list(build_path))
+        # parse_gui_modules(fd, get_gui_list(g7))
+        footer(fd)
 
     return 0
 
 if __name__ == "__main__":
-    sys.exit(main())
+    parser = argparse.ArgumentParser()
+    parser.add_argument("build",
+                        help="Path to GRASS Addons build",
+                        required=True)
+    parser.add_argument("gisbase",
+                        help="Path to GRASS installation",
+                        required=True)
+    args = parser.parse_args()
+
+    # set up GRASS gisbase
+    os.environ['GISBASE'] = args.gisbase
+
+    sys.exit(main(args.build))
