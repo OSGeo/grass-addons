a643b22c8e1b9bddf519e781a1a4163d294d79cd
Caitlin H <caitlin.haedrich@gmail.com>
2021-01-23 17:29:50 -0500

    Update r.centroids.py

--- grass7/raster/r.centroids/r.centroids.py
+++ grass7/raster/r.centroids/r.centroids.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 ############################################################################
 #
 # MODULE:    r.centroids
 #
-# AUTHOR(S): Caitlin Haedrich (wrapper for r.volume, caitlin dot haedrich gmail com)
-
+# AUTHOR(S): Caitlin Haedrich (caitlin dot haedrich gmail com) and 
 #
-# PURPOSE:   Creates vector map of centroids from a raster of "clumps"; r.clumps
-#            creates "clumps" of data.
+# PURPOSE:   Wrapper for r.volume. Creates vector map of centroids from a raster 
+#            of "clumps"; r.clumps creates "clumps" of data.
 #
-# COPYRIGHT: (C) 1997-2016 by the GRASS Development Team
+# COPYRIGHT: (C) 2021 by Caitlin Haedrich and the GRASS Development Team
 #
 #            This program is free software under the GNU General Public
 #            License (>=v2). Read the file COPYING that comes with GRASS
@@ -30,36 +28,23 @@
 
 #%option G_OPT_R_INPUT
 #% key: input
-#% description: raster of clumps, cluster of same-valued pixels
+#% description: Raster of clumps, cluster of same-valued pixels
 #% required: yes
 #%end
 
 #%option G_OPT_V_OUTPUT
 #% key: output
-#% description: name of output vector
 #% required: yes
 #%end
 
-
-import sys
 import grass.script as gs
-
-
-def raster_exists(name):
-    """Check if the raster map exists, call GRASS fatal otherwise"""
-    ffile = gs.find_file(name, element='raster')
-    if not ffile['fullname']:
-        gs.fatal(_("Raster map <%s> not found") % name)
 
 def main(options, flags):
     # options and flags into variables
     ipl = options['input']
-    raster_exists(ipl)
-    opl = options['output'] #'testingCentroids'
+    opl = options['output']
 
-    gs.run_command('r.volume', input=ipl, clump=ipl, centroids=opl)
+    gs.run_command('r.volume', input=ipl, clump=ipl, centroids=opl, errors="exit")
 
 if __name__ == "__main__":
-    options, flags = gs.parser()
-    main(options, flags)
-    exit(0)
+    main(gs.parser()[0], gs.parser()[1])
