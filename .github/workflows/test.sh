498262a95a68c384473fbb07f82ec31d0cdb7b9b
Vaclav Petras <wenzeslaus@gmail.com>
2021-03-04 22:56:16 -0500

    Increase min required test success percentage

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -11,4 +11,4 @@ grass --tmp-location XY --exec \
 grass --tmp-location XY --exec \
     python3 -m grass.gunittest.main \
         --grassdata $HOME --location nc_spm_full_v2alpha2 --location-type nc \
-        --min-success 30
+        --min-success 65
