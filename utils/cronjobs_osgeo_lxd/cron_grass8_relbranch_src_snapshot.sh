e92232fdea2cc8cd190283012c276b34d40ee3b4
Markus Neteler <neteler@gmail.com>
2022-01-21 14:12:04 +0100

    cronjob: cleanup file names, add grass8-devel (#682)
    * added cronjob to generate "grass8-devel source" snapshot (missing so far)
    * cleaned up (renaming) file names to better reflect what they actually do
    * updated README.md
    * removed unused old script grass-addons-fetch-xml.sh
    * updated cronjob table (needs to be deployed on server after PR merge)

--- utils/cronjobs_osgeo_lxd/cron_grass8_relbranch_src_snapshot.sh
+++ utils/cronjobs_osgeo_lxd/cron_grass8_relbranch_src_snapshot.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-# script to build GRASS 8.x sources package from the main branch
+# script to build GRASS 8.x sources package from the release branch
 # (c) GPL 2+ Markus Neteler <neteler@osgeo.org>
 # Markus Neteler 2002, 2003, 2005, 2006, 2007, 2008, 2012, 2015, 2018-2022
 #
 # GRASS GIS github, https://github.com/OSGeo/grass
 #
 ## prep
-# git clone https://github.com/OSGeo/grass.git main
+# git clone https://github.com/OSGeo/grass.git release_branch_8_0
 #
 ###################################################################
 
 MAINDIR=/home/neteler
 GMAJOR=8
-GMINOR=1
+GMINOR=0
 GVERSION=$GMAJOR.$GMINOR.git
 DOTVERSION=$GMAJOR.$GMINOR
 GSHORTGVERSION=$GMAJOR$GMINOR
@@ -21,7 +21,7 @@ GSHORTGVERSION=$GMAJOR$GMINOR
 ###################
 # where to find the GRASS sources (git clone):
 SOURCE=$MAINDIR/src/
-BRANCH=main
+BRANCH=releasebranch_8_0
 # where to put the resulting .tar.gz file:
 TARGETMAIN=/var/www/code_and_data/
 TARGETDIR=$TARGETMAIN/grass${GSHORTGVERSION}/source/snapshot
