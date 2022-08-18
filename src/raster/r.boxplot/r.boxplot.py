46b38000ab23a220f445fb27591e84123f67429f
Paulo van Breugel <paulo@ecodiv.earth>
2022-08-18 20:52:47 +0200

    Update r.boxplot.py
    Add option to plot the median of the whole raster as a line, and the IQR of the whole raster as a band (incl. possibility to define their colors)
    The function will set the region to match that of the zonal raster. Optionally, the user can tell the function to respect the region settings.
    Rearrangement of tabs and other minor fixes.

--- src/raster/r.boxplot/r.boxplot.py
+++ src/raster/r.boxplot/r.boxplot.py
@@ -26,13 +26,13 @@
 # % key: input
 # % description: input raster
 # % required: yes
-# % label: input raster
+# % label: Input raster
 # % guisection: Input
 # %end
 
 # %option G_OPT_R_MAP
 # % key: zones
-# % label: zonal raster
+# % label: Zonal raster
 # % description: categorical map with zones
 # % required: no
 # % guisection: Input
@@ -46,40 +46,67 @@
 # %end
 
 # %option
+# % key: plot_dimensions
+# % type: string
+# % label: Plot dimensions (width,height)
+# % description: Dimensions (width,height) of the figure in inches
+# % required: no
+# % guisection: Output
+# %end
+
+# %option
+# % key: dpi
+# % type: integer
+# % label: DPI
+# % description: resolution of plot
+# % required: no
+# % guisection: Output
+# %end
+
+# %option
 # % key: map_outliers
 # % type: string
 # % label: Name of outlier map
 # % description: Create a vector point layer of outliers
-# % guisection: Output
+# % guisection: Outliers
 # % required: no
+# %end
+
+# %option
+# % key: flier_marker
+# % type: string
+# % label: Flier marker
+# % description: flier marker
+# % required: no
+# % guisection: Outliers
+# %end
+
+# %option
+# % key: flier_size
+# % type: string
+# % label: Flier size
+# % description: flier size
+# % required: no
+# % guisection: Outliers
+# %end
+
+# %option G_OPT_C
+# % key: flier_color
+# % label: Flier color
+# % description: flier color
+# % required: no
+# % guisection: Outliers
 # %end
 
 # %flag
 # % key: o
-# % label: include outliers
+# % label: Include outliers
 # % description: Draw boxplot(s) with outliers
-# % guisection: Statistics
+# % guisection: Outliers
 # %end
 
 # %rules
 # % requires: map_outliers, -o
-# %end
-
-# %flag
-# % key: n
-# % label: notch
-# % description: Draw boxplot(s) with notch
-# % guisection: Statistics
-# %end
-
-# %option
-# % key: range
-# % type: double
-# % label: Range (value > 0)
-# % description: this determines how far the plot whiskers extend out from the box. If range is positive, the whiskers extend to the most extreme data point which is no more than range times the interquartile range from the box. A value of zero causes the whiskers to extend to the data extremes.
-# % required: no
-# % answer: 1.5
-# % guisection: Statistics
 # %end
 
 # %option
@@ -98,55 +125,37 @@
 # % type: integer
 # % label: Font size
 # % description: Default font size
-# % guisection: Plot options
+# % guisection: Output
 # % required: no
 # %end
 
 # %flag
 # % key: h
-# % label: horizontal boxplot(s)
+# % label: Horizontal boxplot(s)
 # % description: Draw the boxplot horizontal
 # % guisection: Plot options
 # %end
 
 # %flag
 # % key: r
-# % label: rotate labels
+# % label: Rotate labels
 # % description: rotate labels
 # % guisection: Plot options
 # %end
 
-# %option
-# % key: plot_dimensions
-# % type: string
-# % label: Plot dimensions (width,height)
-# % description: Dimensions (width,height) of the figure in inches
-# % required: no
-# % guisection: Plot options
-# %end
-
-# %option
-# % key: dpi
-# % type: integer
-# % label: DPI
-# % description: resolution of plot
-# % required: no
-# % guisection: Plot options
-# %end
 
 # %option
 # % key: boxplot_width
 # % type: double
-# % label: boxplot width
+# % label: Boxplot width
 # % description: The width of the boxplots (0,1])
 # % required: no
 # % guisection: Plot options
 # %end
 
-
 # %flag
 # % key: c
-# % label: Color boxploxs
+# % label: Color boxplots
 # % description: Color boxploxs using the colors of the categories of the zonal raster
 # % guisection: Plot options
 # %end
@@ -158,6 +167,62 @@
 # % guisection: Plot options
 # %end
 
+# %option G_OPT_C
+# %key: raster_stat_color
+# %label: Color of the raster IQR and median
+# %description: Color of raster IQR and median
+# %required: no
+# %answer: grey
+# %guisection: Plot options
+# %end
+
+# %option
+# % key: raster_stat_alpha
+# % type: double
+# % description: Transparency of the raster IQR band
+# % required: no
+# %options: 0-1
+# %answer: 0.2
+# % guisection: Plot options
+# %end
+
+# %flag
+# % key: n
+# % label: Draw notches
+# % description: Draw boxplot(s) with notch
+# % guisection: Statistics
+# %end
+
+# %option
+# % key: range
+# % type: double
+# % label: Range (value > 0)
+# % description: this determines how far the plot whiskers extend out from the box. If range is positive, the whiskers extend to the most extreme data point which is no more than range times the interquartile range from the box. A value of zero causes the whiskers to extend to the data extremes.
+# % required: no
+# % answer: 1.5
+# % guisection: Statistics
+# %end
+
+# %option
+# % key: raster_statistics
+# % type: string
+# % description: Plot the raster median and IQR
+# % required: no
+# % multiple: yes
+# % options: median, IQR
+# % guisection: Statistics
+# %end
+
+# %flag
+# % key: g
+# % label: Respect region settings
+# % description: Use region instead of extent and resolution of zonal map
+# %end
+
+# %rules
+# % requires: -r, zones
+# %end
+
 # %rules
 # % requires: -c, zones
 # %end
@@ -165,6 +230,7 @@
 # %rules
 # % requires: -s, zones
 # %end
+
 
 import sys
 import atexit
@@ -239,39 +305,10 @@ def check_integer(name):
     """Check if map values are integer"""
     input_info = gs.raster_info(name)
     if input_info["datatype"] != "CELL":
-        gs.fatal(_("The zonal raster must be of type CELL" " (integer)"))
+        gs.fatal(_("The zonal raster must be of type CELL (integer)"))
 
 
-def bxp_nozones(
-    name,
-    outliers,
-    output,
-    dimensions,
-    dpi,
-    vertical,
-    notch,
-    rotate_label,
-    name_outliers_map,
-    whisker_range,
-    fontsize,
-    bxp_width,
-):
-    """Compute the statistics used to create the boxplot,
-    and create the boxplot. This function is used in case
-    no zonal raster is provided."""
-    try:
-        import matplotlib
-
-        matplotlib.use("WXAgg")
-        from matplotlib import pyplot as plt
-    except ImportError as e:
-        raise ImportError(
-            _(
-                'r.boxplot needs the "matplotlib" '
-                "(python-matplotlib) package to be installed. {0}"
-            ).format(e)
-        )
-
+def raster_stats(name):
     # Compute statistics
     quantile_rules = Module(
         "r.quantile",
@@ -290,6 +327,34 @@ def bxp_nozones(
     quantile_2 = float(quantiles[1][1])
     quantile_3 = float(quantiles[2][1])
     max_value = float(quantiles[3][1])
+
+    # Return values
+    return min_value, quantile_1, quantile_2, quantile_3, max_value
+
+
+def bxp_nozones(
+    name,
+    outliers,
+    output,
+    dimensions,
+    dpi,
+    vertical,
+    notch,
+    rotate_label,
+    name_outliers_map,
+    whisker_range,
+    fontsize,
+    bxp_width,
+    flier_size,
+    flier_marker,
+    flier_color,
+):
+    """Compute the statistics used to create the boxplot,
+    and create the boxplot. This function is used in case
+    no zonal raster is provided."""
+
+    # Compute statistics
+    min_value, quantile_1, quantile_2, quantile_3, max_value = raster_stats(name=name)
 
     # Compute iqr and whisker limits
     iqr = whisker_range * (quantile_3 - quantile_1)
@@ -317,8 +382,10 @@ def bxp_nozones(
         upper_notch = ""
 
     # Define recode rules to extract outliers
-    lower_outliers = f"{min_value}:{lower_whisker}:1"
-    upper_outliers = f"{upper_whisker}:{max_value}:1"
+    lower_outlier_bnd = lower_whisker - 0.000000000001
+    lower_outliers = f"{min_value}:{lower_outlier_bnd}:1"
+    upper_outlier_bnd = upper_whisker + 0.000000000001
+    upper_outliers = f"{upper_outlier_bnd}:{max_value}:1"
     if min_value < lower_whisker and max_value > upper_whisker:
         recode_rules = "{}\n{}".format(lower_outliers, upper_outliers)
     elif min_value < lower_whisker:
@@ -385,7 +452,19 @@ def bxp_nozones(
             "cihi": upper_notch,
         }
     ]
-    ax.bxp(boxes, showfliers=True, widths=bxp_width, vert=vertical, shownotches=notch)
+    ax.bxp(
+        boxes,
+        showfliers=True,
+        widths=bxp_width,
+        vert=vertical,
+        shownotches=notch,
+        flierprops={
+            "marker": flier_marker,
+            "markersize": flier_size,
+            "markerfacecolor": flier_color,
+            "markeredgecolor": flier_color,
+        },
+    )
     if vertical:
         ax.set_ylabel(strip_mapset(name))
         ax.axes.get_xaxis().set_visible(False)
@@ -420,31 +499,15 @@ def bxp_zones(
     fontsize,
     show_catnumbers,
     bxp_width,
+    flier_size,
+    flier_marker,
+    flier_color,
+    plot_rast_stats,
+    raster_stat_color,
+    raster_stat_alpha,
 ):
     """Compute the statistics used to create the boxplot,
     and create the boxplots per zone from the zonal map."""
-    try:
-        import matplotlib
-
-        matplotlib.use("WXAgg")
-        from matplotlib import pyplot as plt
-    except ImportError as e:
-        raise ImportError(
-            _(
-                'r.boxplot needs the "matplotlib" '
-                "(python-matplotlib) package to be installed. {0}"
-            ).format(e)
-        )
-
-    # Get labels
-    labels = Module(
-        "r.category", map=zones, separator="pipe", stdout_=PIPE
-    ).outputs.stdout
-    labels = labels.replace("\r", "").split("\n")
-    labels = [_f for _f in labels if _f]
-    labels = [_y for _y in [_x.split("|") for _x in labels]]
-    labelsids = [int(_z[0]) for _z in labels]
-    labels = [_z[0] if len(_z[1]) == 0 else _z[1] for _z in labels]
 
     # Get colors
     if bpcolors:
@@ -457,7 +520,27 @@ def bxp_zones(
             for _x in zones_color
             if not _x.startswith("nv") and not _x.startswith("default")
         ]
-        zones_colorids = [int(_y[0]) for _y in [_x.split(" ") for _x in zones_color]]
+        zones_colorids = [_y[0] for _y in [_x.split(" ") for _x in zones_color]]
+
+        # Check if zonal map has a color table
+        # (or rather, if the category values are integers)
+        if not all([item.isdigit() for item in zones_colorids]):
+            gs.fatal("The zonal map probably does not have a color table")
+        zones_colorids = [int(_x) for _x in zones_colorids]
+
+    # Get labels
+    labels = Module(
+        "r.category", map=zones, separator="pipe", stdout_=PIPE
+    ).outputs.stdout
+    labels = labels.replace("\r", "").split("\n")
+    labels = [_f for _f in labels if _f]
+    labels = [_y for _y in [_x.split("|") for _x in labels]]
+    labelsids = [int(_z[0]) for _z in labels]
+    labels = [_z[0] if len(_z[1]) == 0 else _z[1] for _z in labels]
+
+    # Get colors (continuation)
+    if bpcolors:
+
         # Select the actual raster categories and extract the rgb values
         zones_color = [
             zones_color[id]
@@ -465,10 +548,6 @@ def bxp_zones(
             if zones_colorids[id] in labelsids
         ]
         zones_colorids = [_c for _c in zones_colorids if _c in labelsids]
-        # Check if zonal map has a color table
-        # (or rather, if the category values are integers)
-        if not all([isinstance(item, int) for item in zones_colorids]):
-            gs.fatal(_("The zonal map does not have a color table"))
         zones_color = [_y[1] for _y in [_x.split(" ") for _x in zones_color]]
         zones_color = [_z.split(":") for _z in zones_color]
         zones_rgb = [[int(_x) / 255 for _x in _y] for _y in zones_color]
@@ -556,8 +635,10 @@ def bxp_zones(
             upper_notch = ""
 
         # Construct recode rules to map outliers
-        lower_outliers = f"{min_value}:{lower_whisker}:1"
-        upper_outliers = f"{upper_whisker}:{max_value}:1"
+        lower_outlier_bnd = lower_whisker - 0.000000000001
+        lower_outliers = f"{min_value}:{lower_outlier_bnd}:1"
+        upper_outlier_bnd = upper_whisker + 0.000000000001
+        upper_outliers = f"{upper_outlier_bnd}:{max_value}:1"
         if min_value < lower_whisker and max_value > upper_whisker:
             recode_rules = "{}\n{}".format(lower_outliers, upper_outliers)
         elif min_value < lower_whisker:
@@ -579,10 +660,7 @@ def bxp_zones(
                 stdin_=recode_rules,
             )
             vectornames.append(tmpvect)
-            mask_present = checkmask()
-            if mask_present:
-                tmpmask = create_temporary_name("tmp03")
-                Module("g.rename", raster=["MASK", tmpmask])
+            # Set mask to select outlier cells
             Module("r.mask", raster=zones, maskcats=int(quantstats_i[0]))
             Module(
                 "r.to.vect",
@@ -592,8 +670,7 @@ def bxp_zones(
             )
             Module("g.remove", type="raster", name=tmpname, flags="f")
             Module("r.mask", flags="r")
-            if mask_present:
-                Module("g.rename", raster=[tmpmask, "MASK"])
+            # Get values input raster and write to outlier points
             colname = strip_mapset(name)
             Module("v.what.rast", map=tmpvect, raster=name, column=colname)
             fliers = Module(
@@ -601,7 +678,7 @@ def bxp_zones(
                 sql=f"select {colname} from {tmpvect}",
                 stdout_=PIPE,
             ).outputs.stdout
-            fliers = fliers.split("\n")[1:-1]
+            fliers = list(set(fliers.split("\n")[1:-1]))
             fliers = [float(x) for x in fliers]
         else:
             fliers = []
@@ -641,6 +718,10 @@ def bxp_zones(
     elif name_outliers_map:
         gs.message("\n--> There are no outliers")
 
+    # Remove intermediate layers
+    if outliers and vectornames:
+        Module("g.remove", type="vector", name=vectornames, flags="f")
+
     # Set plot dimensions and fontsize
     if dimensions:
         dimensions = [float(x) for x in dimensions.split(",")]
@@ -654,6 +735,53 @@ def bxp_zones(
 
     # Plot the figure
     _, ax = plt.subplots(figsize=dimensions)
+
+    # Draw raster statistics
+    rast_median_alpha = min(1, raster_stat_alpha + 0.1)
+    if plot_rast_stats and vertical:
+        _, quantile_1_r, quantile_2_r, quantile_3_r, _ = raster_stats(name=name)
+        plot_rast_stats_l = plot_rast_stats.split(",")
+        if "IQR" in plot_rast_stats_l:
+            ax.axhspan(
+                quantile_1_r,
+                quantile_3_r,
+                0,
+                1,
+                alpha=raster_stat_alpha,
+                color=raster_stat_color,
+                linewidth=0.5,
+            )
+        if "median" in plot_rast_stats_l:
+            ax.axhline(
+                quantile_2_r,
+                color=raster_stat_color,
+                linestyle="-",
+                alpha=rast_median_alpha,
+                linewidth=1,
+            )
+    elif plot_rast_stats:
+        _, quantile_1_r, quantile_2_r, quantile_3_r, _ = raster_stats(name=name)
+        plot_rast_stats_l = plot_rast_stats.split(",")
+        if "IQR" in plot_rast_stats_l:
+            ax.axvspan(
+                quantile_1_r,
+                quantile_3_r,
+                0,
+                1,
+                alpha=raster_stat_alpha,
+                color=raster_stat_color,
+                linewidth=0.5,
+            )
+        if "median" in plot_rast_stats_l:
+            ax.axvline(
+                quantile_2_r,
+                color=raster_stat_color,
+                linestyle="-",
+                alpha=rast_median_alpha,
+                linewidth=1,
+            )
+
+    # Draw boxplots
     bxplot = ax.bxp(
         boxes,
         showfliers=True,
@@ -661,6 +789,12 @@ def bxp_zones(
         vert=vertical,
         shownotches=notch,
         patch_artist=bpcolors,
+        flierprops={
+            "marker": flier_marker,
+            "markersize": flier_size,
+            "markerfacecolor": flier_color,
+            "markeredgecolor": flier_color,
+        },
     )
     if bpcolors:
         for patch, color in zip(bxplot["boxes"], zones_rgb):
@@ -687,14 +821,65 @@ def main(options, flags):
     Draws the boxplot of raster values. Optionally, this is done per category
     of a zonal raster layer
     """
+
+    global plt
+    try:
+        import matplotlib
+        from matplotlib.colors import is_color_like
+
+        matplotlib.use("WXAgg")
+        from matplotlib import pyplot as plt
+    except ModuleNotFoundError:
+        gs.fatal(_("matplotlib is not installed"))
+
     # input
     value_raster = options["input"]
     zones_raster = options["zones"]
     if zones_raster:
         check_integer(zones_raster)
+    if not flags["g"] and zones_raster:
+        gs.use_temp_region()
+        Module("g.region", raster=zones_raster)
+
+    if not options["raster_stat_color"]:
+        raster_stat_color = "grey"
+    elif not ":" in options["raster_stat_color"]:
+        raster_stat_color = options["raster_stat_color"]
+    else:
+        raster_stat_color = [
+            int(_x) / 255 for _x in options["raster_stat_color"].split(":")
+        ]
+    if not is_color_like(raster_stat_color):
+        gs.fatal(_("{} is not a valid color".format(options["raster_stat_color"])))
+
+    # Create tmp input to get right categories and colors
+    mask_present = checkmask()
+    if mask_present:
+        tmp_inputlayer = create_temporary_name("tmpinput")
+        Module(
+            "r.mapcalc",
+            expression="{} = {}".format(tmp_inputlayer, zones_raster),
+        )
+        zones_raster = tmp_inputlayer
     output = options["output"]
     fontsize = options["fontsize"]
     bxp_width = options["boxplot_width"]
+    if not options["flier_size"]:
+        flier_size = 2
+    else:
+        flier_size = int(options["flier_size"])
+    if not options["flier_marker"]:
+        flier_marker = "o"
+    else:
+        flier_marker = options["flier_marker"]
+
+    flier_color = options["flier_color"]
+    if not flier_color:
+        flier_color = "black"
+    elif ":" in flier_color:
+        flier_color = [int(_x) / 255 for _x in options["flier_color"].split(":")]
+    if not is_color_like(flier_color):
+        gs.fatal(_("{} is not a valid color".format(options["flier_color"])))
     if bool(bxp_width):
         bxp_width = float(bxp_width)
         if bxp_width > 1 or bxp_width <= 0:
@@ -726,6 +911,12 @@ def main(options, flags):
     outliers = flags["o"]
     rotate_label = flags["r"]
     print_notch = flags["n"]
+    plot_rast_stats = options["raster_statistics"]
+    if not zones_raster and plot_rast_stats:
+        gs.warning(
+            "No zonal layer was defined.\n"
+            "Therefore, the option to plot the raster statistics is ignored"
+        )
 
     # Plot boxplot(s)
     if zones_raster:
@@ -746,6 +937,12 @@ def main(options, flags):
             fontsize=fontsize,
             show_catnumbers=show_catnumbers,
             bxp_width=bxp_width,
+            flier_size=flier_size,
+            flier_marker=flier_marker,
+            flier_color=flier_color,
+            plot_rast_stats=plot_rast_stats,
+            raster_stat_color=raster_stat_color,
+            raster_stat_alpha=float(options["raster_stat_alpha"]),
         )
     else:
         bxp_nozones(
@@ -761,6 +958,9 @@ def main(options, flags):
             whisker_range=whisker_range,
             fontsize=fontsize,
             bxp_width=bxp_width,
+            flier_size=flier_size,
+            flier_marker=flier_marker,
+            flier_color=flier_color,
         )
 
 
