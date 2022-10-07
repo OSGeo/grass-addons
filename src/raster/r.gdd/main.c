d77bfb346baae3a33ef1c38851653d5c2179309e
Vaclav Petras <wenzeslaus@gmail.com>
2022-10-06 21:25:49 -0400

    aggregation as topic keyword for r.gdd (based on r.series topic keyword)

--- src/raster/r.gdd/main.c
+++ src/raster/r.gdd/main.c
@@ -69,6 +69,7 @@ int main(int argc, char *argv[])
 
     module = G_define_module();
     G_add_keyword(_("raster"));
+    G_add_keyword(_("aggregation"));
     G_add_keyword(_("series"));
     G_add_keyword(_("parallel"));
     module->description =
