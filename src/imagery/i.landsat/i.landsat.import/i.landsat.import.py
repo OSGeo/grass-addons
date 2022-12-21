0f4b8cbe3eb27d4a77640c6f68c2f4bccaa3b0f6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-21 10:03:28 +0000

    Adding tarfile member sanitization to extractall()

--- src/imagery/i.landsat/i.landsat.import/i.landsat.import.py
+++ src/imagery/i.landsat/i.landsat.import/i.landsat.import.py
@@ -156,7 +156,26 @@ def _untar(inputdir, untardir):
 
     for scene in scenes_to_untar:
         with tarfile.open(name=scene, mode="r") as tar:
-            tar.extractall(untardir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, untardir)
 
     untared_tifs = glob.glob(os.path.join(untardir, "*.TIF"))
     return untared_tifs
