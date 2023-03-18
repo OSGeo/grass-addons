6a4384f71ec6ccdff8d5fbcfcec50cc9e139eb51
Vaclav Petras <wenzeslaus@gmail.com>
2023-03-17 22:23:16 -0400

    Copy first category, assume layer 1, convert NULLs to zeros

--- src/vector/v.move.points/v.move.points.py
+++ src/vector/v.move.points/v.move.points.py
@@ -34,17 +34,22 @@
 # %option G_OPT_V_OUTPUT
 # %end
 
+import math
+
 import grass.script as gs
 
 
 def move_vector(input_vector_name, output_vector_name, x_raster_name, y_raster_name):
     # Lazy import ctypes
     # pylint: disable=import-outside-toplevel
+    import ctypes
+
     from grass.pygrass.gis.region import Region
     from grass.pygrass.raster import RasterSegment
     from grass.pygrass.utils import coor2pixel
     from grass.pygrass.vector import VectorTopo
     from grass.pygrass.vector.geometry import Line, Point
+    from grass.lib.vector import Vect_cat_set, Vect_cat_get
 
     x_raster = RasterSegment(x_raster_name)
     x_raster.open("r")
@@ -57,14 +62,23 @@ def move_vector(input_vector_name, output_vector_name, x_raster_name, y_raster_n
         output_vector_name, mode="w"
     ) as output_vector:
         for feature in input_vector:
+            first_cat = ctypes.c_int()
+            Vect_cat_get(feature.c_cats, 1, ctypes.byref(first_cat))
             new_point_list = []
             for point in feature:
                 pixel = coor2pixel((point.x, point.y), region)
                 x_value = x_raster[int(pixel[0]), int(pixel[1])]
                 y_value = y_raster[int(pixel[0]), int(pixel[1])]
+                if math.isnan(x_value):
+                    # NULL is zero.
+                    # Implement also null is warning (boolean) and null is error (raise here).
+                    x_value = 0
+                if math.isnan(y_value):
+                    y_value = 0
                 point = Point(point.x + x_value, point.y + y_value)
                 new_point_list.append(point)
             new_line = Line(new_point_list)
+            Vect_cat_set(new_line.c_cats, 1, first_cat.value)
             output_vector.write(new_line)
 
     x_raster.close()
