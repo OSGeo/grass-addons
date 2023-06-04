10de66e14eaa3f29329da4f01c774279d907d2c9
Martin Landa <landa.martin@gmail.com>
2023-06-04 14:31:42 +0200

    WIP

--- .github/workflows/osgeo4w_build_addons.sh
+++ .github/workflows/osgeo4w_build_addons.sh
@@ -10,9 +10,10 @@ export PATH=/c/msys64/usr/bin:${PATH}
 export LANGUAGE=C
 export OSGEO4W_ROOT_MSYS="/c/OSGeo4W"
 
-SRC_PATH=${ADDON_PATH}/src
-GISBASE_PATH=/c/msys64/usr/src
-TARGET_PATH=/c/grass_addons
+SRC_PATH=$(pwd)/src
+# GISBASE_PATH=/c/msys64/usr/src
+TARGET_PATH=$(pwd)/build
+GRASS_PATH=$OSGEO4W_ROOT_MSYS/apps/grass8
 
 fetchenv() {
     local IFS
@@ -32,19 +33,122 @@ fetchenv() {
 }
 
 function compile {
-    SRC_ADDONS=${GISBASE_PATH}/grass${1:0:1}-addons/src
-    SRC_GRASS=${GISBASE_PATH}/grass$1
-    DST_DIR=${TARGET_PATH}/grass$1/addons
+    SRC_PATH="$1"
+    GRASS_PATH="$2"
+    TARGET_PATH="$3"
+    GRASS_VERSION=`echo -n ${SRC_PATH} | tail -c 1`
+    INDEX_FILE="index"
 
-    (cd $SRC_ADDONS; git pull)
+    if [ ! -d "$3" ] ; then
+        mkdir -p "$3"
+    fi
+
+    if [ -n "$4" ] ; then
+        SEP=1 # useful for collecting files (see build-xml.py)
+    else
+        SEP=0
+    fi
+
+    PLATFORM=x86_64
+
+    rm -rf "$TARGET_PATH"
+    mkdir  "$TARGET_PATH"
+
+    cd "$SRC_PATH"
+
+    date=`date -R`
+    uname=`uname`
+    mkdir "$TARGET_PATH/logs"
+    touch "$TARGET_PATH/logs/${INDEX_FILE}.log"
+    echo "<!--<?xml-stylesheet href=\"style.css\" type=\"text/css\"?>-->
+    <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"
+              \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
+
+    <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" >
+
+    <head>
+    <meta http-equiv=\"Content-Type\" content=\"application/xhtml+xml; charset=utf-8\" />
+    <title>GRASS $TARGET_PATH AddOns Logs ($PLATFORM)</title>
+    <style type=\"text/css\">
+    h1 { font-size: 125%; font-weight: bold; }
+    table
+    {
+    border-collapse:collapse;
+    }
+    table,th, td
+    {
+    border: 1px solid black;
+    }
+    </style>
+    </head>
+    <body>
+    <h1>GRASS $GRASS_VERSION AddOns ($PLATFORM) / $uname (logs generated $date)</h1>
+    <hr /> 
+    <table cellpadding=\"5\">
+    <tr><th style=\"background-color: grey\">AddOns</th>
+    <th style=\"background-color: grey\">Status</th>
+    <th style=\"background-color: grey\">Log file</th></tr>" > "$TARGET_PATH/logs/${INDEX_FILE}.html"
+
+    echo "-----------------------------------------------------"
+    echo "AddOns '$TARGET_PATH'..."
+    echo "-----------------------------------------------------"
+
+    pwd=`pwd`
+    for c in "db" "display" "general" "gui/wxpython" "imagery" "misc" "raster" "raster3d" "temporal" "vector" ; do
+        if [ ! -d $c ]; then
+            continue
+        fi
+        cd $c
+        for m in `ls -d */Makefile 2>/dev/null` ; do
+            m="${m%%/Makefile}"
+            echo -n "Compiling $m..."
+            cd "$m"
+            if [ $SEP -eq 1 ] ; then
+                path="$TARGET_PATH/$m"
+            else
+                path="$TARGET_PATH"
+            fi
+
+            export GRASS_ADDON_BASE=$path
+            echo "<tr><td><tt>$c/$m</tt></td>" >> "$TARGET_PATH/logs/${INDEX_FILE}.html"
+            make MODULE_TOPDIR="$GRASS_PATH" clean > /dev/null 2>&1
+            make MODULE_TOPDIR="$GRASS_PATH" \
+                BIN="$path/bin" \
+                HTMLDIR="$path/docs/html" \
+                MANBASEDIR="$path/docs/man" \
+                SCRIPTDIR="$path/scripts" \
+                ETC="$path/etc" \
+                SOURCE_URL="https://github.com/OSGeo/grass-addons/tree/master/grass${GRASS_VERSION}/" > \
+                "$TARGET_PATH/logs/$m.log" 2>&1
+            if [ `echo $?` -eq 0 ] ; then
+                printf "%-30s%s\n" "$c/$m" "SUCCESS" >> "$TARGET_PATH/logs/${INDEX_FILE}.log"
+                echo " SUCCESS"
+                echo "<td style=\"background-color: green\">SUCCESS</td>" >> "$TARGET_PATH/logs/${INDEX_FILE}.html"
+            else
+                printf "%-30s%s\n" "$c/$m" "FAILED" >> "$TARGET_PATH/logs/${INDEX_FILE}.log"
+                echo " FAILED"
+                echo "<td style=\"background-color: red\">FAILED</td>" >> "$TARGET_PATH/logs/${INDEX_FILE}.html"
+            fi
+            echo "<td><a href=\"$m.log\">log</a></td></tr>" >> "$TARGET_PATH/logs/${INDEX_FILE}.html"
+            cd ..
+        done
+        cd $pwd
+        unset GRASS_ADDON_BASE
+    done
+
+    echo "</table><hr />
+    <div style=\"text-align: right\">Valid: <a href=\"http://validator.w3.org/check/referer\">XHTML</a></div>
+    </body></html>" >> "$TARGET_PATH/logs/${INDEX_FILE}.html"
+}
+
+function build_addons {
+    SRC_GRASS=${GISBASE_PATH}/grass$1
     fetchenv $OSGEO4W_ROOT_MSYS/bin/o4w_env.bat
     export PATH=${PATH}:/c/msys64/usr/bin:/c/msys64/mingw64/bin
-    grass_version=`echo $SRC_GRASS | cut -d '/' -f6 | sed 's/grass//g'`
 
-    rm -rf $DST_DIR
-    ls `pwd`
-    ./osgeo4w_compile_addons.sh $SRC_ADDONS $SRC_GRASS $DST_DIR 1
-    cd $DST_DIR
+    rm -rf $TARGET_PATH
+    compile $SRC_PATH $GRASS_PATH $TARGET_PATH 1
+    cd $TARGET_PATH
     for d in `ls -d */`; do
 	mod=${d%%/}
 	if [ $mod == "logs" ] ; then
@@ -55,7 +159,7 @@ function compile {
 	for f in `ls bin/*.bat 2> /dev/null` ; do
 	    echo $f
 	    if [ `echo $1 | sed -e 's/\(^.*\)\(.$\)/\2/'` = "6" ] ; then
-		replace_gisbase="GRASS_ADDON_PATH"
+		replace_gisbase="GRASS_TARGET_PATH"
 	    else
 		replace_gisbase="GRASS_ADDON_BASE"
 	    fi
@@ -71,10 +175,10 @@ function compile {
 
 if test -z $1 ; then
     while read release; do
-        compile $release
+        build_addons $release
     done < releases.csv
 else
-    compile $1
+    build_addons $1
 fi
 
 exit 0
