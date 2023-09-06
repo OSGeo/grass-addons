9b3aa814a30cb90d62a31376bfad1effc1e0578c
Markus Neteler <neteler@gmail.com>
2023-09-06 11:35:10 +0200

    r.pi manual: completion of submodule list and HTML fixes
    - fix citation
    - fix HTML header
    - add missing keywords

--- src/raster/r.pi/r.pi.graph.pr/main.c
+++ src/raster/r.pi/r.pi.graph.pr/main.c
@@ -122,6 +122,8 @@ int main(int argc, char *argv[])
 
     module = G_define_module();
     G_add_keyword(_("raster"));
+    G_add_keyword(_("landscape structure analysis"));
+    G_add_keyword(_("connectivity analysis"));
     module->description =
         _("Graph Theory - iterative removal (patch relevance analysis).");
 
