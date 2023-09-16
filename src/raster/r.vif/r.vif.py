c9a8d6957f5dabe0935fc9c7eef978a42fbc79c9
Paulo van Breugel <paulo@ecodiv.earth>
2023-09-16 14:49:25 +0200

    r.vif.py: add error message
    The error message given when one of the input layers has no values (within the computational region) does not help to understand the problem. Added an error message to suggest the possible cause of the error. Print error messages in color.

--- src/raster/r.vif/r.vif.py
+++ src/raster/r.vif/r.vif.py
@@ -123,6 +123,12 @@ import grass.script as gs
 
 
 # Functions
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+
+
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+
+
 CLEAN_RAST = []
 
 
@@ -348,7 +354,12 @@ def main(options, flags):
                     vifstat = compute_vif2(x, y)
                 else:
                     # Compute vif using sample
-                    y = p[:, k]
+                    try:
+                        y = p[:, k]
+                    except IndexError as e:
+                        prRed(f"An error occurred: {str(e)}")
+                        prGreen("Tip: check if all input rasters have values"
+                              " within the computation region.")
                     x = np.delete(p, k, axis=1)
                     vifstat = compute_vif(x, y)
 
