14c166988d2efc17e51530040185003349d7edb5
Ondrej Pesek <pesej.ondrek@gmail.com>
2023-11-08 16:51:47 +0100

    apply black

--- src/imagery/i.landsat/i.landsat.import/i.landsat.import.py
+++ src/imagery/i.landsat/i.landsat.import/i.landsat.import.py
@@ -171,9 +171,9 @@ def _untar(inputdir, untardir):
                     member_path = os.path.join(path, member.name)
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
-            
-                tar.extractall(path, members, numeric_owner=numeric_owner) 
-                
+
+                tar.extractall(path, members, numeric_owner=numeric_owner)
+
             safe_extract(tar, untardir)
 
     untared_tifs = glob.glob(os.path.join(untardir, "*.TIF"))
