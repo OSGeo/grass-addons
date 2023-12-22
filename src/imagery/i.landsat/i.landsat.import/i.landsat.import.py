4ad1d02782fc7f08ab6c9ebe2b9b7f6b4335b13d
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2023-12-22 00:16:56 -0500

    Update src/imagery/i.landsat/i.landsat.import/i.landsat.import.py
    Co-authored-by: Markus Neteler <neteler@osgeo.org>

--- src/imagery/i.landsat/i.landsat.import/i.landsat.import.py
+++ src/imagery/i.landsat/i.landsat.import/i.landsat.import.py
@@ -173,7 +173,7 @@ def _untar(inputdir, untardir):
                 for member in tar.getmembers():
                     member_path = os.path.join(path, member.name)
                     if not is_within_directory(path, member_path):
-                        raise Exception("Attempted Path Traversal in Tar File")
+                        raise Exception(_("Attempted path traversal in tar file"))
 
                 tar.extractall(path, members, numeric_owner=numeric_owner)
 
