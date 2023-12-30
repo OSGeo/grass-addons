a5fd00fd73d0c6b9c19d04e17d7d22d0182c388b
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2023-12-30 16:52:30 +0000

    Add comments to function parameters in SWE.h

--- src/raster/r.damflood/SWE.h
+++ src/raster/r.damflood/SWE.h
@@ -7,21 +7,20 @@ and method=3
 
 returns void
 */
-
 void shallow_water(
-    double **m_h1, double **m_u1,
-    double **m_v1,      /* water depth and velocities of the i step*/
-    float **m_z,        /* DTM */
-    float **m_DAMBREAK, /* DTM changes (e.g. DTM) */
-    float **m_m,        /* manning coefficient */
-    int **m_lake, /* lake filter, default>0, if equal to 0 --> do not apply
-                     swe!!!*/
-    double **m_h2, double **m_u2,
-    double **m_v2, /* water depth and velocities of the i+1 step*/
-    int row, int col, int nrows, int ncols, /* matrix size*/
-    float timestep, /* timestep (normally optimized with another function)  */
-    float res_ew, float res_ns,  /* grid resolutions*/
-    int method,                  /* default = 3, various hypothesis*/
-    int num_cell, int num_break, /* number of cell of lake only in case of
-                                    method 1 or 2, elsewhere 0*/
-    double t);                   /* computational instant */
+    double **m_h1,                /* water depth of the i step */
+    double **m_u1, double **m_v1, /* water velocities of the i step */
+    float **m_z,                  /* DTM */
+    float **m_DAMBREAK,           /* DTM changes (e.g. DTM) */
+    float **m_m,                  /* manning coefficient */
+    int **m_lake, // lake filter, default>0, if equal to 0 --> do not apply swe!
+    double **m_h2,                /* water depth of the i+1 step */
+    double **m_u2, double **m_v2, /* water velocities of the i+1 step */
+    int row, int col, int nrows, int ncols, /* matrix size */
+    float timestep, /* timestep (normally optimized with another function) */
+    float res_ew, float res_ns,  /* grid resolutions */
+    int method,                  /* default = 3, various hypothesis */
+    int num_cell, int num_break, /* number of cells of lake only in case of
+                                    method 1 or 2, elsewhere 0 */
+    double t                     /* computational instant */
+);
