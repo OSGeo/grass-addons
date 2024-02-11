90679be7ab289b06e5507a96afe56041332472d3
Paulo van Breugel <paulo@ecodiv.earth>
2024-02-11 15:52:01 +0100

    r.maxent.predict: work in progress

--- src/raster/r.maxent.predict/r.maxent.predict.py
+++ src/raster/r.maxent.predict/r.maxent.predict.py
@@ -30,148 +30,34 @@
 # % key: lambdaFile
 # % label: Lambda model file
 # % description: Lambda model file created by Maxent or the r.maxent.train addon.
-# % guisection: Input
+# % guisection: input
 # % required: yes
+# %end
+
+# %option
+# % key: evp_maps
+# % type: string
+# % description: Environmental parameter map(s)
+# % required : yes
+# % multiple : yes
+# % gisprompt: old,cell,raster
+# % guisection: input
+# %end
+
+# %option
+# % key: alias_names
+# % type: string
+# % description: Alias names for environmental parameter(s)
+# % required : no
+# % guisection: input
 # %end
 
 # %option G_OPT_F_BIN_INPUT
-# % key: environmentallayersfile
-# % label: Sample file with background locations
-# % description:  Please enter the file name of the SWD file with environmental variables (can be created with v.maxent.swd or r.out.maxent_swd).
-# % guisection: Input
-# % required: yes
-# %end
-
-# %option
-# % key: togglelayertype
-# % type: string
-# % label: Prefix that identifies categorical data
-# % description: Toggle continuous/categorical for environmental variables whose names begin with this prefix (default: all continuous)
-# % guisection: Input
-# %end
-
-# %option G_OPT_M_DIR
-# % key: projectionlayers
-# % label: Location of an alternate set of environmental variables.
-# % description: Location of an alternate set of environmental variables. Maxent models will be projected onto these variables. The result will be imported in grass gis.
-# % guisection: Input
+# % key: alias_file
+# % label: Alias file
+# % description: A csv file with in the first column the names of the explanatory variables and the second column with the names of the names of the corresponding raster layers.
+# % guisection: input
 # % required: no
-# %end
-
-# %option
-# % key: suffix
-# % type: string
-# % label: Suffix for name(s) of prediction layer(s)
-# % description: Add a suffix to the name(s) of imported prediction layer(s)
-# % guisection: Input
-# %end
-
-# %option
-# % key: nodata
-# % type: integer
-# % label: Nodata values
-# % description: Value to be interpreted as nodata values in SWD sample data
-# % answer : -9999
-# % required: no
-# % guisection: Input
-# %end
-
-# %option G_OPT_M_DIR
-# % key: outputdirectory
-# % label: Directory where outputs will be written.
-# % description: Directory where outputs will be written. This should be different from the environmental layers directory.
-# % guisection: Output
-# % required: yes
-# %end
-
-# %flag
-# % key: y
-# % label: Create a vector point layer from the sample predictions
-# % description: Import the the file(s) with sample predictions as point feature layer.
-# % guisection: Output
-# %end
-
-# %option G_OPT_V_OUTPUT
-# % key: samplepredictions
-# % label: Name of sample prediction layer
-# % description: Give the name of sample prediction layer. If you leave this empty, the default name given by Maxent will be used.
-# % guisection: Output
-# % required: no
-# %end
-
-# %rules
-# % requires: samplepredictions, -y
-# %end
-
-# %flag
-# % key: b
-# % label: Create a vector point layer with predictions at background points
-# % description: Create a vector point layer with predictions at background points
-# % guisection: Output
-# %end
-
-# %option G_OPT_V_OUTPUT
-# % key: backgroundpredictions
-# % label: Name of background prediction layer
-# % description: Give the name of background prediction layer. If you leave this empty, the default name given by Maxent will be used.
-# % guisection: Output
-# % required: no
-# %end
-
-# %rules
-# % requires: backgroundpredictions, -b
-# %end
-
-# %option G_OPT_R_OUTPUT
-# % key: predictionlayer
-# % label: Name of raster prediction layer
-# % description: Give the name of raster prediction layer. If you leave this empty, the default name given by Maxent will be used.
-# % guisection: Output
-# % required: no
-# %end
-
-# %rules
-# % requires: predictionlayer, projectionlayers
-# %end
-
-# %flag
-# % key: g
-# % label: Create response curves.
-# % description: Create graphs showing how predicted relative probability of occurrence depends on the value of each environmental variable.
-# % guisection: Output
-# %end
-
-# %flag
-# % key: w
-# % label: Write response curve data to file
-# % description: Write output files containing the data used to make response curves, for import into external plotting software.
-# % guisection: Output
-# %end
-
-# %option
-# % key: outputformat
-# % type: string
-# % label: Representation probability
-# % description: Representation of probabilities used in writing output grids. See Help for details.
-# % options: cloglog,logistic,cumulative,raw
-# % answer: cloglog
-# % guisection: Parameters
-# %end
-
-# %option
-# % key: betamultiplier
-# % type: double
-# % label: Multiply all automatic regularization parameters by this number.
-# % description: Multiply all automatic regularization parameters by this number. A higher number gives a more spread-out distribution.
-# % answer: 1.0
-# % guisection: Parameters
-# %end
-
-# %flag
-# % key: e
-# % label: Extrapolate
-# % description: Predict to regions of environmental space outside the limits encountered during training.
-# % guisection: Parameters
 # %end
 
 # %flag
@@ -192,218 +78,14 @@
 # % excludes: -c, -f
 # %end
 
-# %flag
-# % key: l
-# % label: Disable linear features
-# % description: Do not use linear features for the model (they are used by default).
-# % guisection: Parameters
-# %end
-
-# %flag
-# % key: q
-# % label: Disable quadratic features
-# % description: Do not use quadratic features for the model (they are used by default).
-# % guisection: Parameters
-# %end
-
-# %flag
-# % key: p
-# % label: Disable product features
-# % description: Do not use product features for the model (they are used by default).
-# % guisection: Parameters
-# %end
-
-# %flag
-# % key: t
-# % label: Use product features
-# % description: By default, threshold features are not used. Use this flag to enable them.
-# % guisection: Parameters
-# %end
-
-# %flag
-# % key: h
-# % label: Disable hinge features
-# % description: Do not use hinge features for the model (they are used by default).
-# % guisection: Parameters
-# %end
-
-# %flag
-# % key: a
-# % label: Do not use automatic selection of feature classes
-# % description: By default, Maxent automatically select which feature classes to use, based on number of training samples. Use this flag to disable autoselection of features.
-# % guisection: Parameters
-# %end
-
-# %flag
-# % key: n
-# % label: Don't add sample points to background if conditions differ
-# % description: By default, samples that have a combination of environmental values that isn't already present in the background are added to the background samples. Use this flag to avoid that.
-# % guisection: Parameters
-# %end
-
-# %flag
-# % key: j
-# % label: Use jacknife validation
-# % description: Measure importance of each environmental variable by training with each environmental variable first omitted, then used in isolation.
-# % guisection: Validation
-# %end
-
-# %option
-# % key: randomtestpoints
-# % type: integer
-# % label: Percentage of random test points
-# % description: Percentage of presence localities to be randomly set aside as test points, used to compute AUC, omission etc.
-# % answer: 0
-# % guisection: Validation
-# %end
-
-# %option G_OPT_V_INPUT
-# % key: testsamplesfile
-# % type: string
-# % label: Test presence locations
-# % description: Use the presence localities in this csv file to compute statistics (AUC, omission etc.).
-# % guisection: Validation
-# % required: no
-# %end
-
-# %option
-# % key: replicatetype
-# % type: string
-# % label: Number of replicates in cross-validation
-# % description: If replicates > 1, do multiple runs using crossvalidate,bootstrap or subsample. See the Maxent help file for the difference.
-# % guisection: Validation
-# % options: crossvalidate,bootstrap,subsample
-# % answer: crossvalidate
-# %end
-
-# %option
-# % key: replicates
-# % type: integer
-# % label: Number of replicates in cross-validation
-# % description: If replicates > 1, do multiple runs of this type: Crossvalidate: samples divided into replicates folds; each fold in turn used for test data. Bootstrap: replicate sample sets chosen by sampling with replacement. Subsample: replicate sample sets chosen by removing random test percentage without replacement to be used for evaluation.
-# % guisection: Validation
-# % answer: 1
-# % options:1-20
-# %end
-
-# %option
-# % key: maximumiterations
-# % type: integer
-# % label: Maximum iterations optimization
-# % description: Stop training after this many iterations of the optimization algorithm.
-# % answer: 500
-# % guisection: Advanced
-# %end
-
-# %option
-# % key: convergencethreshold
-# % type: double
-# % label: Convergence threshold
-# % description: Stop training when the drop in log loss per iteration drops below this number.
-# % answer: 0.00005
-# % guisection: Advanced
-# %end
-
-# %option
-# % key: lq2lqptthreshold
-# % type: integer
-# % label: Theshold for product and threshold features
-# % description: Number of samples at which product and threshold features start being used.
-# % answer: 80
-# % guisection: Advanced
-# %end
-
-# %option
-# % key: l2lqthreshold
-# % type: integer
-# % label: Theshold for quadratic feature
-# % description: Number of samples at which quadratic features start being used.
-# % answer: 10
-# % guisection: Advanced
-# %end
-
-# %option
-# % key: hingethreshold
-# % type: integer
-# % label: Theshold for hinge feature
-# % description: Number of samples at which hinge features start being used.
-# % answer: 15
-# % guisection: Advanced
-# %end
-
-# %option
-# % key: beta_threshold
-# % type: double
-# % label: Regularization parameter for treshold features
-# % description: Regularization parameter to be applied to all threshold features; negative value enables automatic setting.
-# % answer: -1.0
-# % guisection: Advanced
-# %end
-
-# %option
-# % key: beta_categorical
-# % type: double
-# % label: Regularization parameter for categorical features
-# % description: Regularization parameter to be applied to all categorical features; negative value enables automatic setting.
-# % answer: -1.0
-# % guisection: Advanced
-# %end
-
-# %option
-# % key: beta_lqp
-# % type: double
-# % label: Regularization parameter for lin, quad en prod features
-# % description: Regularization parameter to be applied to all linear, quadratic and product features; negative value enables automatic setting.
-# % answer: -1.0
-# % guisection: Advanced
-# %end
-
-# %option
-# % key: beta_hinge
-# % type: double
-# % label: Regularization parameter for hinge features
-# % description: Regularization parameter to be applied to all linear, quadratic and product features; negative value enables automatic setting.
-# % answer: -1.0
-# % guisection: Advanced
-# %end
-
-# %option
-# % key: defaultprevalence
-# % type: double
-# % label: Default prevalence of the species
-# % description: Default prevalence of the species: probability of presence at ordinary occurrence points. See Elith et al., Diversity and Distributions, 2011 for details.
-# % answer: 0.5
-# % options: 0-1
-# % guisection: Advanced
-# %end
-
-# %flag
-# % key: d
-# % label: Keep duplicate presence records.
-# % description: Keep duplicate presence records. If environmental data are in grids, duplicates are records in the same grid cell. Otherwise, duplicates are records with identical coordinates.
-# % guisection: Advanced
-# %end
-
-# %flag
-# % key: s
-# % label: Use a random seed
-# % description: If selected, a different random seed will be used for each run, so a different random test/train partition will be made and a different random subset of the background will be used, if applicable.
-# % guisection: Advanced
-# %end
-
-# %flag
-# % key: x
-# % label: Add all samples to the background
-# % description: Add all samples to the background, even if they have combinations of environmental values that are already present in the background
-# % guisection: Advanced
-# %end
-
 # %option G_OPT_F_BIN_INPUT
 # % key: maxent
 # % label: Location Maxent jar file
 # % description: Give the path to the Maxent executable file (maxent.jar)
 # % required: no
 # %end
+
+
 
 # %option G_OPT_M_NPROCS
 # % key: threads
@@ -421,12 +103,6 @@
 # %end
 
 # %flag
-# % key: m
-# % label: Do not autorun Maxent
-# % description: When you select this option, Maxent will not start before you hit the start option. This will set the -v flag to true (overriding user choice).
-# %end
-
-# %flag
 # % key: i
 # % label: Copy maxent.jar to addon directory
 # % description: Copy the maxent.jar (path provided with the 'maxent' parameter) to the addon scripts directory.
@@ -436,11 +112,6 @@
 # % key: u
 # % label: Overwrites maxent.jar in addon directory
 # % description: Copy the maxent.jar (path provided with the 'maxent' parameter) to the addon scripts directory. If the file already exist in the addon directory, it is overwritten.
-# %end
-
-# %rules
-# % exclusive: replicates,randomtestpoints
-# % exclusive: -i, -u
 # %end
 
 # import libraries
@@ -564,8 +235,19 @@ def main(options, flags):
             )
             gs.fatal(_(msg))
 
+
+
+
+options = {"lambdaFile":"Bradypus_tridactylus.lambdas", "evp_maps":"bio02,bio04",
+           "alias_names":"bio02,bio04", "alias_file":"alias.csv", "maxent":"maxent.jar",
+           "threads":8, "memory":30000}
+flags = {"c":False, "f":False, "v":False, "i":False, "u":False}
+
+
+
     # Check variable names in swd files and environmental layers
     # ------------------------------------------------------------------
+
     envir_layers = options["environmentallayersfile"]
     sample_layers = options["samplesfile"]
     with open(envir_layers) as f:
