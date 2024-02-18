ecea2aa3494326b5f60952d4209560c5bfb5d2e9
Huidae Cho <grass4u@gmail.com>
2024-02-18 08:12:28 -0700

    r.flowaccumulation: Support FCELL and DCELL outputs (#1027)
    * r.flowaccumulation: Support FCELL and DCELL outputs
    
    * Consistent labels for -z and -Z

--- src/raster/r.flowaccumulation/accumulate.c
+++ src/raster/r.flowaccumulation/accumulate.c
@@ -1,164 +1,114 @@
-#include <stdlib.h>
 #include <grass/raster.h>
 #include "global.h"
 
-#define ACCUM(row, col) accum_map->cells[(size_t)(row)*ncols + (col)]
-#define FIND_UP(row, col)                                                     \
-    ((row > 0 ? (col > 0 && DIR(row - 1, col - 1) == SE ? NW : 0) |           \
-                    (DIR(row - 1, col) == S ? N : 0) |                        \
-                    (col < ncols - 1 && DIR(row - 1, col + 1) == SW ? NE : 0) \
-              : 0) |                                                          \
-     (col > 0 && DIR(row, col - 1) == E ? W : 0) |                            \
-     (col < ncols - 1 && DIR(row, col + 1) == W ? E : 0) |                    \
-     (row < nrows - 1                                                         \
-          ? (col > 0 && DIR(row + 1, col - 1) == NE ? SW : 0) |               \
-                (DIR(row + 1, col) == N ? S : 0) |                            \
-                (col < ncols - 1 && DIR(row + 1, col + 1) == NW ? SE : 0)     \
-          : 0))
-
-#ifdef USE_LESS_MEMORY
-#define UP(row, col) FIND_UP(row, col)
-#else
-#define UP(row, col) up_cells[(size_t)(row)*ncols + (col)]
-static unsigned char *up_cells;
-#endif
-
-static int nrows, ncols;
-
-static void trace_down(struct raster_map *, struct raster_map *, int, int, int);
-static int sum_up(struct raster_map *, int, int, int);
-
-void accumulate(struct raster_map *dir_map, struct raster_map *accum_map)
+void accumulate(struct raster_map *dir_map, struct raster_map *accum_map,
+                int check_overflow, int use_less_memory, int use_zero)
 {
-    int row, col;
-
-    nrows = dir_map->nrows;
-    ncols = dir_map->ncols;
-
-#ifndef USE_LESS_MEMORY
-    up_cells = calloc((size_t)nrows * ncols, sizeof *up_cells);
-
-#pragma omp parallel for schedule(dynamic) private(col)
-    for (row = 0; row < nrows; row++) {
-        for (col = 0; col < ncols; col++)
-            if (!Rast_is_c_null_value(&DIR(row, col)))
-                UP(row, col) = FIND_UP(row, col);
+    switch (accum_map->type) {
+    case CELL_TYPE:
+        if (check_overflow) {
+            if (use_less_memory) {
+                if (use_zero)
+                    accumulate_comz(dir_map, accum_map);
+                else
+                    accumulate_com(dir_map, accum_map);
+            }
+            else {
+                if (use_zero)
+                    accumulate_coz(dir_map, accum_map);
+                else
+                    accumulate_co(dir_map, accum_map);
+            }
+        }
+        else {
+            if (use_less_memory) {
+                if (use_zero)
+                    accumulate_cmz(dir_map, accum_map);
+                else
+                    accumulate_cm(dir_map, accum_map);
+            }
+            else {
+                if (use_zero)
+                    accumulate_cz(dir_map, accum_map);
+                else
+                    accumulate_c(dir_map, accum_map);
+            }
+        }
+        break;
+    case FCELL_TYPE:
+        if (check_overflow) {
+            if (use_less_memory) {
+                if (use_zero)
+                    accumulate_fomz(dir_map, accum_map);
+                else
+                    accumulate_fom(dir_map, accum_map);
+            }
+            else {
+                if (use_zero)
+                    accumulate_foz(dir_map, accum_map);
+                else
+                    accumulate_fo(dir_map, accum_map);
+            }
+        }
+        else {
+            if (use_less_memory) {
+                if (use_zero)
+                    accumulate_fmz(dir_map, accum_map);
+                else
+                    accumulate_fm(dir_map, accum_map);
+            }
+            else {
+                if (use_zero)
+                    accumulate_fz(dir_map, accum_map);
+                else
+                    accumulate_f(dir_map, accum_map);
+            }
+        }
+        break;
+    default:
+        if (check_overflow) {
+            if (use_less_memory) {
+                if (use_zero)
+                    accumulate_domz(dir_map, accum_map);
+                else
+                    accumulate_dom(dir_map, accum_map);
+            }
+            else {
+                if (use_zero)
+                    accumulate_doz(dir_map, accum_map);
+                else
+                    accumulate_do(dir_map, accum_map);
+            }
+        }
+        else {
+            if (use_less_memory) {
+                if (use_zero)
+                    accumulate_dmz(dir_map, accum_map);
+                else
+                    accumulate_dm(dir_map, accum_map);
+            }
+            else {
+                if (use_zero)
+                    accumulate_dz(dir_map, accum_map);
+                else
+                    accumulate_d(dir_map, accum_map);
+            }
+        }
+        break;
     }
-#endif
-
-#pragma omp parallel for schedule(dynamic) private(col)
-    for (row = 0; row < nrows; row++) {
-        for (col = 0; col < ncols; col++)
-            /* if the current cell is not null and has no upstream cells, start
-             * tracing down */
-            if (!Rast_is_c_null_value(&DIR(row, col)) && !UP(row, col))
-                trace_down(dir_map, accum_map, row, col, 1);
-    }
-
-#ifndef USE_LESS_MEMORY
-    free(up_cells);
-#endif
 }
 
-static void trace_down(struct raster_map *dir_map, struct raster_map *accum_map,
-                       int row, int col, int accum)
+void nullify_zero(struct raster_map *accum_map)
 {
-    int up, accum_up = 0;
-
-    /* accumulate the current cell itself */
-    ACCUM(row, col) = accum;
-
-    /* find the downstream cell */
-    switch (DIR(row, col)) {
-    case NW:
-        row--;
-        col--;
+    switch (accum_map->type) {
+    case CELL_TYPE:
+        nullify_zero_c(accum_map);
         break;
-    case N:
-        row--;
+    case FCELL_TYPE:
+        nullify_zero_f(accum_map);
         break;
-    case NE:
-        row--;
-        col++;
-        break;
-    case W:
-        col--;
-        break;
-    case E:
-        col++;
-        break;
-    case SW:
-        row++;
-        col--;
-        break;
-    case S:
-        row++;
-        break;
-    case SE:
-        row++;
-        col++;
+    default:
+        nullify_zero_d(accum_map);
         break;
     }
-
-    /* if the downstream cell is null or any upstream cells of the downstream
-     * cell have never been visited, stop tracing down */
-    if (row < 0 || row >= nrows || col < 0 || col >= ncols ||
-        Rast_is_c_null_value(&DIR(row, col)) || !(up = UP(row, col)) ||
-        !(accum_up = sum_up(accum_map, row, col, up)))
-        return;
-
-    /* use gcc -O2 or -O3 flags for tail-call optimization
-     * (-foptimize-sibling-calls) */
-    trace_down(dir_map, accum_map, row, col, accum_up + 1);
-}
-
-/* if any upstream cells have never been visited, 0 is returned; otherwise, the
- * sum of upstream accumulation is returned */
-static int sum_up(struct raster_map *accum_map, int row, int col, int up)
-{
-    int sum = 0, accum;
-
-#pragma omp flush(accum_map)
-    if (up & NW) {
-        if (!(accum = ACCUM(row - 1, col - 1)))
-            return 0;
-        sum += accum;
-    }
-    if (up & N) {
-        if (!(accum = ACCUM(row - 1, col)))
-            return 0;
-        sum += accum;
-    }
-    if (up & NE) {
-        if (!(accum = ACCUM(row - 1, col + 1)))
-            return 0;
-        sum += accum;
-    }
-    if (up & W) {
-        if (!(accum = ACCUM(row, col - 1)))
-            return 0;
-        sum += accum;
-    }
-    if (up & E) {
-        if (!(accum = ACCUM(row, col + 1)))
-            return 0;
-        sum += accum;
-    }
-    if (up & SW) {
-        if (!(accum = ACCUM(row + 1, col - 1)))
-            return 0;
-        sum += accum;
-    }
-    if (up & S) {
-        if (!(accum = ACCUM(row + 1, col)))
-            return 0;
-        sum += accum;
-    }
-    if (up & SE) {
-        if (!(accum = ACCUM(row + 1, col + 1)))
-            return 0;
-        sum += accum;
-    }
-
-    return sum;
 }
--- src/raster/r.flowaccumulation/global.h
+++ src/raster/r.flowaccumulation/global.h
@@ -19,17 +19,101 @@ int gettimeofday(struct timeval *, struct timezone *);
 #define N             64
 #define NE            128
 
-#define DIR(row, col) dir_map->cells[(size_t)(row)*ncols + (col)]
+#define DIR(row, col) dir_map->cells.uint8[(size_t)(row)*ncols + (col)]
 
 struct raster_map {
+    RASTER_MAP_TYPE type;
+    size_t cell_size;
     int nrows, ncols;
-    CELL *cells;
+    union {
+        void *v;
+        unsigned char *uint8;
+        CELL *c;
+        FCELL *f;
+        DCELL *d;
+    } cells;
 };
 
 /* timeval_diff.c */
 long long timeval_diff(struct timeval *, struct timeval *, struct timeval *);
 
 /* accumulate.c */
-void accumulate(struct raster_map *, struct raster_map *);
+void accumulate(struct raster_map *, struct raster_map *, int, int, int);
+void nullify_zero(struct raster_map *);
+
+/* accumulate_c.c */
+void accumulate_c(struct raster_map *, struct raster_map *);
+void nullify_zero_c(struct raster_map *);
+
+/* accumulate_co.c */
+void accumulate_co(struct raster_map *, struct raster_map *);
+
+/* accumulate_cm.c */
+void accumulate_cm(struct raster_map *, struct raster_map *);
+
+/* accumulate_cz.c */
+void accumulate_cz(struct raster_map *, struct raster_map *);
+
+/* accumulate_com.c */
+void accumulate_com(struct raster_map *, struct raster_map *);
+
+/* accumulate_coz.c */
+void accumulate_coz(struct raster_map *, struct raster_map *);
+
+/* accumulate_cmz.c */
+void accumulate_cmz(struct raster_map *, struct raster_map *);
+
+/* accumulate_comz.c */
+void accumulate_comz(struct raster_map *, struct raster_map *);
+
+/* accumulate_f.c */
+void accumulate_f(struct raster_map *, struct raster_map *);
+void nullify_zero_f(struct raster_map *);
+
+/* accumulate_fo.c */
+void accumulate_fo(struct raster_map *, struct raster_map *);
+
+/* accumulate_fm.c */
+void accumulate_fm(struct raster_map *, struct raster_map *);
+
+/* accumulate_fz.c */
+void accumulate_fz(struct raster_map *, struct raster_map *);
+
+/* accumulate_fom.c */
+void accumulate_fom(struct raster_map *, struct raster_map *);
+
+/* accumulate_foz.c */
+void accumulate_foz(struct raster_map *, struct raster_map *);
+
+/* accumulate_fmz.c */
+void accumulate_fmz(struct raster_map *, struct raster_map *);
+
+/* accumulate_fomz.c */
+void accumulate_fomz(struct raster_map *, struct raster_map *);
+
+/* accumulate_d.c */
+void accumulate_d(struct raster_map *, struct raster_map *);
+void nullify_zero_d(struct raster_map *);
+
+/* accumulate_do.c */
+void accumulate_do(struct raster_map *, struct raster_map *);
+
+/* accumulate_dm.c */
+void accumulate_dm(struct raster_map *, struct raster_map *);
+
+/* accumulate_dz.c */
+void accumulate_dz(struct raster_map *, struct raster_map *);
+
+/* accumulate_dom.c */
+void accumulate_dom(struct raster_map *, struct raster_map *);
+
+/* accumulate_doz.c */
+void accumulate_doz(struct raster_map *, struct raster_map *);
+
+/* accumulate_dmz.c */
+void accumulate_dmz(struct raster_map *, struct raster_map *);
+
+/* accumulate_domz.c */
+void accumulate_domz(struct raster_map *, struct raster_map *);
 
 #endif
