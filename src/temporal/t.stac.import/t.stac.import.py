f4c31e932127d1c50b4b537131287d66915ae017
Corey White <ctwhite48@gmail.com>
2024-02-20 20:13:58 -0500

    Started added static catalog functionality

--- src/temporal/t.stac.import/t.stac.import.py
+++ src/temporal/t.stac.import/t.stac.import.py
@@ -27,16 +27,34 @@
 # % required: yes
 # %end
 
+# %option
+# % key: request_method
+# % type: string
+# % required: yes
+# % multiple: no
+# % options: GET,POST
+# % answer: POST
+# % description:  The HTTP method to use when making a request to the service.
+# %end
+
+# %flag
+# % key: s
+# % description: Is static catalog
+# %end
+
 # %flag
 # % key: c
 # % description: Get the available collections then exit
-# % guisection: Request
 # %end
 
 # %flag
 # % key: i
 # % description: Get the available items from collections then exit
-# % guisection: Request
+# %end
+
+# %flag
+# % key: d
+# % description: (Dry-run) Get the available assets from collections then before download and then exit
 # %end
 
 # %option
@@ -123,18 +141,20 @@
 # %end
 
 # %option
+# % key: filter_lang
+# % type: string
+# % required: no
+# % multiple: no
+# % options: cql2-json,cql2-text
+# % description: Language variant used in the filter body. If filter is a dictionary or not provided, defaults to ‘cql2-json’. If filter is a string, defaults to cql2-text.
+# % guisection: Request
+# %end
+
+# %option
 # % key: sortby
 # % description: A single field or list of fields to sort the response by
 # % required: no
 # % guisection: Request
-# %end
-
-# %option G_OPT_R_OUTPUT
-# % key: output
-# % description: The output raster
-# % required: no
-# % multiple: no
-# % guisection: Output
 # %end
 
 # %option G_OPT_STRDS_OUTPUT
@@ -174,12 +194,18 @@
 
 import os
 import sys
+import json
+import requests
 
 # from multiprocessing.pool import ThreadPool
 from pystac_client import Client
+from pystac_client.exceptions import APIError
 from pystac import MediaType
+from pystac import Catalog
+
 import grass.script as gs
 from grass.exceptions import CalledModuleError
+from urllib3.exceptions import NewConnectionError
 
 try:
     from pystac_client import Client
@@ -210,12 +236,16 @@ def validate_collections_option(client, collections=[]):
     Returns:grass r.import unable to determine number raster bands
         boolean: Returns true if the collection is available.
     """
-    available_collections = client.get_collections()
+    try:
+        available_collections = client.get_collections()
+    except APIError as e:
+        gs.fatal(_("Error getting collections: {}".format(e)))
+
     available_collections_ids = [c.id for c in list(available_collections)]
 
-    gs.warning(_(f"Available Collections: {available_collections_ids}"))
+    gs.message(_(f"Available Collections: {available_collections_ids}"))
 
-    if collections in available_collections_ids:
+    if all(item in available_collections_ids for item in collections):
         return True
 
     for collection in collections:
@@ -227,7 +257,12 @@ def validate_collections_option(client, collections=[]):
 
 def search_stac_api(client, **kwargs):
     """Search the STAC API"""
-    search = client.search(**kwargs)
+    try:
+        search = client.search(**kwargs)
+    except APIError as e:
+        gs.fatal(_("Error searching STAC API: {}".format(e)))
+    except NotImplementedError as e:
+        gs.fatal(_("Error searching STAC API: {}".format(e)))
     gs.message(_(f"{search.matched()} items found"))
     return search
 
@@ -305,39 +340,86 @@ def download_assets(urls, filenames, resample_method, memory, nprocs=1):
 
 def get_all_collections(client):
     """Get a list of collections from STAC Client"""
-    collections = client.get_collections()
-    collection_list = list(collections)
-    gs.message(_(f"{len(collection_list)} collections found:"))
-    for i in collection_list:
-        gs.message(_(i.id))
-    return collection_list
+    try:
+        collections = client.get_collections()
+        collection_list = list(collections)
+        gs.message(_(f"Collections found: {len(collection_list)}"))
+        for i in collection_list:
+            gs.message(_(i.id))
+        return collection_list
+    except APIError as e:
+        gs.fatal(_("Error getting collections: {}".format(e)))
 
 
-def print_summary(data):
-    """Print summary of json data"""
+def generate_indentation(depth):
+    """Generate indentation for summary"""
+    return "    " * depth
+
+
+def print_summary(data, depth=1):
+    """Print summary of json data recursively increasing indentation."""
     for key, value in data.items():
+        indentation = generate_indentation(depth)
         if isinstance(value, dict):
-            gs.message(_(f"{key}:"))
-            for subkey, subvalue in value.items():
-                gs.message(_(f"  {subkey}: {subvalue}"))
+            gs.message(_(f"#\n# {indentation}{key}:"))
+            print_summary(value, depth=depth + 1)
         else:
-            gs.message(_(f"{key}: {value}"))
+            gs.message(_(f"# {indentation}{key}: {value}"))
 
 
 def get_collection_items(client, collection_name):
     """Get collection"""
-    collection = client.get_collection(collection_name)
-    gs.message(_(f"Collection: {collection.title}"))
+    try:
+        collection = client.get_collection(collection_name)
+    except APIError as e:
+        gs.fatal(_(f"Error getting collection {collection_name}: {e}"))
+
+    gs.message(_("*" * 80))
+    gs.message(_(f"Collection Id: {collection.id}"))
+
+    try:
+        gs.message(_(f"Collection Title: {collection.title}"))
+    except AttributeError:
+        gs.info(_("Collection Title not found."))
+
+    gs.message(_(f"Collection Title: {collection.title}"))
     gs.message(_(f"Description: {collection.description}"))
     gs.message(_(f"Spatial Extent: {collection.extent.spatial.bboxes}"))
     gs.message(_(f"Temporal Extent: {collection.extent.temporal.intervals}"))
-    gs.message(_(f"License: {collection.license}"))
+
+    try:
+        gs.message(_(f"License: {collection.license}"))
+    except AttributeError:
+        gs.info(_("License information not found."))
+
+    try:
+        gs.message(_(f"keywords: {collection.keywords}"))
+    except AttributeError:
+        gs.info(_("Keywords not found."))
+
     gs.message(_(f"Links: {collection.links}"))
-    gs.message(_("Properties..."))
-    print_summary(collection.extra_fields)
-    return collection
+
+    try:
+        gs.message(_(f"Extensions: {collection.stac_extensions}"))
+    except AttributeError:
+        gs.info(_("Extensions not found."))
+
+    try:
+        gs.message(_("\n# Summaries:"))
+        print_summary(collection.summaries.to_dict())
+    except AttributeError:
+        gs.info(_("Summaries not found."))
+
+    try:
+        gs.message(_("\n# Extra Fields:"))
+        print_summary(collection.extra_fields)
+    except AttributeError:
+        gs.info(_("# Extra Fields not found."))
+
+    return collection.to_dict()
 
 
+# TODO: Complete functionality to import all media types
 def import_items(
     items,
     region_params=None,
@@ -353,11 +435,37 @@ def import_items(
     asset_name_list = []
 
     for item in items:
+        gs.message(_(f"Collection ID: {item.collection_id} \n"))
         gs.message(_(f"Item: {item.id}"))
-        gs.message(_(f"Spatial Extent: {item.geometry} \n"))
-        gs.message(_(f"Temporal Extent: {item.datetime} \n"))
-        gs.message(_(f"Assets: {item.assets} \n"))
+        gs.message(_(f"Geometry: {item.geometry} \n"))
+        gs.message(_(f"Bbox: {item.bbox} \n"))
 
+        try:
+            gs.message(_(f"Datetime: {item.datetime} \n"))
+        except AttributeError:
+            try:
+                gs.message(_(f"Start Datetime: {item.start_datetime} \n"))
+            except AttributeError:
+                gs.info(_("Start Datetime not found."))
+            try:
+                gs.message(_(f"End Datetime: {item.end_datetime} \n"))
+            except AttributeError:
+                gs.info(_("End Datetime not found."))
+
+        gs.message(_(f"Links: {item.links} \n"))
+
+        try:
+            gs.message(_(f"Extra Fields: {item.extra_fields} \n"))
+        except AttributeError:
+            gs.info(_("Extra Fields not found."))
+
+        try:
+            gs.message(_(f"Extensions: {item.stac_extensions}"))
+        except AttributeError:
+            gs.info(_("Extensions not found."))
+
+        gs.message(_(f"Properties: {item.properties} \n"))
+        # print_summary(item.to_dict())
         for key, asset in item.assets.items():
             media_type = asset.media_type
             # gs.message(_(f"Asset {key}: {media_type} \n"))
@@ -368,7 +476,7 @@ def import_items(
                 asset_name = os.path.splitext(os.path.basename(url))[0]
                 asset_name_list.append(f"{item.id}.{asset_name}")
 
-                gs.message(_(f"Added asset to download queue: {asset.to_dict()} \n"))
+                gs.message(_(f"Asset added to download queue: {asset.to_dict()} \n"))
             # if media_type == MediaType.GEOTIFF:
             # asset_download_list.append(asset.href)
             # gs.message(_(f"Added asset to download queue: {asset} \n"))
@@ -384,30 +492,32 @@ def import_items(
 
             # gs.message(_(f"Asset: {item.assets[asset]} \n"))
 
-        gs.message(_(f"Links: {item.links} \n"))
-        gs.message(_(f"Properties: {item.properties} \n"))
-        gs.message(_(f"Collection ID: {item.collection_id} \n"))
         gs.message(_("*" * 80))
 
-    gs.message(_(f"Asset Download List: {asset_download_list} \n"))
+    # gs.message(_(f"Asset Download List: {asset_download_list} \n"))
     resample_method_list = [method] * len(asset_download_list)
     memory_list = [memory] * len(asset_download_list)
-    gs.message(_(f"Asset Name List: {asset_name_list} \n"))
-    gs.message(_(f"Asset Resample List: {resample_method_list} \n"))
-    gs.message(_(f"Asset Memory List: {memory_list} \n"))
 
     return [asset_download_list, asset_name_list, resample_method_list, memory_list]
 
 
 def main():
     """Main function"""
+
     search_params = {}  # Store STAC API search parameters
-    client_url = options["url"]
-    ids = options["ids"]  # item ids optional
-    collections = options["collections"]  # Maybe limit to one?
+
+    # STAC Client options
+    client_url = options["url"]  # required
+    request_method = options["request_method"]  # required
+    collection_input = options["collections"]  # Maybe limit to one?
+    collections = collection_input.split(",")
     limit = int(options["limit"])  # optional
     max_items = int(options["max_items"])  # optional
     bbox = options["bbox"]  # optional
+    datetime = options["datetime"]  # optional
+    query = options["query"]  # optional
+    filter = options["filter"]  # optional
+    filter_lang = options["filter_lang"]  # optional
 
     intersects = options["intersects"]  # optional
     if intersects:
@@ -423,44 +533,97 @@ def main():
             f.close()
         os.remove(output_geojson)
 
-    datetime = options["datetime"]  # optional
-    if datetime:
-        search_params["datetime"] = datetime
-
-    query = options["query"]  # optional
-    filter = options["filter"]  # optional
-    filter_lang = options["filter_lang"]  # optional
-
     # GRASS import options
     method = options["method"]  # optional
     memory = int(options["memory"])  # optional
     nprocs = int(options["nprocs"])
-    resolution = options["resolution"]  # optional
+    # resolution = options["resolution"]  # optional
 
     # Output options
     strds_output = options["strds_output"]  # optional
-    output = options["output"]  # optional
+    # output = options["output"]  # optional
 
-    client = Client.open(client_url)
-    gs.message(_(f"Catalog: {client.title}"))
+    try:
+        client = Client.open(client_url)
+        gs.message(_(f"Client Id: {client.id}"))
+        gs.message(_(f"Client Title: {client.title}"))
+        gs.message(_(f"Client Title: {client.description}"))
+        gs.message(_(f"Client STAC Extensions: {client.stac_extensions}"))
+        gs.message(_(f"Client Extra Fields: {client.extra_fields}"))
+        gs.message(_(f"Client links: {client.links}"))
+        gs.message(_(f"Client catalog_type: {client.catalog_type}"))
+    except APIError as e:
+        gs.fatal(_("APIError Error opening STAC API: {}".format(e)))
 
+    # Is static catalog not STAC API
+    # TODO: Finish figuring out how to handle static catalogs
+    is_static_catalog = flags["s"]
+    if is_static_catalog:
+        gs.message(_(f"Fetching Static Catalog: {client_url}"))
+        try:
+            client = Catalog.from_file(client_url)
+            gs.message(_(f"Catalog: {client.to_dict()}"))
+            collections = client.get_collections()
+            collection_list = list(collections)
+            gs.message(_(f"Collections: {collection_list}"))
+
+            for collection in collection_list:
+                gs.message(_(f"Collection: {collection}"))
+                collection_items = collection.get_items()
+                item_list = list(collection_items)
+                gs.message(_(f"Items: {item_list}"))
+        except requests.exceptions.RequestException as e:
+            gs.fatal(_("Error making request to STAC API: {}".format(e)))
+        except ValueError as e:
+            gs.fatal(_("Error decoding STAC catalog: {}".format(e)))
+
+    # Parse flags
     collections_only = flags["c"]
     collection_itmes_only = flags["i"]
+    dry_run = flags["d"]
 
     if collections_only:
         get_all_collections(client)
         return None
 
     if collection_itmes_only:
-        get_collection_items(client, collections)
+        for collection in collections:
+            get_collection_items(client, collection)
         return None
+
+    if options["ids"]:
+        ids = options["ids"]  # item ids optional
+        search_params["ids"] = ids.split(",")
 
     # Set the bbox to the current region if the user did not specify the bbox or intersects option
     if not bbox and not intersects:
         gs.message(_("Setting bbox to current region: {}".format(bbox)))
         bbox = region_to_wgs84_decimal_degrees_bbox()
 
+    if datetime:
+        search_params["datetime"] = datetime
+
+    # Add filter to search_params
+    # https://github.com/stac-api-extensions/filter
+    if filter:
+        if isinstance(filter, str):
+            filter = json.loads(filter)
+        if isinstance(filter, dict):
+            search_params["filter"] = filter
+
+    if filter_lang:
+        search_params["filter_lang"] = filter_lang
+
+    if query:
+        if isinstance(query, str):
+            query = json.loads(query)
+        if isinstance(query, dict):
+            search_params["query"] = query
+        if isinstance(query, list):
+            search_params["query"] = query
+
     # Add search parameters to search_params
+    search_params["method"] = request_method
     search_params["collections"] = collections
     search_params["limit"] = limit
     search_params["max_items"] = max_items
@@ -472,10 +635,17 @@ def main():
         asset_list, asset_name_list, method_list, memory_list = import_items(
             list(items_search.items()), method=method, memory=memory
         )
-        download_assets(asset_list, asset_name_list, method_list, memory_list, nprocs)
-        if strds_output:
-            create_strds(strds_output, asset_name_list)
-            # Register the space time raster dataset
+        gs.message(_(f"{len(asset_list)} Assets Ready for download..."))
+
+        if not dry_run:
+            download_assets(
+                asset_list, asset_name_list, method_list, memory_list, nprocs
+            )
+            if strds_output:
+                create_strds(strds_output, asset_name_list)
+                # TODO: Register the space time raster dataset
+    else:
+        gs.warning(_(f"Invalid Collections: {collections}"))
 
 
 if __name__ == "__main__":
