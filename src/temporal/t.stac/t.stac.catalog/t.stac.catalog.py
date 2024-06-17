535e92cda1122d723798a8965f09da46a70c7ddf
Corey White <ctwhite48@gmail.com>
2024-06-17 08:53:29 -0400

    added format option

--- src/temporal/t.stac/t.stac.catalog/t.stac.catalog.py
+++ src/temporal/t.stac/t.stac.catalog/t.stac.catalog.py
@@ -38,9 +38,15 @@
 # % description:  The HTTP method to use when making a request to the service.
 # %end
 
-# %flag
-# % key: b
-# % description: basic info
+# %option
+# % key: format
+# % type: string
+# % required: no
+# % multiple: no
+# % options: json,plain
+# % description: Output format
+# % guisection: Output
+# % answer: json
 # %end
 
 # %option
@@ -72,16 +78,20 @@
 # % guisection: Authentication
 # %end
 
+# %flag
+# % key: b
+# % description: Return basic information only
+# %end
+
 import sys
 from pprint import pprint
 import grass.script as gs
 from grass.pygrass.utils import get_lib_path
 
-try:
-    from pystac_client import Client
-    from pystac_client.exceptions import APIError
-except ImportError:
-    from pystac_client import Client
+# Import STAC Client
+from pystac_client import Client
+from pystac_client.exceptions import APIError
+
 
 path = get_lib_path(modname="t.stac", libname="staclib")
 if path is None:
@@ -106,6 +116,7 @@ def main():
 
     # STAC Client options
     client_url = options["url"]  # required
+    format = options["format"]  # optional
 
     # Flag options
     basic_info = flags["b"]  # optional
@@ -123,20 +134,24 @@ def main():
     try:
         client = Client.open(client_url, headers=req_headers)
 
-        if basic_info:
+        if format == "plain":
             gs.message(_(f"Client Id: {client.id}"))
             gs.message(_(f"Client Title: {client.title}"))
             gs.message(_(f"Client Description: {client.description}"))
             gs.message(_(f"Client STAC Extensions: {client.stac_extensions}"))
             gs.message(_(f"Client Extra Fields: {client.extra_fields}"))
             gs.message(_(f"Client catalog_type: {client.catalog_type}"))
-
+            gs.message(_(f"{'-' * 75}\n"))
             # Get all collections
             collection_list = get_all_collections(client)
-            if basic_info:
+            if not basic_info:
                 gs.message(_(f"Collections: {len(collection_list)}\n"))
                 for i in collection_list:
                     gs.message(_(f"{i.get('id')}: {i.get('title')}"))
+                    gs.message(_(f"{i.get('description')}"))
+                    gs.message(_(f"Extent: {i.get('extent')}"))
+                    gs.message(_(f"License: {i.get('license')}"))
+                    gs.message(_(f"{'-' * 75}\n"))
                 return None
         else:
             pprint(client.to_dict())
