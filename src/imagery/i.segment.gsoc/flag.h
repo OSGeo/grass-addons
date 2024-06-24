e3574646e1c53e12c5781dc8b25cfd13b9c6939e
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-06-24 12:59:11 -0400

    style: apply clang-format 17 to code base

--- src/imagery/i.segment.gsoc/flag.h
+++ src/imagery/i.segment.gsoc/flag.h
@@ -49,13 +49,13 @@ FLAG
 };
 
 #define FLAG_UNSET(flags, row, col) \
-    (flags)->array[(row)][(col) >> 3] &= ~(1 << ((col)&7))
+    (flags)->array[(row)][(col) >> 3] &= ~(1 << ((col) & 7))
 
 #define FLAG_SET(flags, row, col) \
-    (flags)->array[(row)][(col) >> 3] |= (1 << ((col)&7))
+    (flags)->array[(row)][(col) >> 3] |= (1 << ((col) & 7))
 
 #define FLAG_GET(flags, row, col) \
-    (flags)->array[(row)][(col) >> 3] & (1 << ((col)&7))
+    (flags)->array[(row)][(col) >> 3] & (1 << ((col) & 7))
 
 /* flag.c */
 int flag_clear_all(FLAG *);
