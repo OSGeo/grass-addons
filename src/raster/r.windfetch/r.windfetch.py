18a5e23457345269b2005ee2d6913b5ac2e045d8
Anna Petrasova <kratochanna@gmail.com>
2024-08-01 14:03:36 -0400

    fix formatting

--- src/raster/r.windfetch/r.windfetch.py
+++ src/raster/r.windfetch/r.windfetch.py
@@ -128,7 +128,9 @@ def point_fetch(land, coordinates, direction, step, minor_directions, minor_step
     offset = minor_step * (minor_directions - 1) / 2
     output = []
     for point in json.loads(data):
-        distances_dict = {horizons["azimuth"]: horizons['distance'] for horizons in point["horizons"]}
+        distances_dict = {
+            horizons["azimuth"]: horizons["distance"] for horizons in point["horizons"]
+        }
         output.append({"x": point["x"], "y": point["y"], "directions": [], "fetch": []})
         i = 0
         while i < 360:
