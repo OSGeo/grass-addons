7bbd8051a38689885a89f25fd804ca9af125d837
Corey White <smortopahri@gmail.com>
2024-08-19 13:13:37 -0400

    t.stac.import: Add STAC API import functionality (#802)
    * r.in.stac: inital setup of module structure options and flags
    
    * Fixed formatting issues
    
    * r.in.stac: added queries for collections and items
    
    * Organized options
    
    * t.stac.import: Changed module name to t.stac.import
    
    * t.stac.import: Changed module name to t.stac.import
    
    * renamed grass to gs
    
    * added get items function
    
    * Added filter by bbox and user region
    
    * Started the import and download fucntionallity
    
    * fixed typo
    
    * Added download method for COGs via https, s3, and gs
    
    * Added query docs
    
    * Updated multiprocessing options
    
    * Started added static catalog functionality
    
    * Updated docs
    
    * Updated documentation
    
    * Added authentication methods
    
    * Added planetary computer support
    
    * Cleaned code formatting
    
    * Added ability to filter items assets  by asset ids
    
    * Improved collection outputs
    
    * Added additional file types
    
    * Started restructuring code into different modules
    
    * Added t.stac.item
    
    * Updated dependencies
    
    * Fixed tests
    
    * Added catalog tool
    
    * Updted collection
    
    * Updated docs
    
    * Updated options
    
    * Updated copyright
    
    * Can import item vector boundary metadata
    
    * Updated docs
    
    * Added assest metadata vector
    
    * Set s3 download parameters
    
    * added format option
    
    * Fixed item asset metadata
    
    * Removed  t.stac.import
    
    * Added delete to tmpfile creation
    
    * Removed references to t.stac.import
    
    * Added mocked api tests
    
    * Added checks for conformance classes
    
    * Removed auth options from GUI
    
    * Remove username param
    
    * updated pystac and pystac-client versions, documenation and settings file
    
    * Updated docs and basic output
    
    * Added basic collection output
    
    * Added option to create strd registry
    
    * Updated max_limit to no default value
    
    * Updated max_items default value
    
    * Removed broken mock api tests
    
    * Fixed formatting error
    
    ---------
    
    Co-authored-by: Corey White <ctwhite48@gmail.com>
    Co-authored-by: Veronica Andreo <veroandreo@gmail.com>

---
+++ src/temporal/t.stac/t.stac.catalog/testsuite/test_t_stac_catalog.py
@@ -0,0 +1,76 @@
+# #!/usr/bin/env python3
+
+# ############################################################################
+# #
+# # NAME:      t_stac_catalog
+# #
+# # AUTHOR:    Corey T. White
+# #
+# # PURPOSE:   This is a test file for t.stac.catalog
+# #
+# # COPYRIGHT: (C) 2024 by Corey T. White and the GRASS Development Team
+# #
+# #            This program is free software under the GNU General Public
+# #            License (>=v2). Read the file COPYING that comes with GRASS
+# #            for details.
+# #
+# #############################################################################
+
+# # Dependencies
+# # import importlib.util
+
+# from grass.gunittest.case import TestCase
+# from grass.gunittest.main import test
+# from grass.gunittest.gmodules import SimpleModule
+# from pystac_client import Client
+# import json
+# from unittest.mock import patch
+
+# # spec = importlib.util.spec_from_file_location(
+# #     name="stac_lib", location="t.stac.collection.py"
+# # )
+
+# # stac_lib = importlib.util.module_from_spec(spec)
+# # spec.loader.exec_module(stac_lib)
+
+
+# # Tests
+# class TestStacCatalog(TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         """Ensures expected computational region"""
+#         cls.url = "https://earth-search.aws.element84.com/v1/"
+#         # to not override mapset's region (which might be used by other tests)
+#         cls.use_temp_region()
+#         # cls.runModule or self.runModule is used for general module calls
+#         cls.runModule("g.region", raster="elevation")
+#         with open("data/catalog.json") as f:
+#             cls.json_format_expected = json.load(f)
+
+#     @classmethod
+#     def tearDownClass(cls):
+#         """Remove temporary region"""
+#         cls.del_temp_region()
+
+#     # @patch("grass.gunittest.case.TestCase.assertModule")
+#     @patch("pystac_client.Client.open")
+#     @patch("pystac_client.Client.get_collections")
+#     def test_plain_output_json(self, MockClientOpen, MockClientGetCollections):
+#         """Test t.stac.catalog formated as json"""
+#         mock_instance = MockClientOpen.return_value
+#         mock_instance.client = Client.from_dict(self.json_format_expected)
+#         mock_client_collection = MockClientGetCollections.return_value
+#         mock_client_collection.get_collections = mock_instance.get_collections()
+#         self.assertModule("t.stac.catalog", url=self.url, format="json")
+#         self.assertEqual(mock_instance.outputs.stdout, self.json_format_expected)
+
+#     @patch("grass.gunittest.case.TestCase.assertModule")
+#     def test_plain_output_basic_info_flag(self, MockAssertModule):
+#         """Testing format as plain basic info"""
+#         mock_instance = MockAssertModule.return_value
+#         mock_instance.outputs.stdout = json.dumps(self.json_format_expected)
+#         self.assertModule("t.stac.catalog", url=self.url, format="plain", flags="b")
+
+
+# if __name__ == "__main__":
+#     test()
