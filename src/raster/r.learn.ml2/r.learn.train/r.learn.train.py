45e7e6a8125dc060ea5f311258ee80610290770d
rohannallamadge <rohannallamadge@gmail.com>
2024-08-25 00:41:34 +0530

    Fix-#1175-changes-version-of-sklearn-version==1.2.2

--- src/raster/r.learn.ml2/r.learn.train/r.learn.train.py
+++ src/raster/r.learn.ml2/r.learn.train/r.learn.train.py
@@ -445,11 +445,11 @@ def main():
     try:
         import sklearn
 
-        if sklearn.__version__ < "0.20":
-            gs.fatal("Package python3-scikit-learn 0.20 or newer is not installed")
+        if sklearn.__version__ < "1.2.2":
+            gs.fatal("Package python3-scikit-learn 1.2.2 or newer is not installed")
 
     except ImportError:
-        gs.fatal("Package python3-scikit-learn 0.20 or newer is not installed")
+        gs.fatal("Package python3-scikit-learn 1.2.2 or newer is not installed")
 
     try:
         import pandas as pd
