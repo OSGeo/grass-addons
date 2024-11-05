f792edc0de93eb48a1a701db8d3f54bdd0672dcc
Markus Neteler <neteler@osgeo.org>
2024-11-05 09:35:47 +0100

    manual: multiple http/https fixes (#1234)
    * manual: multiple http/https fixes
    * selected URL updates
    * multiple http/https fixes
    * fix NCSU GeoForAll Lab URLs

--- src/imagery/i.segment.uspo/i.segment.uspo.py
+++ src/imagery/i.segment.uspo/i.segment.uspo.py
@@ -19,14 +19,14 @@
 # (2006),
 # Parameter selection for region-growing image segmentation algorithms using
 # spatial autocorrelation, International Journal of Remote Sensing, Vol. 27, Iss.
-# 14, pp. 3035-3040, http://dx.doi.org/10.1080%2f01431160600617194
+# 14, pp. 3035-3040, https://doi.org/10.1080%2f01431160600617194
 #
 # B. A.  Johnson, M. Bragais, I. Endo, D. B. Magcale-Macandog, P. B. M. Macandog
 # (2015),
 # Image Segmentation Parameter Optimization Considering Within- and
 # Between-Segment Heterogeneity at Multiple Scale Levels: Test Case for Mapping
 # Residential Areas Using Landsat Imagery, ISPRS International Journal of
-# Geo-Information, 4(4), pp. 2292-2305, http://dx.doi.org/10.3390/ijgi4042292
+# Geo-Information, 4(4), pp. 2292-2305, https://doi.org/10.3390/ijgi4042292
 #############################################################################
 
 # %Module
