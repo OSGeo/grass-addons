9963b614d7fcb1a0dfad68cb1baca07a1941d5ec
Markus Neteler <neteler@gmail.com>
2025-02-20 09:50:27 +0100

    include function to update timestamp of link to the source timestamp

--- utils/cronjobs_osgeo_lxd/hugo_clean_and_update_job.sh
+++ utils/cronjobs_osgeo_lxd/hugo_clean_and_update_job.sh
@@ -26,10 +26,29 @@
 #  6. restore timestamps of links from their original time stamps in src directory
 ####
 
+# function to update timestamp of link to the source timestamp
+fix_link_timestamp()
+{
+ if [ -z "$1" ] ; then
+   echo 'ERROR: Parameter missing. Specify the folder (. for current)!'
+   exit
+ fi
+
+ for mylink in $(find . -type l) ; do
+  LINK="$(namei ${mylink} | grep '^ l ' | tr -s ' ' ' ' | cut -d' ' -f3)"
+  ORIG="$(namei ${mylink} | grep '^ l ' | tr -s ' ' ' ' | cut -d' ' -f5-)"
+
+  echo "Updating timestamp of link <$ORIG> ---> <$LINK> timestamp"
+
+  # transfer timestamp
+  touch -h -m -r "$ORIG" "$LINK"
+ done
+}
+
 cd /home/neteler/grass-website/ && \
    git pull origin master && \
    rm -rf /home/neteler/grass-website/public/* && \
    nice /home/neteler/go/bin/hugo && \
    rsync -a --delete /home/neteler/grass-website/public/ /var/www/html/ && \
    ln -s /var/www/code_and_data/* /var/www/html/ && \
-   (cd /var/www/html/ && /home/neteler/bin/fix_link_timestamp.sh .)
+   (cd /var/www/html/ && fix_link_timestamp .)
