010c7a362ebf7fa6dca4dda8fe8d7999e40cac16
Paulo van Breugel <paulo@ecodiv.earth>
2025-04-06 17:09:55 +0200

    r.object.thickness: update after csv class refactoring v2
    The commit #1146 hasn't been merged due to issues with branch. Suggestion was to recreate it from start, which I have done here.
    
    In addition, I have added some additional features, including:
    
    - Option to set how transects are created
    - Add area, average, minimum and maximum width statistics to vector object layer (if the user provides a name). A color table is created based on the average width.
    - Add length to attribute table of the itransects vector layer (if the user provides a name).
    
    closes #1146

--- src/raster/r.object.thickness/r.object.thickness.py
+++ src/raster/r.object.thickness/r.object.thickness.py
@@ -23,6 +23,7 @@
 # %end
 # %option G_OPT_R_INPUT
 # %end
+
 # %option
 # % key: category
 # % type: integer
@@ -30,6 +31,7 @@
 # % multiple: no
 # % description: Category to evaluate
 # %end
+
 # %option
 # % key: tsize
 # % type: double
@@ -38,6 +40,7 @@
 # % description: Expected maximum size in map units
 # % answer: 100
 # %end
+
 # %option
 # % key: tspace
 # % type: double
@@ -46,6 +49,7 @@
 # % description: Transect spacing in map units
 # % answer: 2
 # %end
+
 # %option
 # % key: resolutiondir
 # % type: string
@@ -54,30 +58,42 @@
 # % options: N-S,E-W
 # % answer: N-S
 # %end
+
 # %option G_OPT_R_OUTPUT
 # % key: rmedian
 # % description: Raster map of median lines
 # % required: no
 # % guisection: Optional
 # %end
+
 # %option G_OPT_V_OUTPUT
 # % key: vmedian
 # % description: Vector map of median lines
 # % required: no
 # % guisection: Optional
 # %end
+
+# %option G_OPT_V_OUTPUT
+# % key: vcategories
+# % description: Vector map of areas
+# % required: no
+# % guisection: Optional
+# %end
+
 # %option G_OPT_V_OUTPUT
 # % key: transects
 # % description: Vector map of complete transects
 # % required: no
 # % guisection: Optional
 # %end
+
 # %option G_OPT_V_OUTPUT
 # % key: itransects
 # % description: Vector map of inner transects
 # % required: no
 # % guisection: Optional
 # %end
+
 # %option G_OPT_F_OUTPUT
 # % key: csvfilename
 # % description: CSV output file
@@ -85,12 +101,33 @@
 # % guisection: Optional
 # %end
 
+# %option
+# % key: metric
+# % type: string
+# % description: Determines how transect spacing is measured
+# % multiple: no
+# % options: straight, along
+# % descriptions: straight;Straight distance between transect points;along;Distance along the line (see v.transect for details)
+# % answer: straight
+# %end
+
+# %option
+# % key: transect_perpendicular
+# % type: string
+# % description: Determines which line is the transect perpendicular to
+# % multiple: no
+# % options: trend, line
+# % descriptions: trend;Perpendicular to the line connecting transect points;line;Perpendicular to the particular segment of the original line  (see v.transect for details)
+# % answer: trend
 
 import sys
 import os
 import atexit
+from collections import defaultdict
 import csv
-
+import numpy as np
+from statistics import median
+import uuid
 import grass.script as gs
 from grass.exceptions import CalledModuleError
 
@@ -99,9 +136,37 @@ import gettext
 
 gettext.install("grassmods", os.path.join(os.getenv("GISBASE"), "locale"))
 
+# Cleanup
+CLEAN_LAY = []
+DBTABLE = []
 
-def main():
-    options, flags = gs.parser()
+
+def cleanup():
+    """Remove temporary maps specified in the global list"""
+    cleanrast = list(reversed(CLEAN_LAY))
+    for rast in cleanrast:
+        gs.run_command(
+            "g.remove", type=["raster", "vector"], name=rast, quiet=True, flags="f"
+        )
+    if len(DBTABLE) > 0:
+        gs.run_command("db.droptable", flags="f", table=DBTABLE[0], quiet=True)
+
+
+def create_unique_name(name):
+    """Generate a tmp name which contains prefix
+    Store the name in the global list.
+    Use only for raster maps.
+    """
+    return name + str(uuid.uuid4().hex)
+
+
+def create_temporary_name(prefix):
+    tmpf = create_unique_name(prefix)
+    CLEAN_LAY.append(tmpf)
+    return tmpf
+
+
+def main(options, flags):
 
     # required input
     input = options["input"]
@@ -111,6 +176,7 @@ def main():
 
     # optional output
     rmedian = options["rmedian"]
+    vcategories = options["vcategories"]
     vmedian = options["vmedian"]
     transects = options["transects"]
     itransects = options["itransects"]
@@ -130,18 +196,16 @@ def main():
 
     # check if input file exists
     if not gs.find_file(input)["file"]:
-        gs.fatal(_("Raster map <%s> not found") % input)
+        gs.fatal(_("Raster map {} not found".format(input)))
 
     # strip mapset name
     in_name_strip = options["input"].split("@")
     in_name = in_name_strip[0]
 
-    tmp = str(os.getpid())
-
     # create a map containing only the category to replace and NULL
-    categorymap = "{}".format(in_name) + "_bin_" + "{}".format(tmp)
-    gs.verbose(_("Category map: <%s>") % categorymap)
-    gs.verbose(_("Extracting category: <%d>") % category)
+    categorymap = create_temporary_name(f"{in_name}_bin_")
+    gs.verbose("Category map: {}".format(categorymap))
+    gs.verbose(_("Extracting category: {}".format(category)))
 
     gs.run_command(
         "r.mapcalc",
@@ -154,7 +218,8 @@ def main():
 
     # create a map containing midlines for each clump
     gs.verbose(_("Finding median lines"))
-    categorymap_thin = "{}".format(categorymap) + "_thin"
+    categorymap_thin = f"{categorymap}_thin"
+    CLEAN_LAY.append(categorymap_thin)
     gs.run_command(
         "r.thin", input=categorymap, output=categorymap_thin, quiet=True, overwrite="t"
     )
@@ -167,13 +232,13 @@ def main():
         output=categorymap_thin,
         type="line",
         quiet=True,
-        overwrite="t",
     )
 
     # create transects
-    # half size (left and right) of the transect, must be larger than the larger expected hslf size of objects
+    # half size (left and right) of the transect, must be larger than the larger expected half size of objects
     dsize = tsize / 2
-    categorymap_transects = "{}".format(categorymap) + "_transects"
+    categorymap_transects = f"{categorymap}_transects"
+    CLEAN_LAY.append(categorymap_transects)
     gs.verbose(_("Creating transects"))
     gs.run_command(
         "v.transects",
@@ -182,29 +247,25 @@ def main():
         transect_spacing=tspace,
         dleft=dsize,
         dright=dsize,
+        transect_perpendicular=options["transect_perpendicular"],
+        metric=options["metric"],
         quiet=True,
-        overwrite="t",
     )
 
     # clip transects with the clumps
     # convert binarymap to vector
     gs.verbose(_("Creating vector area map"))
     gs.run_command(
-        "r.to.vect",
-        input=categorymap,
-        output=categorymap,
-        type="area",
-        quiet=True,
-        overwrite="t",
+        "r.to.vect", input=categorymap, output=categorymap, type="area", quiet=True
     )
 
     # drop unneeded column "label" from the table
-    gs.run_command(
-        "v.db.dropcolumn", map=categorymap, columns="label", quiet=True, overwrite="t"
-    )
+    gs.run_command("v.db.dropcolumn", map=categorymap, columns="label", quiet=True)
 
     # clip transects with object
-    categorymap_transects_inside = "{}".format(categorymap) + "_transects_inside"
+    categorymap_transects_inside = f"{categorymap}_transects_inside"
+    CLEAN_LAY.append(categorymap_transects_inside)
+
     gs.verbose(_("Clipping transects"))
     # exit if no transect has been created
     try:
@@ -236,15 +297,6 @@ def main():
         message += _(" lower transects spacing.")
         gs.fatal(message)
 
-    # add a column for the transects length
-    gs.verbose(_("Evaluating transects lengths"))
-    gs.run_command(
-        "v.db.addcolumn",
-        map=categorymap_transects_inside,
-        columns="length double",
-        quiet=True,
-        overwrite="t",
-    )
     # upload transects' lengths
     gs.run_command(
         "v.to.db",
@@ -252,12 +304,11 @@ def main():
         option="length",
         columns="length",
         quiet=True,
-        overwrite="t",
     )
 
     # CSV manipulation
-    # create csv file
-    csv_file = "./" + "{}".format(categorymap_transects_inside) + ".csv"
+    # create temporary csv file
+    csv_file = f"{gs.tempdir()}/{gs.tempname(8)}.csv"
 
     gs.verbose(_("Creating CSV file"))
     gs.run_command(
@@ -266,7 +317,6 @@ def main():
         output=csv_file,
         format="CSV",
         quiet=True,
-        overwrite="t",
     )
 
     # initializing the titles and rows list
@@ -279,7 +329,7 @@ def main():
         csvreader = csv.reader(csvfile)
 
         # extracting field names through first row
-        fields = csvreader.next()
+        fields = next(csvreader)
 
         # extracting each data row one by one
         for row in csvreader:
@@ -295,6 +345,7 @@ def main():
     min_thickness = min(result, key=float)
     max_thickness = max(result, key=float)
     mean_thickness = sum(result) / len(result)
+    median_thickness = median(result)
 
     # region resolution in map units
     region = gs.region()
@@ -308,25 +359,34 @@ def main():
     min_thickness_pixel = min_thickness / resolution
     max_thickness_pixel = max_thickness / resolution
     mean_thickness_pixel = mean_thickness / resolution
+    median_thickness_pixel = median_thickness / resolution
 
     gs.message(
-        _("Thickness in map units: min %f  max %f  mean %f")
-        % (min_thickness, max_thickness, mean_thickness)
+        _(
+            "Thickness in map units:\nminimum = {}\nmaximun = {}\nmean    = {}\nmedian  = {}\n\n"
+        ).format(min_thickness, max_thickness, mean_thickness, median_thickness)
     )
     gs.message(
-        _("Thickness in pixels: min %f  max %f  mean %f")
-        % (min_thickness_pixel, max_thickness_pixel, mean_thickness_pixel)
+        _(
+            "Thickness in pixels:\nminimum = {:.4f}\nmaximum = {:.4f}\nmean   = {:.4f}\nmedian = {:.4f}"
+        ).format(
+            min_thickness_pixel,
+            max_thickness_pixel,
+            mean_thickness_pixel,
+            median_thickness_pixel,
+        )
     )
 
     # If the maximum thickness is equal to the expected maximum size print a warning
     if max_thickness >= tsize:
-        message = _(
-            "maximum thickness %(max_thickness)f is larger or equal to "
-            "the input expected maximum size %(size)f: "
-            "the border of the largest object has not been reached, "
-            "rise the expected maximum size value."
+        gs.warning(
+            _(
+                "maximum thickness {} is larger or equal to the imput \n"
+                "expected maximum size {}: the border of the largest \n"
+                "object has not been reached, rise the expected maximum \n"
+                "size value."
+            ).format(str(max_thickness), str(tsize))
         )
-        gs.warning(message % {"max_thickness": max_thickness, "size": tsize})
 
     # copy the maps if the user has provided a name
     if rmedian:
@@ -336,6 +396,73 @@ def main():
             overwrite="{}".format(overwrite_flag),
             quiet=True,
         )
+    if vcategories:
+        gs.run_command(
+            "g.copy",
+            vector="{inmap},{outmap}".format(inmap=categorymap, outmap=vcategories),
+            overwrite="{}".format(overwrite_flag),
+            quiet=True,
+        )
+        gs.run_command(
+            "v.db.dropcolumn", map=vcategories, columns=["value"], quiet=True
+        )
+        gs.run_command(
+            "v.to.db",
+            map=vcategories,
+            option="area",
+            columns="area",
+            quiet=True,
+        )
+
+        def table_exists(table_name):
+            tables = gs.read_command("db.tables", flags="p").splitlines()
+            return table_name in tables
+
+        # Create temporary table name
+        while True:
+            temp_table = create_unique_name("tmp")
+            if not table_exists(temp_table):
+                break
+        DBTABLE.append(temp_table)
+
+        # Create table with statistics
+        sqlstat1 = (
+            f"CREATE TABLE {temp_table} ("
+            f"b_cat INTEGER PRIMARY KEY, "
+            f"avg_width DOUBLE PRECISION, "
+            f"min_width DOUBLE PRECISION, "
+            f"max_width DOUBLE PRECISION);"
+        )
+        sqlstat2 = (
+            f"INSERT INTO {temp_table} "
+            f"(b_cat, avg_width, min_width, max_width) "
+            f"SELECT b_cat, "
+            f"AVG(length), "
+            f"MIN(length), "
+            f"MAX(length) "
+            f"FROM {categorymap_transects_inside} "
+            f"GROUP BY b_cat;"
+        )
+        gs.run_command("db.execute", sql=sqlstat1)
+        gs.run_command("db.execute", sql=sqlstat2)
+
+        # Link statistics to attribute table
+        gs.run_command(
+            "v.db.join",
+            map=vcategories,
+            column="cat",
+            other_table=temp_table,
+            other_column="b_cat",
+        )
+        gs.run_command(
+            "v.colors",
+            map=vcategories,
+            use="attr",
+            column="avg_width",
+            color="bgyr",
+            quiet=True,
+        )
+
     if vmedian:
         gs.run_command(
             "g.copy",
@@ -343,6 +470,17 @@ def main():
             overwrite="{}".format(overwrite_flag),
             quiet=True,
         )
+        gs.run_command(
+            "v.db.dropcolumn", map=vmedian, columns=["label", "value"], quiet=True
+        )
+        gs.run_command(
+            "v.to.db",
+            map=vmedian,
+            option="length",
+            columns="length",
+            quiet=True,
+        )
+
     if transects:
         gs.run_command(
             "g.copy",
@@ -361,6 +499,14 @@ def main():
             overwrite="{}".format(overwrite_flag),
             quiet=True,
         )
+        gs.run_command(
+            "v.colors",
+            map=itransects,
+            use="attr",
+            column="length",
+            color="bgyr",
+            quiet=True,
+        )
     if csv_file_out:
         gs.run_command(
             "v.out.ogr",
@@ -368,29 +514,12 @@ def main():
             output=csv_file_out,
             format="CSV",
             quiet=True,
-            overwrite="t",
         )
-
-    # cleanup
-    # raster
-    temp_raster_maps = [categorymap, categorymap_thin]
-    for temp_map in temp_raster_maps:
-        gs.run_command("g.remove", type="raster", name=temp_map, flags="f", quiet=True)
-    # vector
-    temp_vector_maps = [
-        categorymap,
-        categorymap_thin,
-        categorymap_transects,
-        categorymap_transects_inside,
-    ]
-    for temp_map in temp_vector_maps:
-        gs.run_command("g.remove", type="vector", name=temp_map, flags="f", quiet=True)
 
     # remove temporary csv_file
     os.remove(csv_file)
 
 
 if __name__ == "__main__":
-    options, flags = gs.parser()
-    # atexit.register(cleanup)
-    main()
+    atexit.register(cleanup)
+    sys.exit(main(*gs.parser()))
