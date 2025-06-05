28b1f6afaabc5bb4d5ae31526b859e3349cbecaf
Anna Petrasova <kratochanna@gmail.com>
2025-06-05 15:21:30 -0400

    better test

--- src/raster/r.gravity.terrain/testsuite/test_r_gravity_terrain.py
+++ src/raster/r.gravity.terrain/testsuite/test_r_gravity_terrain.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
@@ -7,23 +8,31 @@ import grass.script as gs
 
 
 class TestRGravityTerrain(TestCase):
-    elevation = "test_elevation"
+    elevation = "elevation"
     points = "test_points"
+    points3d = "test_points3d"
     output = "test_output.csv"
 
     @classmethod
     def setUpClass(cls):
         cls.use_temp_region()
-        cls.runModule("g.region", n=5, s=0, e=5, w=0, res=1)
+        cls.runModule("g.region", raster=cls.elevation)
         gs.write_command(
-            "v.in.ascii", input="-", output=cls.points, z=3, stdin="2.5|2.5|1"
+            "v.in.ascii",
+            input="-",
+            output=cls.points,
+            stdin="637216.750|221422.888\n641376.038|222513.187",
         )
-        cls.runModule("r.mapcalc", expression=f"{cls.elevation} = 1")
+        cls.runModule(
+            "v.drape", input=cls.points, output=cls.points3d, elevation=cls.elevation
+        )
+        cls.runModule("g.region", vector=cls.points, align=cls.elevation, grow=200)
 
     @classmethod
     def tearDownClass(cls):
-        cls.runModule("g.remove", flags="f", type="raster", name=cls.elevation)
-        cls.runModule("g.remove", flags="f", type="vector", name=cls.points)
+        cls.runModule(
+            "g.remove", flags="f", type="vector", name=[cls.points, cls.points3d]
+        )
         cls.del_temp_region()
 
     def tearDown(cls):
@@ -35,12 +44,32 @@ class TestRGravityTerrain(TestCase):
         self.assertModule(
             "r.gravity.terrain",
             elevation=self.elevation,
-            points=self.points,
-            minimum_distance=1,
-            maximum_distance=5,
+            points=self.points3d,
+            minimum_distance=30,
+            maximum_distance=2000,
             output=self.output,
+            nprocs=1,
         )
+
         self.assertTrue(os.path.exists(self.output), f"{self.output} does not exist")
+
+        expected_data = {
+            1: 0.026039,
+            2: 0.016678,
+        }
+
+        with open(self.output, "r") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                category = int(row["category"])
+                correction = float(row["correction"])
+                self.assertIn(category, expected_data)
+                self.assertAlmostEqual(
+                    correction,
+                    expected_data[category],
+                    places=6,
+                    msg=f"Correction value for category {category} does not match expected",
+                )
 
 
 if __name__ == "__main__":
