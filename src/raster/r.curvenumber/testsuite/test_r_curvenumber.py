dc88bbcdd2cab242dc9136d96fd1445fbfe8fa77
mabdazzam <mabdazzam@outlook.com>
2025-09-21 22:30:02 -0600

    fix: update the unittest

--- src/raster/r.curvenumber/testsuite/test_r_curvenumber.py
+++ src/raster/r.curvenumber/testsuite/test_r_curvenumber.py
@@ -16,16 +16,9 @@ class TestRCn(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.use_temp_region()
-        cls.runModule("r.in.gdal", input=f"data/{cls.lc}.tif", output=cls.lc, flags="o")
-        cls.runModule(
-            "r.in.gdal", input=f"data/{cls.hsg}.tif", output=cls.hsg, flags="o"
-        )
-        cls.runModule(
-            "r.in.gdal",
-            input=f"data/{cls.expected}.tif",
-            output=cls.expected,
-            flags="o",
-        )
+        cls.runModule("r.in.gdal", input=f"data/{cls.lc}.tif", output=cls.lc, flags="o", overwrite=True)
+        cls.runModule("r.in.gdal", input=f"data/{cls.hsg}.tif", output=cls.hsg, flags="o", overwrite=True)
+        cls.runModule("r.in.gdal", input=f"data/{cls.expected}.tif", output=cls.expected, flags="o", overwrite=True)
         cls.runModule("g.region", raster=cls.lc)
 
     @classmethod
@@ -40,7 +33,7 @@ class TestRCn(TestCase):
             "g.remove", flags="f", type="raster", name=(self.computed, self.diff)
         )
 
-    def test_nlcd_against_expected(self):
+    def test_esa_against_expected(self):
         """Run r.curvenumber with source=esa and verify pixel-perfect match."""
         # 1) compute CN with r.curvenumber module
         self.assertModule(
@@ -55,7 +48,7 @@ class TestRCn(TestCase):
         # 2) subtract expected from computed
         self.runModule(
             "r.mapcalc",
-            expression=f"{self.diff} = {self.computed} - {self.expected}",
+            expression=f"{self.diff} = {self.computed} - {self.expected}", overwrite=True
         )
 
         # 3) difference must be zero everywhere
