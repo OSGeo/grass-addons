7a82bfe26b6e38a032343572906add45383f16cc
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-10-18 16:24:10 +0000

    r.estimap.recreation: Add optional score parameter in compute_attractiveness() as used in other calls of this function

--- src/raster/r.estimap.recreation/estimap_recreation/distance.py
+++ src/raster/r.estimap.recreation/estimap_recreation/distance.py
@@ -90,7 +90,7 @@ def build_distance_function(
 
 
 def compute_attractiveness(
-    raster, metric, constant, kappa, alpha, mask=None, output_name=None
+    raster, metric, constant, kappa, alpha, score=None, mask=None, output_name=None
 ):
     """
     Compute a raster map whose values follow an (euclidean) distance function
