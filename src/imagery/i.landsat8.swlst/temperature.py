2a202244367e353bd949acd5a15fb80f331fbd5e
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-10-18 16:05:21 +0000

    Move inputs list in docstring before parameters in src/imagery/i.landsat8.swlst/temperature.py estimate_lst()

--- src/imagery/i.landsat8.swlst/temperature.py
+++ src/imagery/i.landsat8.swlst/temperature.py
@@ -89,6 +89,13 @@ def estimate_lst(
     Produce a Land Surface Temperature map based on a mapcalc expression
     returned from a SplitWindowLST object.
 
+    Inputs are:
+
+    - brightness temperature maps t10, t11
+    - column water vapor map
+    - a temporary filename
+    - a valid mapcalc expression
+
     Parameters
     ----------
     outname
@@ -114,13 +121,6 @@ def estimate_lst(
     celsius
 
     quiet
-
-    Inputs are:
-
-    - brightness temperature maps t10, t11
-    - column water vapor map
-    - a temporary filename
-    - a valid mapcalc expression
     """
     msg = "\n|i Estimating land surface temperature "
     g.message(msg)
