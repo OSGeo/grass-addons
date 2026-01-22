a1efbe4bd9163714f32b9ba473c21f5f6b676ffe
Paulo van Breugel <paulo@ecodiv.earth>
2026-01-22 19:52:29 +0100

    add parallel processing for r.neighbors

--- src/raster/r.forestfrag/r.forestfrag.py
+++ src/raster/r.forestfrag/r.forestfrag.py
@@ -67,6 +67,12 @@
 # % required: no
 # %end
 
+# %option G_OPT_MEMORYMB
+# %end
+
+# %option G_OPT_M_NPROCS
+# %end
+
 # %flag
 # % key: r
 # % description: Set computational region to input raster map
@@ -186,6 +192,8 @@ def main(options, flags):
     ipl = options["input"]
     raster_exists(ipl)
     opl = options["output"]
+    memory = int(options["memory"])
+    nprocs = int(options["nprocs"])
     # Size option backwards compatibility with window
     if not options["size"] and not options["window"]:
         gs.fatal(_("Required parameter <%s> not set") % "size")
@@ -258,6 +266,8 @@ def main(options, flags):
         output=[tmpA2, tmpC3],
         method=["sum", "count"],
         size=wz,
+        nprocs=nprocs,
+        memory=memory,
     )
 
     # Create pf map
@@ -325,8 +335,7 @@ def main(options, flags):
     else:
         indexfin2 = opl
     mapcalc(
-        "eval("
-        "dpf = $pf - $pff,"
+        "eval(" "dpf = $pf - $pff,"
         # Individual classes
         "patch = if($pf < 0.4, 1, 0),"
         "transitional = if($pf >= 0.4 && $pf < 0.6, 2, 0),"
