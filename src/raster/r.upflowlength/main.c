538b814ad13b38979c06d30e91d1654267f668c9
Huidae Cho <grass4u@gmail.com>
2026-06-03 03:21:42 -0600

    r.upflowlength: Remove gdal.h and cpl_conv.h

--- src/raster/r.upflowlength/main.c
+++ src/raster/r.upflowlength/main.c
@@ -17,8 +17,6 @@
  *****************************************************************************/
 #include <stdlib.h>
 #include <string.h>
-#include <gdal.h>
-#include <cpl_conv.h>
 #ifdef _OPENMP
 #include <omp.h>
 #endif
