ca91147fa53032a0f52b91820dc4d7ebb920fb89
tomazz <tomazz@localhost>
2026-07-15 21:45:24 +0200

    libsixsv: remove manual seed, prepare for subtree

--- src/imagery/i.hyper/libsixsv/src/kernel.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../include/sixs_ctx.h"
-void sixs_kernel(const SixsCtx *ctx, int is, int mu,
-                  const float *rm_off,
-                  double *xpl_off,
-                  double *psl,
-                  double *bp);
--- src/imagery/i.hyper/libsixsv/src/trunca.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "../include/sixs_ctx.h"
-
-/* Decompose ctx->polar.pha[] into Legendre coefficients ctx->polar.betal[].
- * ipol: 0 = scalar only. Sets *coeff = 0 (no truncation). */
-void sixs_trunca(SixsCtx *ctx, int ipol, float *coeff);
