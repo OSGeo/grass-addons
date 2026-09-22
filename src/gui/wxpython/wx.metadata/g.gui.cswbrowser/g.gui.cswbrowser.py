4ddf6e3ef291430fea7dfc620b636a66b6cb438e
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2026-09-22 07:45:52 -0400

    g.gui.cswbrowser: Import the GUI after the parser (#1857)

--- src/gui/wxpython/wx.metadata/g.gui.cswbrowser/g.gui.cswbrowser.py
+++ src/gui/wxpython/wx.metadata/g.gui.cswbrowser/g.gui.cswbrowser.py
@@ -18,45 +18,20 @@ This program is free software under the GNU General Public License
 # %end
 
 import grass.script as gs
-from grass.script.setup import set_gui_path
-
-gs.utils.set_path(modulename="wx.metadata", dirname="mdlib", path="..")
-
-from mdlib import globalvar
-from mdlib.cswlib import CSWBrowserPanel, CSWConnectionPanel
-
-import wx
-
-set_gui_path()
-
-
-class CswBrowserMainDialog(wx.Frame):
-    def __init__(self, giface=None):
-        wx.Frame.__init__(self, None, title="Metadata browser", size=(1024, 760))
-
-        self.mainNotebook = wx.Notebook(self, wx.ID_ANY)
-        self.config = wx.Config("g.gui.cswbrowser")
-
-        self.BrowserPanel = CSWBrowserPanel(self.mainNotebook, self, giface)
-        self.connectionPanel = CSWConnectionPanel(self.mainNotebook, self)
-        self.mainNotebook.AddPage(self.BrowserPanel, text="Find")
-        self.mainNotebook.AddPage(self.connectionPanel, text="Configure")
-        self._layout()
-
-    def _layout(self):
-        self.mainsizer = wx.BoxSizer(wx.VERTICAL)
-        self.mainsizer.Add(
-            self.mainNotebook,
-            1,
-            wx.EXPAND,
-        )
-        self.SetSizer(self.mainsizer)
 
 
 def main(giface=None):
+    # The GUI is imported only after the parser has run, so that the
+    # interface description can be created without wxPython installed.
+    gs.utils.set_path(modulename="wx.metadata", dirname="mdlib", path="..")
+
+    import wx
+
+    from mdlib.cswlib import CswBrowserMainDialog
+
     app = wx.App()
-    a = CswBrowserMainDialog(giface)
-    a.Show()
+    browser = CswBrowserMainDialog(giface)
+    browser.Show()
     app.MainLoop()
 
 
